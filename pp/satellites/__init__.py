1f1af8f0e10cc492244b2d0022d2ec1bd0bfdb18
Martin Raspaud <martin.raspaud@smhi.se>
2010-09-16 13:43:28 +0200

    Feature: Moved get_satellite_class and build_satellite_class to pp.satellites.

--- pp/satellites/__init__.py
+++ pp/satellites/__init__.py
@@ -1,16 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010.
+
+# SMHI,
+# Folkborgsvägen 1,
+# Norrköping, 
+# Sweden
+
+# Author(s):
+ 
+#   Martin Raspaud <martin.raspaud@smhi.se>
+
+# This file is part of mpop.
+
+# mpop is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# mpop is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with
+# mpop.  If not, see <http://www.gnu.org/licenses/>.
+
 """Load relevant modules.
 """
 
-import meteosat07
-import meteosat09
-import noaa15
-import noaa17
-import noaa18
-import noaa19
-import metop02
-import globalmetop02
-import earsmetop02
-import aqua
+import pp.satellites.meteosat07
+import pp.satellites.meteosat09
+import pp.satellites.noaa15
+import pp.satellites.noaa17
+import pp.satellites.noaa18
+import pp.satellites.noaa19
+import pp.satellites.metop02
+import pp.satellites.globalmetop02
+import pp.satellites.earsmetop02
+import pp.satellites.aqua
 import pp.satellites
 
 from pp import CONFIG_PATH
@@ -20,8 +46,10 @@ import logging
 
 LOG = logging.getLogger("pp.satellites")
 
-def get_satellite_class(satellite, number, variant):
-    """Get the class for a given satellite.
+def get_satellite_class(satellite, number, variant=""):
+    """Get the class for a given satellite, defined by the three strings
+    *satellite*, *number*, and *variant*. If no class is found, an attempt is
+    made to build the class from a corresponding configuration file.
     """
     for i in dir(pp.satellites):
         module_name = "pp.satellites."+i
@@ -49,16 +77,17 @@ def build_instrument(name, channels):
         instrument_name = name
     return Instrument
                      
-def build_satellite_class(satellite, num, var):
-    """Build a class for the given satellite on the fly, using a config file.
+def build_satellite_class(satellite, num, var=""):
+    """Build a class for the given satellite (defined by the three strings
+    *satellite*, *num*, and *var*) on the fly, using a config file. The
+    function returns as many classes as there are instruments defined in the
+    configuration files.
     """
 
     fullname = var + satellite + num
     
     conf = ConfigParser()
     conf.read(os.path.join(CONFIG_PATH, fullname + ".cfg"))
-    LOG.debug("Build new class from " +
-              os.path.join(CONFIG_PATH, fullname + ".cfg"))
     instruments = eval(conf.get("satellite", "instruments"))
     sat_classes = []
     for instrument in instruments:
