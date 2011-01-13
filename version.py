d73d3d757ea4110228c689b3db0ac3bb6d2943a4
Martin Raspaud <martin.raspaud@smhi.se>
2011-01-13 08:24:25 +0100

    Bugfix: remove hudson from tags when getting version.

--- version.py
+++ version.py
@@ -38,7 +38,8 @@ from subprocess import Popen, PIPE
 
 def call_git_describe(abbrev=4):
     try:
-        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev],
+        p = Popen(['git', 'describe',
+                   '--match="[^hudson]*" --abbrev=%d' % abbrev],
                   stdout=PIPE, stderr=PIPE)
         p.stderr.close()
         line = p.stdout.readlines()[0]
