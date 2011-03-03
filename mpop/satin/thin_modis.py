7d08f53ae14ed3dec617eb1171ee5e269f51e38d
Martin Raspaud <martin.raspaud@smhi.se>
2011-03-03 14:02:30 +0100

    Feature: thin modis reader upgrade, with lonlat estimator and channel trimmer for broken sensors.

--- mpop/satin/thin_modis.py
+++ mpop/satin/thin_modis.py
@@ -37,7 +37,7 @@ import numpy as np
 from pyhdf.SD import SD
 
 from mpop import CONFIG_PATH
-
+from mpop.satin.logger import LOG
 
 def load(satscene, *args, **kwargs):
     """Read data from file and load it into *satscene*.
@@ -162,6 +162,32 @@ def load_thin_modis(satscene, options):
                                                     copy = False)
             
                     satscene[band] = banddata
+
+    mda = data.attributes()["CoreMetadata.0"]
+    orbit_idx = mda.index("ORBITNUMBER")
+    satscene.orbit = mda[orbit_idx + 111:orbit_idx + 116]
+
+    lat, lon = get_lat_lon(satscene, None)
+    from pyresample import geometry
+    satscene.area = geometry.SwathDefinition(lons=lon, lats=lat)
+
+    # trimming out dead sensor lines
+    if satscene.satname == "aqua":
+        for band in ["6", "27"]:
+            if not satscene[band].is_loaded() or satscene[band].data.mask.all():
+                continue
+            indices = satscene[band].data.mask.sum(1) < 1354
+            satscene[band] = satscene[band].data[indices, :]
+            satscene[band].area = geometry.SwathDefinition(
+                lons=satscene.area.lons[indices,:],
+                lats=satscene.area.lats[indices,:])
+            satscene[band].area.area_id = ("swath_" + satscene.fullname + "_"
+                                           + str(satscene.time_slot) + "_"
+                                           + str(satscene[band].shape) + "_"
+                                           + str(band))
+
+
+    
 def get_lat_lon(satscene, resolution):
     """Read lat and lon.
     """
@@ -193,6 +219,299 @@ def get_lat_lon_thin_modis(satscene, options):
     
 
     return lat, lon
+
+def get_lonlat(satscene, row, col):
+    """Estimate lon and lat.
+    """
+
+    conf = ConfigParser()
+    conf.read(os.path.join(CONFIG_PATH, satscene.fullname + ".cfg"))
+    path = conf.get("modis-level2", "dir")
+    filename = satscene.time_slot.strftime("thin_MOD03.P%Y%j.%H%M.hdf")
+    filename = os.path.join(path, filename)
+    
+    if(os.path.exists(filename) and
+       (satscene.lon is None or satscene.lat is None)):
+        data = SD(filename)
+        lat = data.select("Latitude")
+        fill_value = lat.attributes()["_FillValue"]
+        satscene.lat = np.ma.masked_equal(lat.get(), fill_value)
+        lon = data.select("Longitude")
+        fill_value = lon.attributes()["_FillValue"]
+        satscene.lon = np.ma.masked_equal(lon.get(), fill_value)
+
+    estimate = True
+
+    try:
+        lon = satscene.lon[row, col]
+        lat = satscene.lat[row, col]
+        if satscene.lon.mask[row, col] == False and satscene.lat.mask[row, col] == False:
+            estimate = False
+    except TypeError:
+        pass
+    except IndexError:
+        pass
+
+    if not estimate:
+        return lon, lat
+
+    from mpop.saturn.two_line_elements import Tle
+
+    tle = Tle(satellite=satscene.satname)
+    track_start = tle.get_latlonalt(satscene.time_slot)
+    track_end = tle.get_latlonalt(satscene.time_slot + satscene.granularity)
+
+    # WGS84
+    # flattening
+    f = 1/298.257223563
+    # semi_major_axis
+    a = 6378137.0
+
+    s, alpha12, alpha21 = vinc_dist(f, a,
+                                    track_start[0], track_start[1],
+                                    track_end[0], track_end[1])
+    scanlines = satscene.granularity.seconds / satscene.span
+
+    if row < scanlines/2:
+        if row == 0:
+            track_now = track_start
+        else:
+            track_now = vinc_pt(f, a, track_start[0], track_start[1], alpha12,
+                                (s * row) / scanlines)
+        lat_now = track_now[0]
+        lon_now = track_now[1]
+        
+        s, alpha12, alpha21 = vinc_dist(f, a,
+                                        lat_now, lon_now,
+                                        track_end[0], track_end[1])
+        fac = 1
+    else:
+        if scanlines - row - 1 == 0:
+            track_now = track_end
+        else:
+            track_now = vinc_pt(f, a, track_end[0], track_end[1], alpha21,
+                                (s * (scanlines - row - 1)) / scanlines)
+        lat_now = track_now[0]
+        lon_now = track_now[1]
+        
+        s, alpha12, alpha21 = vinc_dist(f, a,
+                                        lat_now, lon_now,
+                                        track_start[0], track_start[1])
+        fac = -1
+
+    if col < 1354/2:
+        lat, lon, alp = vinc_pt(f, a, lat_now, lon_now, alpha12 + np.pi/2 * fac,
+                                2340000.0 / 2 - (2340000.0/1354) * col)
+    else:
+        lat, lon, alp = vinc_pt(f, a, lat_now, lon_now, alpha12 - np.pi/2 * fac,
+                                (2340000.0/1354) * col - 2340000.0 / 2)
+
+    lon = np.rad2deg(lon)
+    lat = np.rad2deg(lat)
+
+    if lon > 180:
+        lon -= 360
+    if lon <= -180:
+        lon += 360
+    
+    return lon, lat
+        
+    
+import math
+
+def vinc_dist(  f,  a,  phi1,  lembda1,  phi2,  lembda2 ) :
+    """ 
+
+    Returns the distance between two geographic points on the ellipsoid
+    and the forward and reverse azimuths between these points.
+    lats, longs and azimuths are in radians, distance in metres 
+
+    Returns ( s, alpha12,  alpha21 ) as a tuple
+
+    """
+
+    if (abs( phi2 - phi1 ) < 1e-8) and ( abs( lembda2 - lembda1) < 1e-8 ) :
+        return 0.0, 0.0, 0.0
+
+    two_pi = 2.0*math.pi
+
+    b = a * (1.0 - f)
+
+    TanU1 = (1-f) * math.tan( phi1 )
+    TanU2 = (1-f) * math.tan( phi2 )
+
+    U1 = math.atan(TanU1)
+    U2 = math.atan(TanU2)
+
+    lembda = lembda2 - lembda1
+    last_lembda = -4000000.0                # an impossibe value
+    omega = lembda
+
+    # Iterate the following equations, 
+    #  until there is no significant change in lembda 
+
+    while ( last_lembda < -3000000.0 or lembda != 0 and abs( (last_lembda - lembda)/lembda) > 1.0e-9 ) :
+
+        sqr_sin_sigma = pow( math.cos(U2) * math.sin(lembda), 2) + \
+                        pow( (math.cos(U1) * math.sin(U2) - \
+                              math.sin(U1) *  math.cos(U2) * math.cos(lembda) ), 2 )
+
+        Sin_sigma = math.sqrt( sqr_sin_sigma )
+        
+        Cos_sigma = math.sin(U1) * math.sin(U2) + math.cos(U1) * math.cos(U2) * math.cos(lembda)
+
+        sigma = math.atan2( Sin_sigma, Cos_sigma )
+
+        Sin_alpha = math.cos(U1) * math.cos(U2) * math.sin(lembda) / math.sin(sigma)
+        alpha = math.asin( Sin_alpha )
+      
+        Cos2sigma_m = math.cos(sigma) - (2 * math.sin(U1) * math.sin(U2) / pow(math.cos(alpha), 2) )
+
+        C = (f/16) * pow(math.cos(alpha), 2) * (4 + f * (4 - 3 * pow(math.cos(alpha), 2)))
+
+        last_lembda = lembda
+        
+        lembda = omega + (1-C) * f * math.sin(alpha) * (sigma + C * math.sin(sigma) * \
+                                                        (Cos2sigma_m + C * math.cos(sigma) * (-1 + 2 * pow(Cos2sigma_m, 2) )))
+
+
+    u2 = pow(math.cos(alpha),2) * (a*a-b*b) / (b*b)
+
+    A = 1 + (u2/16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
+
+    B = (u2/1024) * (256 + u2 * (-128+ u2 * (74 - 47 * u2)))
+
+    delta_sigma = B * Sin_sigma * (Cos2sigma_m + (B/4) * \
+            (Cos_sigma * (-1 + 2 * pow(Cos2sigma_m, 2) ) - \
+            (B/6) * Cos2sigma_m * (-3 + 4 * sqr_sin_sigma) * \
+            (-3 + 4 * pow(Cos2sigma_m,2 ) )))
+
+    s = b * A * (sigma - delta_sigma)
+
+    alpha12 = math.atan2( (math.cos(U2) * math.sin(lembda)), \
+            (math.cos(U1) * math.sin(U2) - math.sin(U1) * math.cos(U2) * math.cos(lembda)))
+
+    alpha21 = math.atan2( (math.cos(U1) * math.sin(lembda)), \
+            (-math.sin(U1) * math.cos(U2) + math.cos(U1) * math.sin(U2) * math.cos(lembda)))
+
+    if ( alpha12 < 0.0 ) : 
+        alpha12 =  alpha12 + two_pi
+    if ( alpha12 > two_pi ) : 
+        alpha12 = alpha12 - two_pi
+        
+    alpha21 = alpha21 + two_pi / 2.0
+    if ( alpha21 < 0.0 ) : 
+        alpha21 = alpha21 + two_pi
+    if ( alpha21 > two_pi ) : 
+        alpha21 = alpha21 - two_pi
+            
+    return s, alpha12,  alpha21 
+
+# END of Vincenty's Inverse formulae 
+
+
+#----------------------------------------------------------------------------
+# Vincenty's Direct formulae                                                |
+# Given: latitude and longitude of a point (phi1, lembda1) and              |
+# the geodetic azimuth (alpha12)                                            |
+# and ellipsoidal distance in metres (s) to a second point,                 |
+#                                                                           |
+# Calculate: the latitude and longitude of the second point (phi2, lembda2) |
+# and the reverse azimuth (alpha21).                                        |
+#                                                                           |
+#----------------------------------------------------------------------------
+
+def  vinc_pt( f, a, phi1, lembda1, alpha12, s ) :
+    """
+
+    Returns the lat and long of projected point and reverse azimuth
+    given a reference point and a distance and azimuth to project.
+    lats, longs and azimuths are passed in decimal degrees
+
+    Returns ( phi2,  lambda2,  alpha21 ) as a tuple 
+
+    """
+
+
+    two_pi = 2.0*math.pi
+
+    if ( alpha12 < 0.0 ) : 
+        alpha12 = alpha12 + two_pi
+    if ( alpha12 > two_pi ) : 
+        alpha12 = alpha12 - two_pi
+
+
+    b = a * (1.0 - f)
+
+    TanU1 = (1-f) * math.tan(phi1)
+    U1 = math.atan( TanU1 )
+    sigma1 = math.atan2( TanU1, math.cos(alpha12) )
+    Sinalpha = math.cos(U1) * math.sin(alpha12)
+    cosalpha_sq = 1.0 - Sinalpha * Sinalpha
+
+    u2 = cosalpha_sq * (a * a - b * b ) / (b * b)
+    A = 1.0 + (u2 / 16384) * (4096 + u2 * (-768 + u2 * \
+            (320 - 175 * u2) ) )
+    B = (u2 / 1024) * (256 + u2 * (-128 + u2 * (74 - 47 * u2) ) )
+
+    # Starting with the approximation
+    sigma = (s / (b * A))
+
+    last_sigma = 2.0 * sigma + 2.0  # something impossible
+
+    # Iterate the following three equations 
+    # until there is no significant change in sigma 
+
+    # two_sigma_m , delta_sigma
+
+    while ( abs( (last_sigma - sigma) / sigma) > 1.0e-9 ) :
+
+        two_sigma_m = 2 * sigma1 + sigma
+
+        delta_sigma = B * math.sin(sigma) * ( math.cos(two_sigma_m) \
+                                              + (B/4) * (math.cos(sigma) * \
+                                                         (-1 + 2 * math.pow( math.cos(two_sigma_m), 2 ) -  \
+                                                          (B/6) * math.cos(two_sigma_m) * \
+                                                          (-3 + 4 * math.pow(math.sin(sigma), 2 )) *  \
+                                                          (-3 + 4 * math.pow( math.cos (two_sigma_m), 2 )))))
+        last_sigma = sigma
+        sigma = (s / (b * A)) + delta_sigma
+
+
+    phi2 = math.atan2 ( (math.sin(U1) * math.cos(sigma) + math.cos(U1) * math.sin(sigma) * math.cos(alpha12) ), \
+                        ((1-f) * math.sqrt( math.pow(Sinalpha, 2) +  \
+                                            pow(math.sin(U1) * math.sin(sigma) - math.cos(U1) * math.cos(sigma) * math.cos(alpha12), 2))))
+
+
+    lembda = math.atan2( (math.sin(sigma) * math.sin(alpha12 )), (math.cos(U1) * math.cos(sigma) -  \
+                                                                  math.sin(U1) *  math.sin(sigma) * math.cos(alpha12)))
+
+    C = (f/16) * cosalpha_sq * (4 + f * (4 - 3 * cosalpha_sq ))
+
+    omega = lembda - (1-C) * f * Sinalpha *  \
+            (sigma + C*math.sin(sigma)*(math.cos(two_sigma_m) +
+                                        C * math.cos(sigma) *
+                                        (-1 + 2 *
+                                         math.pow(math.cos(two_sigma_m),2))))
+
+    lembda2 = lembda1 + omega
+
+    alpha21 = math.atan2 ( Sinalpha, (-math.sin(U1) * math.sin(sigma) +
+                                      math.cos(U1) * math.cos(sigma) *
+                                      math.cos(alpha12)))
+
+    alpha21 = alpha21 + two_pi / 2.0
+    if ( alpha21 < 0.0 ) :
+        alpha21 = alpha21 + two_pi
+    if ( alpha21 > two_pi ) :
+        alpha21 = alpha21 - two_pi
+
+
+    return phi2,  lembda2,  alpha21 
+
+# END of Vincenty's Direct formulae
+
+
 
 
 CASES = {
