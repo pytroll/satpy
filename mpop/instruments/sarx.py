ff22a6a7a00199fd01c3360a6b11b85bcb76a3b1
Lars Orum Rasmussen <ras@dmi.dk>
2012-12-03 11:22:22 +0000

    Introducing clip percentage for SAR average product

--- mpop/instruments/sarx.py
+++ mpop/instruments/sarx.py
@@ -13,7 +13,25 @@ class SarxCompositer(Compositer):
     instrument_name = "sarx"
 
     def average(self, downscaling_factor=2, average_window=None,
-                stretch="linear"):
+                stretch="linear", clip_percentage=None):
+        """
+        Makes a mean convolution of an image.
+
+        :Parameters:
+         `downscaling_factor` : int
+             image downscaling factor, default is a factor 2.             
+         `average_window` : int
+             window size for calculating mean values, default is
+             the same as downscaling_factor.
+         `stretch` : string
+             enhance methode, default is `linear`.
+         `clip_percentage` : tuple
+             percentage discard in near and far end range.
+
+        :Returns:
+         `image` : GeoImage
+             mean convoluted image.
+        """
         from mpop.imageo.geo_image import GeoImage
         from pyresample import geometry
         import scipy.ndimage as ndi
@@ -23,11 +41,11 @@ class SarxCompositer(Compositer):
         if average_window == None:
             average_window = downscaling_factor
 
+        ch = self[9.65]
+
         LOG.info("Downsampling a factor %d and averaging "%downscaling_factor + 
                  "in a window of %dx%d"%(average_window, average_window))
         
-        ch = self[9.65]
-
         # If average window and downscale factor is the same
         # the following could be used:
         #
@@ -35,10 +53,12 @@ class SarxCompositer(Compositer):
         #                         swidth, width/swidth]).mean(3).mean(1)
 
         # avg kernel
+        data = ch.data.astype(np.float).filled(np.nan)
         kernel = np.ones((average_window, average_window), dtype=np.float) / \
             (average_window*average_window)
         # do convolution
-        data = ndi.filters.correlate(ch.data.astype(np.float), kernel, mode='nearest')
+        data = ndi.filters.correlate(data, kernel, mode='nearest')
+        data = np.ma.numpy(data, mask=
         # downscale
         data = data[1::downscaling_factor, 1::downscaling_factor]
 
@@ -59,6 +79,16 @@ class SarxCompositer(Compositer):
                                        data.shape[1], data.shape[0],
                                        area_extent)
 
+        if clip_percentage != None:
+            from scipy import stats
+            cdata = data.flatten() #.compressed()
+            cdata = cdata[cdata != np.nan]
+            score = (stats.scoreatpercentile(cdata, clip_percentage[0]),
+                     stats.scoreatpercentile(cdata, 100. - clip_percentage[1]))
+            LOG.info("Clipping data %f%%, %f%% (%f, %f) in far and near edge", 
+                     clip_percentage[0], clip_percentage[1], score[0], score[1])
+            data = np.clip(data, score[0], score[1], data)
+        
         img = GeoImage(data, area, self.time_slot,
                        fill_value=(0,), mode='L')
         if stretch:
