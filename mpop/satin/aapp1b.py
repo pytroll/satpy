c1142f54df303230b3e0ed6602f0e23392699614
Lars Orum Rasmussen <ras@dmi.dk>
2012-12-04 16:21:01 +0000

    Better show and more cosmetic

--- mpop/satin/aapp1b.py
+++ mpop/satin/aapp1b.py
@@ -62,11 +62,10 @@ def load(satscene, *args, **kwargs):
 def load_avhrr(satscene, options):
     """Read avhrr data from file and load it into *satscene*.
     """
-   
     if "filename" not in options:
         raise IOError("No filename given, cannot load.")
 
-    chns = satscene.channels_to_load & set(avhrr_channel_names)
+    chns = satscene.channels_to_load & set(AVHRR_CHANNEL_NAMES)
     LOG.info("Loading channels " + str(sorted(list(chns))))
     if len(chns) == 0:
         return
@@ -113,193 +112,192 @@ def load_avhrr(satscene, options):
         satscene.area = geometry.SwathDefinition(lons=scene.lons,
                                                  lats=scene.lats)
 
+AVHRR_CHANNEL_NAMES = ("1", "2", "3A", "3B", "4", "5")
+
 # AAPP 1b header
 
-headertype = np.dtype([("siteid", "S3"),
-                       ("blank", "S1"),
-                       ("l1bversnb", "<u2"),
-                       ("l1bversyr", "<u2"),
-                       ("l1bversdy", "<u2"),
-                       ("reclg", "<u2"),
-                       ("blksz", "<u2"),
-                       ("hdrcnt", "<u2"),
-                       ("filler0", "S6"),
-                       ("dataname", "S42"),
-                       ("prblkid", "S8"),
-                       ("satid", "<u2"),
-                       ("instid", "<u2"),
-                       ("datatype", "<u2"),
-                       ("tipsrc", "<u2"),
-                       ("startdatajd", "<u4"),
-                       ("startdatayr", "<u2"),
-                       ("startdatady", "<u2"),
-                       ("startdatatime", "<u4"),
-                       ("enddatajd", "<u4"),
-                       ("enddatayr", "<u2"),
-                       ("enddatady", "<u2"),
-                       ("enddatatime", "<u4"),
-                       ("cpidsyr", "<u2"),
-                       ("cpidsdy", "<u2"),
-                       ("filler1", "S8"),
-                       # data set quality indicators
-                       ("inststat1", "<u4"),
-                       ("filler2", "S2"),
-                       ("statchrecnb", "<u2"),
-                       ("inststat2", "<u4"),
-                       ("scnlin", "<u2"),
-                       ("callocscnlin", "<u2"),
-                       ("misscnlin", "<u2"),
-                       ("datagaps", "<u2"),
-                       ("okdatafr", "<u2"),
-                       ("pacsparityerr", "<u2"),
-                       ("auxsyncerrsum", "<u2"),
-                       ("timeseqerr", "<u2"),
-                       ("timeseqerrcode", "<u2"),
-                       ("socclockupind", "<u2"),
-                       ("locerrind", "<u2"),
-                       ("locerrcode", "<u2"),
-                       ("pacsstatfield", "<u2"),
-                       ("pacsdatasrc", "<u2"),
-                       ("filler3", "S4"),
-                       ("spare1", "S8"),
-                       ("spare2", "S8"),
-                       ("filler4", "S10"),
-                       # Calibration
-                       ("racalind", "<u2"),
-                       ("solarcalyr", "<u2"),
-                       ("solarcaldy", "<u2"),
-                       ("pcalalgind", "<u2"),
-                       ("pcalalgopt", "<u2"),
-                       ("scalalgind", "<u2"),
-                       ("scalalgopt", "<u2"),
-                       ("irttcoef", "<u2", (4, 6)),
-                       ("filler5", "<i4", (2, )),
-                       # radiance to temperature conversion
-                       ("albcnv", "<i4", (2, 3)),
-                       ("radtempcnv", "<i4", (3, 3)),
-                       ("filler6", "<i4", (3, )),
-                       # Navigation
-                       ("modelid", "S8"),
-                       ("nadloctol", "<u2"),
-                       ("locbit", "<u2"),
-                       ("filler7", "S2"),
-                       ("rollerr", "<u2"),
-                       ("pitcherr", "<u2"),
-                       ("yawerr", "<u2"),
-                       ("epoyr", "<u2"),
-                       ("epody", "<u2"),
-                       ("epotime", "<u4"),
-                       ("smaxis", "<u4"),
-                       ("eccen", "<u4"),
-                       ("incli", "<u4"),
-                       ("argper", "<u4"),
-                       ("rascnod", "<u4"),
-                       ("manom", "<u4"),
-                       ("xpos", "<i4"),
-                       ("ypos", "<i4"),
-                       ("zpos", "<i4"),
-                       ("xvel", "<i4"),
-                       ("yvel", "<i4"),
-                       ("zvel", "<i4"),
-                       ("earthsun", "<u4"),
-                       ("filler8", "S16"),
-                       # analog telemetry conversion
-                       ("pchtemp", "<u2", (5, )),
-                       ("reserved1", "<u2"),
-                       ("pchtempext", "<u2", (5, )),
-                       ("reserved2", "<u2"),
-                       ("pchpow", "<u2", (5, )),
-                       ("reserved3", "<u2"),
-                       ("rdtemp", "<u2", (5, )),
-                       ("reserved4", "<u2"),
-                       ("bbtemp1", "<u2", (5, )),
-                       ("reserved5", "<u2"),
-                       ("bbtemp2", "<u2", (5, )),
-                       ("reserved6", "<u2"),
-                       ("bbtemp3", "<u2", (5, )),
-                       ("reserved7", "<u2"),
-                       ("bbtemp4", "<u2", (5, )),
-                       ("reserved8", "<u2"),
-                       ("eleccur", "<u2", (5, )),
-                       ("reserved9", "<u2"),
-                       ("motorcur", "<u2", (5, )),
-                       ("reserved10", "<u2"),
-                       ("earthpos", "<u2", (5, )),
-                       ("reserved11", "<u2"),
-                       ("electemp", "<u2", (5, )),
-                       ("reserved12", "<u2"),
-                       ("chtemp", "<u2", (5, )),
-                       ("reserved13", "<u2"),
-                       ("bptemp", "<u2", (5, )),
-                       ("reserved14", "<u2"),
-                       ("mhtemp", "<u2", (5, )),
-                       ("reserved15", "<u2"),
-                       ("adcontemp", "<u2", (5, )),
-                       ("reserved16", "<u2"),
-                       ("d4bvolt", "<u2", (5, )),
-                       ("reserved17", "<u2"),
-                       ("d5bvolt", "<u2", (5, )),
-                       ("reserved18", "<u2"),
-                       ("bbtempchn3B", "<u2", (5, )),
-                       ("reserved19", "<u2"),
-                       ("bbtempchn4", "<u2", (5, )),
-                       ("reserved20", "<u2"),
-                       ("bbtempchn5", "<u2", (5, )),
-                       ("reserved21", "<u2"),
-                       ("refvolt", "<u2", (5, )),
-                       ("reserved22", "<u2"),
+_headertype = np.dtype([("siteid", "S3"),
+                        ("blank", "S1"),
+                        ("l1bversnb", "<u2"),
+                        ("l1bversyr", "<u2"),
+                        ("l1bversdy", "<u2"),
+                        ("reclg", "<u2"),
+                        ("blksz", "<u2"),
+                        ("hdrcnt", "<u2"),
+                        ("filler0", "S6"),
+                        ("dataname", "S42"),
+                        ("prblkid", "S8"),
+                        ("satid", "<u2"),
+                        ("instid", "<u2"),
+                        ("datatype", "<u2"),
+                        ("tipsrc", "<u2"),
+                        ("startdatajd", "<u4"),
+                        ("startdatayr", "<u2"),
+                        ("startdatady", "<u2"),
+                        ("startdatatime", "<u4"),
+                        ("enddatajd", "<u4"),
+                        ("enddatayr", "<u2"),
+                        ("enddatady", "<u2"),
+                        ("enddatatime", "<u4"),
+                        ("cpidsyr", "<u2"),
+                        ("cpidsdy", "<u2"),
+                        ("filler1", "S8"),
+                        # data set quality indicators
+                        ("inststat1", "<u4"),
+                        ("filler2", "S2"),
+                        ("statchrecnb", "<u2"),
+                        ("inststat2", "<u4"),
+                        ("scnlin", "<u2"),
+                        ("callocscnlin", "<u2"),
+                        ("misscnlin", "<u2"),
+                        ("datagaps", "<u2"),
+                        ("okdatafr", "<u2"),
+                        ("pacsparityerr", "<u2"),
+                        ("auxsyncerrsum", "<u2"),
+                        ("timeseqerr", "<u2"),
+                        ("timeseqerrcode", "<u2"),
+                        ("socclockupind", "<u2"),
+                        ("locerrind", "<u2"),
+                        ("locerrcode", "<u2"),
+                        ("pacsstatfield", "<u2"),
+                        ("pacsdatasrc", "<u2"),
+                        ("filler3", "S4"),
+                        ("spare1", "S8"),
+                        ("spare2", "S8"),
+                        ("filler4", "S10"),
+                        # Calibration
+                        ("racalind", "<u2"),
+                        ("solarcalyr", "<u2"),
+                        ("solarcaldy", "<u2"),
+                        ("pcalalgind", "<u2"),
+                        ("pcalalgopt", "<u2"),
+                        ("scalalgind", "<u2"),
+                        ("scalalgopt", "<u2"),
+                        ("irttcoef", "<u2", (4, 6)),
+                        ("filler5", "<i4", (2, )),
+                        # radiance to temperature conversion
+                        ("albcnv", "<i4", (2, 3)),
+                        ("radtempcnv", "<i4", (3, 3)),
+                        ("filler6", "<i4", (3, )),
+                        # Navigation
+                        ("modelid", "S8"),
+                        ("nadloctol", "<u2"),
+                        ("locbit", "<u2"),
+                        ("filler7", "S2"),
+                        ("rollerr", "<u2"),
+                        ("pitcherr", "<u2"),
+                        ("yawerr", "<u2"),
+                        ("epoyr", "<u2"),
+                        ("epody", "<u2"),
+                        ("epotime", "<u4"),
+                        ("smaxis", "<u4"),
+                        ("eccen", "<u4"),
+                        ("incli", "<u4"),
+                        ("argper", "<u4"),
+                        ("rascnod", "<u4"),
+                        ("manom", "<u4"),
+                        ("xpos", "<i4"),
+                        ("ypos", "<i4"),
+                        ("zpos", "<i4"),
+                        ("xvel", "<i4"),
+                        ("yvel", "<i4"),
+                        ("zvel", "<i4"),
+                        ("earthsun", "<u4"),
+                        ("filler8", "S16"),
+                        # analog telemetry conversion
+                        ("pchtemp", "<u2", (5, )),
+                        ("reserved1", "<u2"),
+                        ("pchtempext", "<u2", (5, )),
+                        ("reserved2", "<u2"),
+                        ("pchpow", "<u2", (5, )),
+                        ("reserved3", "<u2"),
+                        ("rdtemp", "<u2", (5, )),
+                        ("reserved4", "<u2"),
+                        ("bbtemp1", "<u2", (5, )),
+                        ("reserved5", "<u2"),
+                        ("bbtemp2", "<u2", (5, )),
+                        ("reserved6", "<u2"),
+                        ("bbtemp3", "<u2", (5, )),
+                        ("reserved7", "<u2"),
+                        ("bbtemp4", "<u2", (5, )),
+                        ("reserved8", "<u2"),
+                        ("eleccur", "<u2", (5, )),
+                        ("reserved9", "<u2"),
+                        ("motorcur", "<u2", (5, )),
+                        ("reserved10", "<u2"),
+                        ("earthpos", "<u2", (5, )),
+                        ("reserved11", "<u2"),
+                        ("electemp", "<u2", (5, )),
+                        ("reserved12", "<u2"),
+                        ("chtemp", "<u2", (5, )),
+                        ("reserved13", "<u2"),
+                        ("bptemp", "<u2", (5, )),
+                        ("reserved14", "<u2"),
+                        ("mhtemp", "<u2", (5, )),
+                        ("reserved15", "<u2"),
+                        ("adcontemp", "<u2", (5, )),
+                        ("reserved16", "<u2"),
+                        ("d4bvolt", "<u2", (5, )),
+                        ("reserved17", "<u2"),
+                        ("d5bvolt", "<u2", (5, )),
+                        ("reserved18", "<u2"),
+                        ("bbtempchn3B", "<u2", (5, )),
+                        ("reserved19", "<u2"),
+                        ("bbtempchn4", "<u2", (5, )),
+                        ("reserved20", "<u2"),
+                        ("bbtempchn5", "<u2", (5, )),
+                        ("reserved21", "<u2"),
+                        ("refvolt", "<u2", (5, )),
+                        ("reserved22", "<u2"),
 ])
 
 # AAPP 1b scanline
 
-scantype = np.dtype([("scnlin", "<i2"),
-                     ("scnlinyr", "<i2"),
-                     ("scnlindy", "<i2"),
-                     ("clockdrift", "<i2"),
-                     ("scnlintime", "<i4"),
-                     ("scnlinbit", "<i2"),
-                     ("filler0", "S10"),
-                     ("qualind", "<i4"),
-                     ("scnlinqual", "<i4"),
-                     ("calqual", "<u2", (3, )),
-                     ("cbiterr", "<i2"),
-                     ("filler1", "S8"),
-                     # Calibration
-                     ("calvis", "<i4", (3, 3, 5)),
-                     ("calir", "<i4", (3, 2, 3)),
-                     ("filler2", "<i4", (3, )),
-                     # Navigation
-                     ("navstat", "<i4"),
-                     ("attangtime", "<i4"),
-                     ("rollang", "<i2"),
-                     ("pitchang", "<i2"),
-                     ("yawang", "<i2"),
-                     ("scalti", "<i2"),
-                     ("ang", "<i2", (51, 3)),
-                     ("filler3", "<i2", (3, )),
-                     ("pos", "<i4", (51, 2)),
-                     ("filler4", "<i4", (2, )),
-                     ("telem", "<i2", (103, )),
-                     ("filler5", "<i2"),
-                     ("hrpt", "<i2", (2048, 5)),
-                     ("filler6", "<i4", (2, )),
-                     # tip minor frame header
-                     ("tipmfhd", "<i2", (7, 5)),
-                     # cpu telemetry
-                     ("cputel", "S6", (2, 5)),
-                     ("filler7", "<i2", (67, )),
-                     ])
-                     
-
-avhrr_channel_names = ("1", "2", "3A", "3B", "4", "5")
+_scantype = np.dtype([("scnlin", "<i2"),
+                      ("scnlinyr", "<i2"),
+                      ("scnlindy", "<i2"),
+                      ("clockdrift", "<i2"),
+                      ("scnlintime", "<i4"),
+                      ("scnlinbit", "<i2"),
+                      ("filler0", "S10"),
+                      ("qualind", "<i4"),
+                      ("scnlinqual", "<i4"),
+                      ("calqual", "<u2", (3, )),
+                      ("cbiterr", "<i2"),
+                      ("filler1", "S8"),
+                      # Calibration
+                      ("calvis", "<i4", (3, 3, 5)),
+                      ("calir", "<i4", (3, 2, 3)),
+                      ("filler2", "<i4", (3, )),
+                      # Navigation
+                      ("navstat", "<i4"),
+                      ("attangtime", "<i4"),
+                      ("rollang", "<i2"),
+                      ("pitchang", "<i2"),
+                      ("yawang", "<i2"),
+                      ("scalti", "<i2"),
+                      ("ang", "<i2", (51, 3)),
+                      ("filler3", "<i2", (3, )),
+                      ("pos", "<i4", (51, 2)),
+                      ("filler4", "<i4", (2, )),
+                      ("telem", "<i2", (103, )),
+                      ("filler5", "<i2"),
+                      ("hrpt", "<i2", (2048, 5)),
+                      ("filler6", "<i4", (2, )),
+                      # tip minor frame header
+                      ("tipmfhd", "<i2", (7, 5)),
+                      # cpu telemetry
+                      ("cputel", "S6", (2, 5)),
+                      ("filler7", "<i2", (67, )),
+                      ])
 
 class AAPP1b(object):
     """AAPP-level 1b data reader
     """
     def __init__(self, fname):
         self.filename = fname
-        self.channels = dict([(i, None) for i in avhrr_channel_names])
-        self.units = dict([(i, 'counts') for i in avhrr_channel_names])
+        self.channels = dict([(i, None) for i in AVHRR_CHANNEL_NAMES])
+        self.units = dict([(i, 'counts') for i in AVHRR_CHANNEL_NAMES])
 
         self._data = None
         self._header = None
@@ -312,9 +310,9 @@ class AAPP1b(object):
         """
         tic = datetime.datetime.now()
         with open(self.filename, "rb") as fp_:
-            header =  np.fromfile(fp_, dtype=headertype, count=1)
+            header =  np.fromfile(fp_, dtype=_headertype, count=1)
             fp_.seek(10664 * 2, 1)
-            data = np.fromfile(fp_, dtype=scantype)
+            data = np.fromfile(fp_, dtype=_scantype)
 
         LOG.debug("Reading time " + str(datetime.datetime.now() - tic))
         self._header = header
@@ -323,7 +321,6 @@ class AAPP1b(object):
     def navigate(self):
         """Return the longitudes and latitudes of the scene.
         """
-
         tic = datetime.datetime.now()
         lons40km = self._data["pos"][:, :, 1] * 1e-4
         lats40km = self._data["pos"][:, :, 0] * 1e-4
@@ -355,7 +352,6 @@ class AAPP1b(object):
     def calibrate(self, chns=("1", "2", "3A", "3B", "4", "5")):
         """Calibrate the data
         """
-
         tic = datetime.datetime.now()
 
         if "1" in chns:
@@ -399,7 +395,6 @@ class AAPP1b(object):
 def _vis_calibrate(data, chn):
     """Visible channel calibration only
     """
-
     # Calibration count to albedo, the calibration is performed separately for
     # two value ranges.
     
@@ -425,7 +420,6 @@ def _vis_calibrate(data, chn):
 def _ir_calibrate(header, data, irchn):
     """IR calibration
     """
-
     ir_const_1 = 1.1910659e-5
     ir_const_2 = 1.438833
 
@@ -470,12 +464,15 @@ def _ir_calibrate(header, data, irchn):
     return np.ma.masked_array(tb_, np.isnan(tb_))
 
 
-def show(data):
+def show(data, negate=False):
     """Show the stetched data.
     """
     import Image as pil
-    img = pil.fromarray(np.array((data - data.min()) * 255.0 /
-                                 (data.max() - data.min()), np.uint8))
+    data = np.array((data - data.min()) * 255.0 /
+                    (data.max() - data.min()), np.uint8)
+    if negate:
+        data = 255 - data
+    img = pil.fromarray(data)
     img.show()
 
 CASES = {
@@ -492,9 +489,9 @@ if __name__ == "__main__":
     SCENE.read()
     SCENE.calibrate()
     SCENE.navigate()
-    for i_ in ('1', '2', '3A', '3B', '4', '5'):
+    for i_ in AVHRR_CHANNEL_NAMES:
         data_ = SCENE.channels[i_]
         print >> sys.stderr, "%-3s" % i_, \
             "%6.2f%%" % (100.*(float(np.ma.count(data_))/data_.size)), \
             "%6.2f, %6.2f, %6.2f" % (data_.min(), data_.mean(), data_.max())    
-    show(SCENE.channels['1'])
+    show(SCENE.channels['4'], negate=True)
