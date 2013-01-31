b646e1c6b4db4c09da303c123b37d9f487e70766
Martin Raspaud <martin.raspaud@smhi.se>
2013-01-31 14:04:42 +0100

    Add pyresample to mock for doc building.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['Image', 'numpy']
+MOCK_MODULES = ['Image', 'numpy', 'pyresample']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
