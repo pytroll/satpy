dcc86ca850e77f86bfb18cbebbf570e5c0ef5b96
Lars Orum Rasmussen <ras@dmi.dk>
2013-10-29 09:24:03 +0000

    Trying out a chlorophyll product

--- mpop/instruments/viirs.py
+++ mpop/instruments/viirs.py
@@ -469,3 +469,41 @@ class ViirsCompositer(VisirCompositer):
         return img
 
     hr_ir108.prerequisites = set(["I05"])
+
+    def chlorophyll(self, stretch=None):
+        """ From http://oceancolor.gsfc.nasa.gov/REPROCESSING/R2009/ocv6/
+
+        * Rrs1 = blue wavelength Rrs (e.g., 443, 490, or 510-nm)
+        * Rrs2 = green wavelength Rrs (e.g., 547, 555, or 565-nm)
+        * X = log10(Rrs1 / Rrs2)
+        * chlor_a = 10^(a0 + a1*X + a2*X^2 + a3*X^3 + a4*X^4)
+        
+        sensor  default *      blue     green   a0       a1      a2       a3       a4
+        OC3V    VIIRS   Y      443>486  550     0.2228  -2.4683  1.5867  -0.4275  -0.7768
+
+        blue: M02(445)>M03(488)
+        green: M04(555)
+
+        * X = log10(max(M2, M3)/M4)
+        """
+        self.check_channels("M02", "M03", "M04")
+
+        a0, a1, a2, a3, a4 = (0.2228, -2.4683, 1.5867, -0.4275, -0.7768)
+        
+        #X = np.maximum(self["M02"].data, self["M03"].data)/self["M04"].data
+        X = self["M02"].data/self["M04"].data
+        X = np.log10(X)
+        chlor_a = 10**(a0 + a1*X + a2*(X**2) + a3*(X**3) + a4(*X**4))
+        print 'chlor_a:', chlor_a.min(), chlor_a.mean(), chlor_a.max()
+        
+        img = geo_image.GeoImage(chlor_a,
+                                 self.area,
+                                 self.time_slot,
+                                 fill_value=0,
+                                 mode="L")
+
+        if stretch:
+            img.enhance(stretch=stretch)
+        return img
+
+    chlorophyll.prerequisites = set(["M02", "M03", "M04"])
