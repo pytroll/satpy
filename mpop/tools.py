165457c70d326dc742efb98490abb56c08ae430a
Panu Lahtinen <pnuu+git@iki.fi>
2014-02-24 12:57:46 +0200

    Sun zenith angle correction split into two functions.

--- mpop/tools.py
+++ mpop/tools.py
@@ -22,65 +22,27 @@
 '''Helper functions for eg. performing Sun zenith angle correction.
 '''
 
-import copy
 import numpy as np
-from mpop.logger import LOG
 
-def sunzen_corr(chan, time_slot, lonlats=None, limit=80., mode='cos'):
-
-    '''Perform Sun zenith angle correction to the given channel
-    (*chan*), and return the corrected data as a new channel.  The
-    parameter *limit* can be used to set the maximum zenith angle for
-    which the correction is calculated.  For larger angles, the
-    correction is the same as at the *limit* (default: 80.0 degrees).
-    Coordinate values can be given as a 2-tuple or a two-element list
-    *lonlats* of numpy arrays; if None, the coordinates will be read
-    from the channel data.  Parameter *mode* is a placeholder for
-    other possible zenith angle corrections.  The name of the new
-    channel will be *original_chan.name+'_SZC'*, eg. "VIS006_SZC".
-    This name is also stored to the chan.info dictionary of the
-    originating channel.
+def sunzen_corr_cos(data, cos_zen, limit=80.):
+    '''Perform Sun zenith angle correction to the given *data* using
+    cosine of the zenith angle (*cos_zen*).  The correction is limited
+    to *limit* degrees (default: 80.0 degrees).  For larger zenith
+    angles, the correction is the same as at the *limit*.  Both *data*
+    and *cos_zen* are given either as Numpy arrays or as Numpy
+    MaskedArrays, and they should have equal dimensions.
     '''
     
-    try:
-        from pyorbital import astronomy
-    except ImportError:
-        LOG.warning("Could not load pyorbital.astronomy")
-        return None
-
-    if lonlats is None or len(lonlats) != 2:
-        # Read coordinates
-        LOG.debug("No valid coordinates given, reading from the channel data")
-        lons, lats = chan.area.get_lonlats()
-    else:
-        lons, lats = lonlats
-    
-    # Calculate cosine of Sun zenith angle
-    cos_zen = astronomy.cos_zen(time_slot, lons, lats)
-
     # Convert the zenith angle limit to cosine of zenith angle
     limit = np.cos(np.radians(limit))
 
-    # Copy the channel
-    new_ch = copy.deepcopy(chan)
-    # Set the channel name
-    new_ch.name += '_SZC'
+    # Cosine correction
+    lim_y, lim_x = np.where(cos_zen > limit)
+    data[lim_y, lim_x] /= cos_zen[lim_y, lim_x]
+    # Use constant value (the limit) for larger zenith
+    # angles
+    lim_y, lim_x = np.where(cos_zen <= limit)
+    data[lim_y, lim_x] /= limit
 
-    if mode == 'cos':
-        # Cosine correction
-        lim_y, lim_x = np.where(cos_zen > limit)
-        new_ch.data[lim_y, lim_x] /= cos_zen[lim_y, lim_x]
-        # Use constant value (the limit) for larger zenith
-        # angles
-        lim_y, lim_x = np.where(cos_zen <= limit)
-        new_ch.data[lim_y, lim_x] /= limit
-    else:
-        # placeholder for other corrections
-        pass
-
-    # Add information about the corrected version to original
-    # channel
-    chan.info["sun_zen_corrected"] = new_ch.name
-
-    return new_ch
+    return data
 
