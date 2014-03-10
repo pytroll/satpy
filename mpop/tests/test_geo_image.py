8fc173c661968009d694ff20fc473e5639da89d0
Martin Raspaud <martin.raspaud@smhi.se>
2014-03-10 19:49:37 +0100

    Add tests for geo_image.
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- mpop/tests/test_geo_image.py
+++ mpop/tests/test_geo_image.py
@@ -2,7 +2,7 @@
 """
 import datetime
 import unittest
-import os
+from mock import patch, MagicMock
 
 import numpy as np
 
@@ -16,8 +16,10 @@ class TestGeoImage(unittest.TestCase):
         """Setup the test.
         """
         time_slot = datetime.datetime(2009, 10, 8, 14, 30)
-        self.img = geo_image.GeoImage(np.zeros((512, 512), dtype=np.uint8),
-                                      area="euro", time_slot=time_slot)
+        self.data = np.zeros((512, 512), dtype=np.uint8)
+        self.img = geo_image.GeoImage(self.data,
+                                      area="euro",
+                                      time_slot=time_slot)
         
 
 #     def test_add_overlay(self):
@@ -37,79 +39,66 @@ class TestGeoImage(unittest.TestCase):
 #                           [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
         
 #         self.assert_(np.all(self.img.channels[0][300:310, 300:310] == model))
+    @patch.object(geo_image.GeoImage, 'geotiff_save')
+    def test_save(self, mock_save):
+        """Save a geo image.
+        """
+
         
-    def test_save_geotiff(self):
+        #assert geo_image.GeoImage.geotiff_save == mock_save
+        #self.assertEquals(self.img.geotiff_save, mock_save)
+        self.img.save("test.tif", compression=0)
+        mock_save.assert_called_once_with("test.tif", 0, None, None, 256)
+        mock_save.reset_mock()
+        self.img.save("test.tif", compression=9)
+        mock_save.assert_called_once_with("test.tif", 9, None, None, 256)
+        mock_save.reset_mock()
+        self.img.save("test.tif", compression=9, floating_point=True)
+        mock_save.assert_called_once_with("test.tif", 9, None, None, 256,
+                                          floating_point=True)
+        
+    @patch('osgeo.gdal.GDT_Float64')
+    @patch('osgeo.gdal.GDT_Byte')
+    @patch('osgeo.gdal.GDT_UInt16')
+    @patch('osgeo.gdal.GDT_UInt32')
+    @patch('osgeo.gdal.GetDriverByName')
+    @patch.object(geo_image.GeoImage, '_gdal_write_channels')
+    def test_save_geotiff(self, mock_write_channels, gtbn, gui32, gui16, gby, gf):
         """Save to geotiff format.
         """
-        # self.img.save("test.tif", compression = 0)
-        # self.assertEqual(str(os.popen("tiffdump test.tif").read()),
-        #                  "test.tif:\n"
-        #                  "Magic: 0x4949 <little-endian> Version: 0x2a\n"
-        #                  "Directory 0: offset 524688 (0x80190) next 0 (0)\n"
-        #                  "ImageWidth (256) SHORT (3) 1<512>\n"
-        #                  "ImageLength (257) SHORT (3) 1<512>\n"
-        #                  "BitsPerSample (258) SHORT (3) 2<8 8>\n"
-        #                  "Compression (259) SHORT (3) 1<1>\n"
-        #                  "Photometric (262) SHORT (3) 1<1>\n"
-        #                  "SamplesPerPixel (277) SHORT (3) 1<2>\n"
-        #                  "PlanarConfig (284) SHORT (3) 1<1>\n"
-        #                  "DateTime (306) ASCII (2) 20<2009:10:08 14:30:00\\0>\n"
-        #                  "TileWidth (322) SHORT (3) 1<256>\n"
-        #                  "TileLength (323) SHORT (3) 1<256>\n"
-        #                  "TileOffsets (324) LONG (4) 4<400 131472 262544 393616"
-        #                  ">\n"
-        #                  "TileByteCounts (325) LONG (4) 4<131072 131072 131072 "
-        #                  "131072>\n"
-        #                  "ExtraSamples (338) SHORT (3) 1<0>\n"
-        #                  "SampleFormat (339) SHORT (3) 2<1 1>\n"
-        #                  "33550 (0x830e) DOUBLE (12) 3<8000 8000 0>\n"
-        #                  "33922 (0x8482) DOUBLE (12) 6<0 0 0 -2.71718e+06 -1.47"
-        #                  "505e+06 0>\n"
-        #                  "34735 (0x87af) SHORT (3) 64<1 1 0 15 1024 0 1 1 1025 "
-        #                  "0 1 1 1026 34737 8 0 2048 0 1 4326 2049 34737 7 8 ..."
-        #                  ">\n"
-        #                  "34736 (0x87b0) DOUBLE (12) 5<60 14 1 0 0>\n"
-        #                  "34737 (0x87b1) ASCII (2) 16<unnamed|WGS 84|\\0>\n"
-        #                  "42112 (0xa480) ASCII (2) 154<<GDALMetadata>\\n  <Item"
-        #                  " n ...>\n")
+        #gtbn.Create = MagicMock()
+        self.img.geotiff_save("test.tif", 0, None, None, 256)
+        gtbn.assert_called_once_with("GTiff")
+        ds_dst, data, opacity, fill_value = mock_write_channels.call_args[0]
+        raster_call_args = gtbn.mock_calls[1][1]
+        raster_call_kw = gtbn.mock_calls[1][2]
+        #print raster_call_args
+        #print raster_call_kw
+        self.assertEquals(raster_call_args[0], "test.tif")
+        self.assertEquals(tuple(raster_call_args[1:3]), self.data.shape)
+        self.assertEquals(raster_call_args[3], 2)
+
+        self.assertEquals(raster_call_args[5],
+                          ['TILED=YES',
+                           'BLOCKXSIZE=256',
+                           'BLOCKYSIZE=256',
+                           'ALPHA=YES'])
+        #self.assertEquals(raster_call_args[4].name, "GDT_Byte")
+        self.assertTrue(isinstance(ds_dst, MagicMock))
+        self.assertTrue(np.all(data == self.data))
+        self.assertEquals(opacity, 255)
+        self.assertEquals(fill_value, None)
+
+        self.img.geotiff_save("test.tif", 9, None, None, 256)
+        gtbn.assert_called_with("GTiff")
+        self.assertEquals(gtbn.call_count, 2)
+        ds_dst, data, opacity, fill_value = mock_write_channels.call_args[0]
+        self.assertTrue(isinstance(ds_dst, MagicMock))
+        self.assertTrue(np.all(data == self.data))
+        self.assertEquals(opacity, 255)
+        self.assertEquals(fill_value, None)
 
         
-        # os.remove("test.tif")
-
-        # self.img.save("test.tif", compression = 6)
-        # self.assertEqual(os.popen("tiffdump test.tif").read(),
-        #                  "test.tif:\n"
-        #                  "Magic: 0x4949 <little-endian> Version: 0x2a\n"
-        #                  "Directory 0: offset 8 (0x8) next 0 (0)\n"
-        #                  "ImageWidth (256) SHORT (3) 1<512>\n"
-        #                  "ImageLength (257) SHORT (3) 1<512>\n"
-        #                  "BitsPerSample (258) SHORT (3) 2<8 8>\n"
-        #                  "Compression (259) SHORT (3) 1<8>\n"
-        #                  "Photometric (262) SHORT (3) 1<1>\n"
-        #                  "SamplesPerPixel (277) SHORT (3) 1<2>\n"
-        #                  "PlanarConfig (284) SHORT (3) 1<1>\n"
-        #                  "DateTime (306) ASCII (2) 20<2009:10:08 14:30:00\\0>\n"
-        #                  "Predictor (317) SHORT (3) 1<1>\n"
-        #                  "TileWidth (322) SHORT (3) 1<256>\n"
-        #                  "TileLength (323) SHORT (3) 1<256>\n"
-        #                  "TileOffsets (324) LONG (4) 4<703 853 1003 1153>\n"
-        #                  "TileByteCounts (325) LONG (4) 4<150 150 150 150>\n"
-        #                  "ExtraSamples (338) SHORT (3) 1<1>\n"
-        #                  "SampleFormat (339) SHORT (3) 2<1 1>\n"
-        #                  "33550 (0x830e) DOUBLE (12) 3<8000 8000 0>\n"
-        #                  "33922 (0x8482) DOUBLE (12) 6<0 0 0 -2.71718e+06 -1.47"
-        #                  "505e+06 0>\n"
-        #                  "34735 (0x87af) SHORT (3) 88<1 1 0 20 1024 0 1 1 1025 "
-        #                  "0 1 1 1026 34737 6 0 2048 0 1 32767 2049 34737 78 6 "
-        #                  "...>\n"
-        #                  "34736 (0x87b0) DOUBLE (12) 8<60 14 1 0 0 6.3774e+06 "
-        #                  "299.153 0>\n"
-        #                  "34737 (0x87b1) ASCII (2) 85<ps60n|GCS Name = Bessel"
-        #                  "  ...>\n")
-
-
-        # os.remove("test.tif")
-
 
 
 def suite():
