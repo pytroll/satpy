fddb16106e430021b9e66fa8f2b06d2bed51fe70
Panu Lahtinen <pnuu+git@iki.fi>
2014-05-09 15:22:33 +0300

    Test using area definitions instead of definition names

--- mpop/tests/test_satin_helpers.py
+++ mpop/tests/test_satin_helpers.py
@@ -20,10 +20,9 @@
 # mpop.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
-import mpop.satin
 
 '''Integration testing of
- - :mod:`mpop.satin`
+ - :mod:`mpop.satin.helper_functions`
 '''
 
 class TestSatinHelpers(unittest.TestCase):
@@ -32,10 +31,11 @@ class TestSatinHelpers(unittest.TestCase):
     def test_area_def_names_to_extent(self):
         '''Test conversion of area definition names to maximal area
         extent.'''
-        
-        from mpop.satin.helper_functions import area_def_names_to_extent
 
-        # MSG3 proj4 string from 
+        from mpop.satin.helper_functions import area_def_names_to_extent
+        from pyresample.geometry import AreaDefinition
+
+        # MSG3 proj4 string from
         #  xrit.sat.load(..., only_metadata=True).proj4_params
         proj4_str = 'proj=geos lon_0=0.00 lat_0=0.00 ' \
             'a=6378169.00 b=6356583.80 h=35785831.00'
@@ -43,12 +43,32 @@ class TestSatinHelpers(unittest.TestCase):
         # MSG3 maximum extent
         msg_extent = (-5567248.07, -5570248.48, 5570248.48, 5567248.07)
 
-        area_def_names = ['eurol', 'euro4']
+        euro4 = AreaDefinition('euro4', 'Euro 4 km area', 'ps60n',
+                               {'ellps': 'bessel',
+                                'lat_0': '90',
+                                'lat_ts': '60',
+                                'lon_0': '14',
+                                'proj': 'stere'},
+                               1024, 1024,
+                               (-2717181.7304994687, -5571048.14031214,
+                                 1378818.2695005313, -1475048.1403121399))
+
+        eurol = AreaDefinition('eurol', 'Euro 3.0 km area', 'ps60wgs84',
+                               {'ellps': 'WGS84',
+                                'lat_0': '90',
+                                'lat_ts': '60',
+                                'lon_0': '0',
+                                'proj': 'stere'},
+                               2560, 2048,
+                               (-3780000.0, -7644000.0,
+                                 3900000.0, -1500000.0))
+
+        area_defs = [eurol, euro4]
 
         correct_values = [-3182169.2947746729, 1935792.2825874905,
                            3263159.1568166986, 5388866.0573552148]
 
-        max_extent = area_def_names_to_extent(area_def_names,
+        max_extent = area_def_names_to_extent(area_defs,
                                               proj4_str,
                                               msg_extent)
 
@@ -60,11 +80,21 @@ class TestSatinHelpers(unittest.TestCase):
         # so one of the extent values ('right' or 'east' border) is
         # replaced with the default value
 
-        area_def_names = 'afghanistan'
+        afghanistan = AreaDefinition('afghanistan', 'Afghanistan', 'merc',
+                                     {'a': '6370997.0',
+                                      'lat_0': '35',
+                                      'lat_ts': '35',
+                                      'lon_0': '67.5',
+                                      'proj': 'merc'},
+                                     1600, 1600,
+                                     (-1600000.0, 1600000.0,
+                                       1600000.0, 4800000.0))
+
+        area_defs = afghanistan
 
         correct_values = [-5567248.07, -5570248.48, 5570248.48, 5567248.07]
 
-        max_extent = area_def_names_to_extent(area_def_names,
+        max_extent = area_def_names_to_extent(area_defs,
                                               proj4_str,
                                               msg_extent)
 
@@ -79,5 +109,5 @@ def suite():
     loader = unittest.TestLoader()
     mysuite = unittest.TestSuite()
     mysuite.addTest(loader.loadTestsFromTestCase(TestSatinHelpers))
-    
+
     return mysuite
