7649c1ea4a1bee049e0a8e22a0e231c54405bd79
Martin Raspaud <martin.raspaud@smhi.se>
2015-06-21 23:48:26 +0200

    More unittesting.
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- mpop/tests/test_projectable.py
+++ mpop/tests/test_projectable.py
@@ -62,15 +62,20 @@ class TestDataset(unittest.TestCase):
         ref = np.arange(1, 25)
         ds2 = ds + 1
         self.assert_(all((ds + 1).data == ref + 1))
+        self.assert_(all((1 + ds).data == ref + 1))
+        self.assert_(all((ds + ds).data == ref * 2))
         self.assert_(all((ds * 2).data == ref * 2))
         self.assert_(all((2 * ds).data == ref * 2))
-        self.assert_(all((1 + ds).data == ref + 1))
+        self.assert_(all((ds * ds).data == ref ** 2))
         self.assert_(all((ds - 1).data == ref - 1))
         self.assert_(all((1 - ds).data == 1 - ref))
+        self.assert_(all((ds - ds).data == np.zeros_like(ref)))
         self.assert_(all((ds / 2).data == ref / 2))
         self.assert_(all((2 / ds).data == 2 / ref))
+        self.assert_(all((ds / ds).data == np.ones_like(ref)))
         self.assert_(all((-ds).data == -ref))
         self.assert_(all((abs(ds)).data == abs(ref)))
+        self.assert_(all((ds ** 2).data == ref ** 2))
 
 class TestProjectable(unittest.TestCase):
     """
@@ -90,7 +95,7 @@ class TestProjectable(unittest.TestCase):
         self.assertTrue(projectable.Projectable(data=1).is_loaded())
 
     @mock.patch('mpop.projectable.GeoImage')
-    def test_to_image(self, mock_geoimage):
+    def test_to_image_1D(self, mock_geoimage):
         """
         Conversion to image
         """
@@ -98,20 +103,69 @@ class TestProjectable(unittest.TestCase):
         p = projectable.Projectable(np.arange(25))
         self.assertRaises(ValueError, p.to_image)
 
+    @mock.patch('mpop.projectable.GeoImage')
+    def test_to_image_2D(self, mock_geoimage):
+        """
+        Conversion to image
+        """
         # 2D
-        data = np.arange(25).reshape((5,5))
+        data = np.arange(25).reshape((5, 5))
         p = projectable.Projectable(data)
-        img = p.to_image()
+        p.to_image()
         np.testing.assert_array_equal(data, mock_geoimage.call_args[0][0][0])
         mock_geoimage.reset_mock()
 
+    @mock.patch('mpop.projectable.GeoImage')
+    def test_to_image_3D(self, mock_geoimage):
+        """
+        Conversion to image
+        """
         # 3D
         data = np.arange(75).reshape((3, 5, 5))
         p = projectable.Projectable(data)
-        img = p.to_image()
+        p.to_image()
         np.testing.assert_array_equal(data[0], mock_geoimage.call_args[0][0][0])
         np.testing.assert_array_equal(data[1], mock_geoimage.call_args[0][0][1])
         np.testing.assert_array_equal(data[2], mock_geoimage.call_args[0][0][2])
+
+    @mock.patch('mpop.projectable.GeoImage')
+    def test_show_show(self, mock_geoimage):
+        data = np.arange(25).reshape((5, 5))
+        p = projectable.Projectable(data)
+        p.show()
+        self.assertTrue(mock_geoimage.return_value.show.called)
+
+    @mock.patch('mpop.projectable.GeoImage')
+    def test_show_save(self, mock_geoimage):
+        data = np.arange(25).reshape((5, 5))
+        p = projectable.Projectable(data)
+        filename = "whatever"
+        p.show(filename)
+        mock_geoimage.return_value.save.assert_called_once_with(filename)
+
+    @mock.patch('mpop.projectable.resample_kd_tree_nearest')
+    def test_resample_2D(self, mock_resampler):
+        data = np.arange(25).reshape((5, 5))
+        p = projectable.Projectable(data)
+        source_area = "here"
+        destination_area = "there"
+        p.info["area"] = source_area
+        p.resample(destination_area)
+        mock_resampler.assert_called_once_with(source_area, data, destination_area)
+
+    @mock.patch('mpop.projectable.resample_kd_tree_nearest')
+    def test_resample_3D(self, mock_resampler):
+        data = np.arange(75).reshape((3, 5, 5))
+        p = projectable.Projectable(data)
+        source_area = "here"
+        destination_area = "there"
+        p.info["area"] = source_area
+        p.resample(destination_area)
+        self.assertEqual(mock_resampler.call_args[0][0], source_area)
+        np.testing.assert_array_equal(np.rollaxis(data, 0, 3), mock_resampler.call_args[0][1])
+        self.assertEqual(mock_resampler.call_args[0][2], destination_area)
+            #(source_area, np.rollaxis(data, 0, 3), destination_area)
+
 
 def suite():
     """The test suite for test_projector.
