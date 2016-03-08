54b3a211f4778931a5b095d7385a86c83dbb38ce
Martin Raspaud <martin.raspaud@smhi.se>
2016-03-08 13:12:39 +0100

    Mock trollsift.parser for documentation building
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,7 +40,8 @@ class Mock(object):
 
 MOCK_MODULES = ['Image', 'pyhdf.SD', 'pyhdf.error', 'numpy', 'numpy.core',
                 'numpy.core.multiarray', 'pyresample', 'pyresample.utils',
-                'pyresample.geometry', 'pyresample.kd_tree', 'h5py', 'trollsift']
+                'pyresample.geometry', 'pyresample.kd_tree', 'h5py',
+                'trollsift', 'trollsift.parser']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
