b06ce5af79b05dee13cdd1b432e2e989aaf0bd26
Martin Raspaud <martin.raspaud@smhi.se>
2016-06-22 16:49:37 +0200

    Update the yamlbased aapp reader
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- satpy/readers/aapp_l1b.py
+++ satpy/readers/aapp_l1b.py
@@ -36,187 +36,20 @@ http://research.metoffice.gov.uk/research/interproj/nwpsaf/aapp/
 NWPSAF-MF-UD-003_Formats.pdf
 """
 
-import numpy as np
-from datetime import datetime
-
-
-import yaml
-from trollsift.parser import globify
-import numbers
-from satpy.readers import DatasetID
-import glob
-import os
-from datetime import timedelta
-from satpy.projectable import Projectable
-from pyresample.geometry import SwathDefinition
-
 import logging
+from datetime import datetime, timedelta
+
+import numpy as np
+
+from pyresample.geometry import SwathDefinition
+from satpy.projectable import Projectable
+from satpy.readers.yaml_reader import SatFileHandler
 
 logger = logging.getLogger(__name__)
 
 
-
-class YAMLBasedReader(object):
-    def __init__(self, config_files):
-        self.config = {}
-        for config_file in config_files:
-            with open(config_file) as fd:
-                self.config.update(yaml.load(fd))
-        self.datasets = self.config['datasets']
-        self.ids = {}
-        self.get_dataset_ids()
-
-    def find_filenames(self, directory, file_patterns=None):
-        if file_patterns is None:
-            file_patterns = []
-            file_patterns.extend(item['file_patterns'] for item in self.config['file_types'])
-        filelist = []
-        for pattern in file_patterns:
-            filelist.extend(glob.glob(os.path.join(directory, globify(pattern))))
-        return filelist
-
-    def get_dataset_ids(self):
-        """Get the dataset ids from the config."""
-        ids = []
-        for dskey, dataset in self.datasets.items():
-            nb_ids = 1
-            for key, val in dataset.items():
-                if not key.endswith('range') and isinstance(val, (list, tuple, set)):
-                    nb_ids = len(val)
-                    break
-            for idx in range(nb_ids):
-                kwargs = {'name': dataset.get('name'),
-                          'wavelength': tuple(dataset.get('wavelength_range'))}
-                for key in ['resolution', 'calibration', 'polarization']:
-                    kwargs[key] = dataset.get(key)
-                    if isinstance(kwargs[key], (list, tuple, set)):
-                        kwargs[key] = kwargs[key][idx]
-                dsid = DatasetID(**kwargs)
-                ids.append(dsid)
-                self.ids[dsid] = dskey, dataset
-        return ids
-
-    def load(self, dataset_keys, base_directory, area=None, start_time=None, end_time=None):
-        loaded_filenames = {}
-        datasets = {}
-        for dataset_key in dataset_keys:
-            dsid = self.get_dataset_key(dataset_key)
-            filetype = self.ids[dsid][1]['file_type']
-            filenames = self.find_filenames(base_directory, self.config['file_types'][filetype]['file_patterns'])
-            for filename in filenames:
-                if filename in loaded_filenames:
-                    fhd = loaded_filenames[filename]
-                else:
-                    fhd = self.config['file_types'][filetype]['file_reader'](filename)
-                    loaded_filenames[filename] = fhd
-                datasets.setdefault(dsid, []).append(fhd)
-        return multiload(datasets, area, start_time, end_time)
-
-    def get_dataset_key(self, key, calibration=None, resolution=None, polarization=None, aslist=False):
-        """Get the fully qualified dataset corresponding to *key*, either by name or centerwavelength.
-
-        If `key` is a `DatasetID` object its name is searched if it exists, otherwise its wavelength is used.
-        """
-        # TODO This can be made simpler
-        # get by wavelength
-        if isinstance(key, numbers.Number):
-            datasets = [ds for ds in self.ids if ds.wavelength and (ds.wavelength[0] <= key <= ds.wavelength[2])]
-            datasets = sorted(datasets, key=lambda ch: abs(ch.wavelength[1] - key))
-
-            if not datasets:
-                raise KeyError("Can't find any projectable at %gum" % key)
-        elif isinstance(key, DatasetID):
-            if key.name is not None:
-                datasets = self.get_dataset_key(key.name, aslist=True)
-            elif key.wavelength is not None:
-                datasets = self.get_dataset_key(key.wavelength, aslist=True)
-            else:
-                raise KeyError("Can't find any projectable '{}'".format(key))
-
-            if calibration is None:
-                calibration = [key.calibration]
-            if resolution is None:
-                resolution = [key.resolution]
-            if polarization is None:
-                polarization = [key.polarization]
-        # get by name
-        else:
-            datasets = [ds_id for ds_id in self.ids if ds_id.name == key]
-            if not datasets:
-                raise KeyError("Can't find any projectable called '{}'".format(key))
-
-        # default calibration choices
-        if calibration is None:
-            calibration = ["brightness_temperature", "reflectance"]
-
-        if resolution is not None:
-            if not isinstance(resolution, (tuple, list, set)):
-                resolution = [resolution]
-            datasets = [ds_id for ds_id in datasets if ds_id.resolution in resolution]
-        if calibration is not None:
-            # order calibration from highest level to lowest level
-            calibration = [x for x in ["brightness_temperature", "reflectance", "radiance", "counts"] if x in calibration]
-            datasets = [ds_id for ds_id in datasets if ds_id.calibration is None or ds_id.calibration in calibration]
-        if polarization is not None:
-            datasets = [ds_id for ds_id in datasets if ds_id.polarization in polarization]
-
-        if not datasets:
-            raise KeyError("Can't find any projectable matching '{}'".format(str(key)))
-        if aslist:
-            return datasets
-        else:
-            return datasets[0]
-
-# what about the metadata ?
-def multiload(datasets, area, start_time, end_time):
-    files = {}
-    # select files to use
-    trimmed_datasets = datasets.copy()
-    for dataset, fhds in trimmed_datasets.items():
-        for fhd in reversed(fhds):
-            remove = False
-            if start_time and fhd.start_time() < start_time:
-                remove = True
-            if end_time and fhd.end_time() > end_time:
-                remove = True
-            # TODO: area-based selection
-            if remove:
-                fhds.remove(fhd)
-            else:
-                files.setdefault(fhd, []).append(dataset)
-
-    # TODO concatenate granules
-    res = []
-    for fhd, datasets in files.items():
-        res.extend(fhd.load(datasets))
-
-    # sort datasets by ds_id and time
-
-    datasets = {}
-    for dataset in res:
-        datasets.setdefault(dataset.info['id'], []).append(dataset)
-    print datasets
-
-    res = [np.ma.vstack(dss) for dss in datasets.values()]
-
-    return res
-
-
-#what about file pattern and config ?
-class SatFileHandler(object):
-    def __init__(self, filename):
-        self.filename = filename
-
-    def get_shape(self, dataset_id):
-        raise NotImplementedError
-
-    def start_time(self):
-        raise NotImplementedError
-
-    def end_time(self):
-        raise NotImplementedError
-
 class AVHRRAAPPL1BFile(SatFileHandler):
+
     def __init__(self, filename):
         super(AVHRRAAPPL1BFile, self).__init__(filename)
         self.channels = {i: None for i in AVHRR_CHANNEL_NAMES}
@@ -235,7 +68,7 @@ class AVHRRAAPPL1BFile(SatFileHandler):
 
     def end_time(self):
         return datetime(self._data['scnlinyr'][-1], 1, 1) + timedelta(days=int(self._data['scnlindy'][-1]) - 1,
-                                                                     milliseconds=int(self._data['scnlintime'][-1]))
+                                                                      milliseconds=int(self._data['scnlintime'][-1]))
 
     def shape(self):
         return self._data.shape
@@ -319,7 +152,7 @@ class AVHRRAAPPL1BFile(SatFileHandler):
             # Is it 3a or 3b:
             is3b = np.expand_dims(np.bitwise_and(
                 np.right_shift(self._data['scnlinbit'], 0), 1) == 1, 1)
-            self._is3b = is3b
+            self._is3b = np.repeat(is3b[:, np.newaxis], self._data['hrpt'][0].shape[0], axis=1)
 
         for idx, name in enumerate(['1', '2', '3a']):
             if name in chns:
@@ -327,7 +160,7 @@ class AVHRRAAPPL1BFile(SatFileHandler):
                 # FIXME data should be masked before calibration
                 ds = Projectable(_vis_calibrate(self._data, idx,
                                                 chns[name].calibration, pre_launch_coeffs,
-                                                coeffs, mask=(name == '3a' and is3b)),
+                                                coeffs, mask=(name == '3a' and self._is3b)),
                                  units=units[chns[name].calibration],
                                  id=chns[name],
                                  **chns[name]._asdict())
@@ -337,7 +170,7 @@ class AVHRRAAPPL1BFile(SatFileHandler):
             if name in chns:
                 ds = Projectable(_ir_calibrate(self._header, self._data, idx,
                                                chns[name].calibration,
-                                               mask=(name == '3b' and not is3b)),
+                                               mask=(name == '3b' and (np.logical_not(self._is3b)))),
                                  units=units[chns[name].calibration],
                                  id=chns[name],
                                  **chns[name]._asdict())
@@ -346,7 +179,6 @@ class AVHRRAAPPL1BFile(SatFileHandler):
         logger.debug("Calibration time %s", str(datetime.now() - tic))
 
         return res
-
 
 
 AVHRR_CHANNEL_NAMES = ("1", "2", "3a", "3b", "4", "5")
@@ -680,7 +512,6 @@ if __name__ == '__main__':
         arr = a__ * 1.0
         arr = (arr - arr.min()) * 255.0 / (arr.max() - arr.min())
         return arr.astype(np.uint8)
-
 
     def show(a__):
         """show array.
