652ae1bcb5a6b3662269d5d28f890b87c42e601c
davidh-ssec <david.hoese@ssec.wisc.edu>
2016-08-25 11:35:34 -0500

    Add initial AMSR2 L1B reader (yaml)

---
+++ satpy/readers/amsr2_l1b.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Reader for AMSR2 L1B files in HDF5 format.
+"""
+
+import numpy as np
+from satpy.readers.hdf5_utils import HDF5FileHandler
+from satpy.projectable import Projectable
+
+
+class AMSR2L1BFileHandler(HDF5FileHandler):
+    def start_time(self):
+        return self.filename_info["start_time"]
+
+    def end_time(self):
+        return self.filename_info["start_time"]
+
+    def get_area(self, nav_name, nav_info, lon_out, lat_out):
+        lon_key = nav_info["longitude_key"]
+        lat_key = nav_info["latitude_key"]
+
+        # FIXME: Lower frequency channels need CoRegistration parameters applied
+        if self[lon_key].shape[1] > lon_out.shape[1]:
+            lon_data = self[lon_key][:, ::2]
+            lat_data = self[lat_key][:, ::2]
+        else:
+            lon_data = self[lon_key]
+            lat_data = self[lat_key]
+
+        fill_value = nav_info.get("fill_value", -9999.)
+        lon_out[:] = np.ma.masked_array(lon_data, mask=lon_data == fill_value)
+        lat_out[:] = np.ma.masked_array(lat_data, mask=lat_data == fill_value)
+
+        return {}
+
+    def get_metadata(self, m_key):
+        raise NotImplementedError()
+
+    def get_dataset(self, ds_key, ds_info, out=None):
+        var_path = ds_info["file_key"]
+        fill_value = ds_info.get("fill_value", 65535)
+
+        data = self[var_path]
+        dtype = ds_info.get("dtype", np.float32)
+        mask = data == fill_value
+        data = data.astype(dtype) * self[var_path + "/attr/SCALE FACTOR"]
+        ds_info.update({
+            "name": ds_key.name,
+            "id": ds_key,
+            "units": self[var_path + "/attr/UNIT"],
+        })
+        return Projectable(data, mask=mask, **ds_info)
