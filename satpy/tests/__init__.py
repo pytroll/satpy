e7c45dd9666793fe730d0e5236a8804d79b700da
Martin Raspaud <martin.raspaud@smhi.se>
2016-09-20 08:38:13 +0200

    Deactivate viirssdr testing while migrating to yaml
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- satpy/tests/__init__.py
+++ satpy/tests/__init__.py
@@ -19,32 +19,19 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 """The tests package.
 """
 
 import sys
+
+from satpy.tests import test_readers  # test_viirs_sdr,
+from satpy.tests import (test_projectable, test_resample, test_scene,
+                         test_writers)
+
 if sys.version_info < (2, 7):
     import unittest2 as unittest
 else:
     import unittest
-from satpy.tests import (#test_satin_helpers,
-                        #test_pp_core, # crash
-                        #test_channel,
-                        #test_image,
-                        #test_geo_image,
-                        #test_mipp,
-                        test_projectable,
-                        #test_satellites,
-                        test_scene,
-                        test_writers,
-                        test_readers,
-                        test_resample,
-                        #test_seviri,
-                        test_viirs_sdr,
-                        #test_visir,
-                        )
-
 
 
 def suite():
@@ -52,18 +39,8 @@ def suite():
     """
 
     mysuite = unittest.TestSuite()
-    #mysuite.addTests(test_satin_helpers.suite())
-    #mysuite.addTests(test_pp_core.suite())
-    #mysuite.addTests(test_channel.suite())
-    #mysuite.addTests(test_image.suite())
-    #mysuite.addTests(test_geo_image.suite())
-    #mysuite.addTests(test_mipp.suite())
-    #mysuite.addTests(test_projector.suite())
-    #mysuite.addTests(test_satellites.suite())
     mysuite.addTests(test_scene.suite())
-    #mysuite.addTests(test_seviri.suite())
-    mysuite.addTests(test_viirs_sdr.suite())
-    #mysuite.addTests(test_visir.suite())
+    #mysuite.addTests(test_viirs_sdr.suite())
     mysuite.addTests(test_projectable.suite())
     mysuite.addTests(test_writers.suite())
     mysuite.addTests(test_readers.suite())
