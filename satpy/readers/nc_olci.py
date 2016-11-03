e6d36c9c0b06b7c5f1f715340c3ab971c39a0652
Martin Raspaud <martin.raspaud@smhi.se>
2016-11-03 19:36:43 +0100

    Adjust OLCI reader for reflectance calibration

--- satpy/readers/nc_olci.py
+++ satpy/readers/nc_olci.py
@@ -23,6 +23,7 @@
 """
 
 import logging
+import os
 from datetime import datetime
 
 import numpy as np
@@ -83,6 +84,9 @@ class NCOLCI1B(BaseFileHandler):
                                        filetype_info)
         self.nc = h5netcdf.File(filename, 'r')
         self.channel = filename_info['dataset_name']
+        cal_file = os.path.join(os.path.dirname(
+            filename), 'instrument_data.nc')
+        self.cal = h5netcdf.File(cal_file, 'r')
 
     def get_dataset(self, key, info):
         """Load a dataset
@@ -91,11 +95,26 @@ class NCOLCI1B(BaseFileHandler):
             return
         logger.debug('Reading %s.', key.name)
         variable = self.nc[self.channel + '_radiance']
-        proj = Projectable(np.ma.masked_equal(variable[:],
-                                              variable.attrs['_FillValue']) *
-                           variable.attrs['scale_factor'] +
-                           variable.attrs['add_offset'],
-                           units=variable.attrs['units'])
+
+        radiances = (np.ma.masked_equal(variable[:],
+                                        variable.attrs['_FillValue'], copy=False) *
+                     variable.attrs['scale_factor'] +
+                     variable.attrs['add_offset'])
+        units = variable.attrs['units']
+        if key.calibration == 'reflectance':
+            solar_flux = self.cal['solar_flux'][:]
+            d_index = np.ma.masked_equal(self.cal['detector_index'][:],
+                                         self.cal['detector_index'].attrs[
+                                             '_FillValue'],
+                                         copy=False)
+            idx = int(key.name[2:]) - 1
+            radiances /= solar_flux[idx, d_index]
+            radiances *= np.pi * 100
+            units = '%'
+
+        proj = Projectable(radiances,
+                           copy=False,
+                           units=units)
         return proj
 
     @property
