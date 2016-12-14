b3ed8490243519a98fb1a2ac7e820d61694f307b
Martin Raspaud <martin.raspaud@smhi.se>
2016-12-14 13:38:49 +0100

    Initialize angles in epsl1b reader
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- satpy/readers/eps_l1b.py
+++ satpy/readers/eps_l1b.py
@@ -116,6 +116,7 @@ class EPSAVHRRFile(BaseFileHandler):
             filename, filename_info, filetype_info)
 
         self.lons, self.lats = None, None
+        self.sun_azi, self.sun_zen, self.sat_azi, self.sat_zen = None, None, None, None
         self.area = None
         self.three_a_mask, self.three_b_mask = None, None
         self._start_time = filename_info['start_time']
