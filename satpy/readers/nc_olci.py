5a6215b2100cb2647709b98adeefddc4959c3db1
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2016-12-17 14:24:44 +0100

    editorials
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- satpy/readers/nc_olci.py
+++ satpy/readers/nc_olci.py
@@ -19,7 +19,8 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Compact viirs format.
+
+"""Sentinel-3 OLCI reader
 """
 
 import logging
