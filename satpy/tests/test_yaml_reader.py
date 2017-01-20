8ff13736533106ee47be9c13980a4f7f272657a7
Martin Raspaud <martin.raspaud@smhi.se>
2017-01-20 09:01:10 +0100

    Cleanup test_yaml_reader.py
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- satpy/tests/test_yaml_reader.py
+++ satpy/tests/test_yaml_reader.py
@@ -19,8 +19,9 @@
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Testing the yaml_reader module."""
+
 import os
-import sys
 import unittest
 from tempfile import mkdtemp
 
@@ -30,19 +31,30 @@ import satpy.readers.yaml_reader as yr
 
 
 class TestUtils(unittest.TestCase):
+    """Test the utility functions."""
 
     def test_get_filebase(self):
-        pattern = '{mission_id:3s}_OL_{processing_level:1s}_{datatype_id:_<6s}_{start_time:%Y%m%dT%H%M%S}_{end_time:%Y%m%dT%H%M%S}_{creation_time:%Y%m%dT%H%M%S}_{duration:4d}_{cycle:3d}_{relative_orbit:3d}_{frame:4d}_{centre:3s}_{mode:1s}_{timeliness:2s}_{collection:3s}.SEN3/geo_coordinates.nc'
-        filename = '/home/a001673/data/satellite/Sentinel-3/S3A_OL_1_EFR____20161020T081224_20161020T081524_20161020T102406_0179_010_078_2340_SVL_O_NR_002.SEN3/Oa05_radiance.nc'
-        expected = 'S3A_OL_1_EFR____20161020T081224_20161020T081524_20161020T102406_0179_010_078_2340_SVL_O_NR_002.SEN3/Oa05_radiance.nc'
+        """Check the get_filebase function."""
+        pattern = ('{mission_id:3s}_OL_{processing_level:1s}_{datatype_id:_<6s'
+                   '}_{start_time:%Y%m%dT%H%M%S}_{end_time:%Y%m%dT%H%M%S}_{cre'
+                   'ation_time:%Y%m%dT%H%M%S}_{duration:4d}_{cycle:3d}_{relati'
+                   've_orbit:3d}_{frame:4d}_{centre:3s}_{mode:1s}_{timeliness:'
+                   '2s}_{collection:3s}.SEN3/geo_coordinates.nc')
+        filename = ('/home/a001673/data/satellite/Sentinel-3/S3A_OL_1_EFR____2'
+                    '0161020T081224_20161020T081524_20161020T102406_0179_010_0'
+                    '78_2340_SVL_O_NR_002.SEN3/Oa05_radiance.nc')
+        expected = ('S3A_OL_1_EFR____20161020T081224_20161020T081524_20161020T'
+                    '102406_0179_010_078_2340_SVL_O_NR_002.SEN3/Oa05_radiance.'
+                    'nc')
         self.assertEqual(yr.get_filebase(filename, pattern), expected)
 
 
 class TestFileSelection(unittest.TestCase):
+    """Test the file selection methods from FileYAMLReader."""
 
     @patch('satpy.readers.yaml_reader.recursive_dict_update')
     @patch('satpy.readers.yaml_reader.yaml', spec=yr.yaml)
-    def setUp(self, _, rec_up):
+    def setUp(self, _, rec_up):  # pylint: disable=arguments-differ
         """Setup a reader instance with a fake config."""
         patterns = ['a{something:3s}.bla']
         res_dict = {'reader': {'name': 'fake',
@@ -68,7 +80,6 @@ class TestFileSelection(unittest.TestCase):
         """Check select_files_from_directory."""
         filelist = ['a001.bla', 'a002.bla', 'abcd.bla', 'k001.bla', 'a003.bli']
         dpath = mkdtemp()
-
         for fname in filelist:
             with open(os.path.join(dpath, fname), 'w'):
                 pass
@@ -85,8 +96,7 @@ class TestFileSelection(unittest.TestCase):
 
 
 def suite():
-    """The test suite for test_scene.
-    """
+    """The test suite for test_scene."""
     loader = unittest.TestLoader()
     mysuite = unittest.TestSuite()
     mysuite.addTest(loader.loadTestsFromTestCase(TestUtils))
