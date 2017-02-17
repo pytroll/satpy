47b116e60436e6f556f1dbac72b75aa20b15c3b6
Martin Raspaud <martin.raspaud@smhi.se>
2017-02-17 18:37:45 +0100

    Add new composites for AHI

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -408,6 +408,18 @@ class CO2Corrector(CompositeBase):
         return proj
 
 
+class DifferenceCompositor(CompositeBase):
+
+    def __call__(self, projectables, nonprojectables=None, **info):
+        if len(projectables) != 2:
+            raise ValueError("Expected 2 datasets, got %d" %
+                             (len(projectables), ))
+        info = combine_info(*projectables)
+        info['name'] = self.info['name']
+
+        return Projectable(projectables[0] - projectables[1], **info)
+
+
 class RGBCompositor(CompositeBase):
 
     def __call__(self, projectables, nonprojectables=None, **info):
