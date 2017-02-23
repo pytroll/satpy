a8dc04077919a77882e33aebbc77840249a503bb
Mikhail Itkin <itkin.m@gmail.com>
2017-02-23 16:11:47 +0100

    CALIOP reader WIP

--- satpy/readers/hdf4_caliopv3.py
+++ satpy/readers/hdf4_caliopv3.py
@@ -11,7 +11,7 @@ import math
 import multiprocessing
 import os.path
 from ConfigParser import ConfigParser
-from datetime import datetime
+from datetime import datetime, timedelta
 from fnmatch import fnmatch
 
 import numpy as np
@@ -29,11 +29,15 @@ logger = logging.getLogger(__name__)
 class HDF4BandReader(BaseFileHandler):
 
     def __init__(self, filename, filename_info, filetype_info):
-        super(HDF4BandReader, self).__init__(self, filename,
+        super(HDF4BandReader, self).__init__(filename,
                                     filename_info, filetype_info)
+        self.lons = None
+        self.lats = None
+        self._start_time = None
+        self._end_time = None
 
         self._start_time = filename_info['start_time']
-        self._end_time = filename_info['end_time']
+        self._end_time = self._start_time + timedelta(minutes=10)
 
         self.filename = filename
         self.get_filehandle()
@@ -48,11 +52,35 @@ class HDF4BandReader(BaseFileHandler):
     def get_dataset(self, key, info):
         """Read data from file and return the corresponding projectables.
         """
-        return None
+
+        if key.name in ['longitude', 'latitude']:
+            logger.debug('Reading coordinate arrays.')
+
+            if self.lons is None or self.lats is None:
+                self.lons, self.lats = self.get_lonlats()
+
+            if key.name == 'latitude':
+                proj = Dataset(self.lats, id=key, **info)
+            else:
+                proj = Dataset(self.lons, id=key, **info)
+
+        else:
+            data = self.get_sds_variable(key.name)
+            proj = Dataset(data, id=key, **info)
+
+        return proj
+
+    def get_sds_variable(self, name):
+        """ Read variable from the HDF4 file
+        """
+        sds_obj = self.filehandle.select(name)
+        data = sds_obj.get()
+        return data
+        
 
     def get_lonlats(self):
-        longitudes = None
-        latitudes = None
+        longitudes = self.get_sds_variable('Longitude')
+        latitudes = self.get_sds_variable('Latitude')
         return longitudes, latitudes
 
     @property
