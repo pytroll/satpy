399aabab097d9925a8fec4696cdccd5d2aaa7a92
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2017-03-30 13:46:48 +0200

    Handle subset of SEVIRI channels Full disk supported only.
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- satpy/readers/msg_base.py
+++ satpy/readers/msg_base.py
@@ -24,7 +24,6 @@
 """
 
 from datetime import datetime, timedelta
-import numpy as np
 
 
 def get_cds_time(days, msecs):
@@ -33,20 +32,3 @@ def get_cds_time(days, msecs):
     """
     return datetime(1958, 1, 1) + timedelta(days=float(days),
                                             milliseconds=float(msecs))
-
-
-def dec10to16(data):
-    """Unpacking the 10 bit data to 16 bit"""
-
-    arr10 = data.astype(np.uint16).flat
-    new_shape = list(data.shape[:-1]) + [(data.shape[-1] * 8) / 10]
-    arr16 = np.zeros(new_shape, dtype=np.uint16)
-    arr16.flat[::4] = np.left_shift(arr10[::5], 2) + \
-        np.right_shift((arr10[1::5]), 6)
-    arr16.flat[1::4] = np.left_shift((arr10[1::5] & 63), 4) + \
-        np.right_shift((arr10[2::5]), 4)
-    arr16.flat[2::4] = np.left_shift(arr10[2::5] & 15, 6) + \
-        np.right_shift((arr10[3::5]), 2)
-    arr16.flat[3::4] = np.left_shift(arr10[3::5] & 3, 8) + \
-        arr10[4::5]
-    return arr16
