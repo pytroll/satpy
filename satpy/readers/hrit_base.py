5add63a6ba494357d9abb9e5c6831dfb9374647b
Martin Raspaud <martin.raspaud@smhi.se>
2017-03-31 08:25:23 +0200

    Add support for LRIT 8 bits

--- satpy/readers/hrit_base.py
+++ satpy/readers/hrit_base.py
@@ -296,7 +296,11 @@ class HRITFileHandler(BaseFileHandler):
             elif self.mda['number_of_bits_per_pixel'] == 16:
                 data = np.fromfile(fp_, dtype='>u2', count=int(np.ceil(
                     self.mda['data_field_length'] / 8.)))
-
+                out.data[:] = data.reshape((self.mda['number_of_lines'],
+                                            self.mda['number_of_columns']))[yslice, xslice] * 1.0
+            elif self.mda['number_of_bits_per_pixel'] == 8:
+                data = np.fromfile(fp_, dtype='>u1', count=int(np.ceil(
+                    self.mda['data_field_length'] / 8.)))
                 out.data[:] = data.reshape((self.mda['number_of_lines'],
                                             self.mda['number_of_columns']))[yslice, xslice] * 1.0
             out.mask[:] = out.data == 0
