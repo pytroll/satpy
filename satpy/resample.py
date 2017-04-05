57d77c9642d6067e8ccd19b10812ed7a8d27a686
Panu Lahtinen <pnuu+git@iki.fi>
2017-04-05 10:11:42 +0300

    Handle datasets with multiple channels

--- satpy/resample.py
+++ satpy/resample.py
@@ -467,13 +467,28 @@ class BilinearResampler(BaseResampler):
         """Resample the given data using bilinear interpolation"""
         del kwargs
 
-        res = np.ma.masked_invalid(
-            get_sample_from_bil_info(data.ravel(),
-                                     self.cache['bilinear_t'],
-                                     self.cache['bilinear_s'],
-                                     self.cache['input_idxs'],
-                                     self.cache['idx_arr'],
-                                     output_shape=self.target_geo_def.shape))
+        target_shape = self.target_geo_def.shape
+        if data.ndim == 3:
+            output_shape = list(target_shape)
+            output_shape.append(data.shape[-1])
+            res = np.zeros(output_shape, dtype=data.dtype)
+            for i in range(data.shape[-1]):
+                res[:, :, i] = \
+                    get_sample_from_bil_info(data[:, :, i].ravel(),
+                                             self.cache['bilinear_t'],
+                                             self.cache['bilinear_s'],
+                                             self.cache['input_idxs'],
+                                             self.cache['idx_arr'],
+                                             output_shape=target_shape)
+
+        else:
+            res = \
+                get_sample_from_bil_info(data.ravel(),
+                                         self.cache['bilinear_t'],
+                                         self.cache['bilinear_s'],
+                                         self.cache['input_idxs'],
+                                         self.cache['idx_arr'],
+                                         output_shape=target_shape)
         res = np.ma.masked_invalid(res)
 
         return res
