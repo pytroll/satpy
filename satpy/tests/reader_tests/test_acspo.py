7a0902dfa1e1b1a3364bf0126eb873bedfe08d84
davidh-ssec <david.hoese@ssec.wisc.edu>
2017-04-17 14:33:40 -0500

    Reduce redundant code in netcdf4 based tests

--- satpy/tests/reader_tests/test_acspo.py
+++ satpy/tests/reader_tests/test_acspo.py
@@ -12,7 +12,7 @@ else:
 import mock
 from datetime import datetime, timedelta
 import numpy as np
-from satpy.readers.netcdf_utils import NetCDF4FileHandler
+from satpy.tests.reader_tests.test_netcdf_utils import FakeNetCDF4FileHandler
 
 DEFAULT_FILE_DTYPE = np.uint16
 DEFAULT_FILE_SHAPE = (10, 300)
@@ -25,78 +25,77 @@ DEFAULT_LON_DATA = np.linspace(5, 45, DEFAULT_FILE_SHAPE[1]).astype(DEFAULT_FILE
 DEFAULT_LON_DATA = np.repeat([DEFAULT_LON_DATA], DEFAULT_FILE_SHAPE[0], axis=0)
 
 
-def get_test_content(filename, filename_info, filetype_info):
-    dt = filename_info.get('start_time', datetime(2016, 1, 1, 12, 0, 0))
-    sat, inst = {
-        'VIIRS_NPP': ('NPP', 'VIIRS'),
-    }[filename_info['sensor_id']]
+class FakeNetCDF4FileHandler(FakeNetCDF4FileHandler):
+    """Swap-in NetCDF4 File Handler"""
+    def get_test_content(self, filename, filename_info, filetype_info):
+        """Mimic reader input file content"""
+        dt = filename_info.get('start_time', datetime(2016, 1, 1, 12, 0, 0))
+        sat, inst = {
+            'VIIRS_NPP': ('NPP', 'VIIRS'),
+        }[filename_info['sensor_id']]
 
-    file_content = {
-        '/attr/platform': sat,
-        '/attr/sensor': inst,
-        '/attr/spatial_resolution': '742 m at nadir',
-        '/attr/time_coverage_start': dt.strftime('%Y%m%dT%H%M%SZ'),
-        '/attr/time_coverage_end': (dt + timedelta(minutes=6)).strftime('%Y%m%dT%H%M%SZ'),
-    }
+        file_content = {
+            '/attr/platform': sat,
+            '/attr/sensor': inst,
+            '/attr/spatial_resolution': '742 m at nadir',
+            '/attr/time_coverage_start': dt.strftime('%Y%m%dT%H%M%SZ'),
+            '/attr/time_coverage_end': (dt + timedelta(minutes=6)).strftime('%Y%m%dT%H%M%SZ'),
+        }
 
-    file_content['lat'] = DEFAULT_LAT_DATA
-    file_content['lat/attr/comment'] = 'Latitude of retrievals'
-    file_content['lat/attr/long_name'] = 'latitude'
-    file_content['lat/attr/standard_name'] = 'latitude'
-    file_content['lat/attr/units'] = 'degrees_north'
-    file_content['lat/attr/valid_min'] = -90.
-    file_content['lat/attr/valid_max'] = 90.
-    file_content['lat/shape'] = DEFAULT_FILE_SHAPE
+        file_content['lat'] = DEFAULT_LAT_DATA
+        file_content['lat/attr/comment'] = 'Latitude of retrievals'
+        file_content['lat/attr/long_name'] = 'latitude'
+        file_content['lat/attr/standard_name'] = 'latitude'
+        file_content['lat/attr/units'] = 'degrees_north'
+        file_content['lat/attr/valid_min'] = -90.
+        file_content['lat/attr/valid_max'] = 90.
+        file_content['lat/shape'] = DEFAULT_FILE_SHAPE
 
-    file_content['lon'] = DEFAULT_LON_DATA
-    file_content['lon/attr/comment'] = 'Longitude of retrievals'
-    file_content['lon/attr/long_name'] = 'longitude'
-    file_content['lon/attr/standard_name'] = 'longitude'
-    file_content['lon/attr/units'] = 'degrees_east'
-    file_content['lon/attr/valid_min'] = -180.
-    file_content['lon/attr/valid_max'] = 180.
-    file_content['lon/shape'] = DEFAULT_FILE_SHAPE
+        file_content['lon'] = DEFAULT_LON_DATA
+        file_content['lon/attr/comment'] = 'Longitude of retrievals'
+        file_content['lon/attr/long_name'] = 'longitude'
+        file_content['lon/attr/standard_name'] = 'longitude'
+        file_content['lon/attr/units'] = 'degrees_east'
+        file_content['lon/attr/valid_min'] = -180.
+        file_content['lon/attr/valid_max'] = 180.
+        file_content['lon/shape'] = DEFAULT_FILE_SHAPE
 
-    for k in ['sea_surface_temperature',
-              'satellite_zenith_angle',
-              'sea_ice_fraction',
-              'wind_speed']:
-        file_content[k] = DEFAULT_FILE_DATA
-        file_content[k + '/attr/scale_factor'] = 1.1
-        file_content[k + '/attr/add_offset'] = 0.1
-        file_content[k + '/attr/units'] = 'some_units'
-        file_content[k + '/attr/comment'] = 'comment'
-        file_content[k + '/attr/standard_name'] = 'standard_name'
-        file_content[k + '/attr/long_name'] = 'long_name'
-        file_content[k + '/attr/valid_min'] = 0
-        file_content[k + '/attr/valid_max'] = 65534
-        file_content[k + '/attr/_FillValue'] = 65534
-        file_content[k + '/shape'] = DEFAULT_FILE_SHAPE
+        for k in ['sea_surface_temperature',
+                  'satellite_zenith_angle',
+                  'sea_ice_fraction',
+                  'wind_speed']:
+            file_content[k] = DEFAULT_FILE_DATA
+            file_content[k + '/attr/scale_factor'] = 1.1
+            file_content[k + '/attr/add_offset'] = 0.1
+            file_content[k + '/attr/units'] = 'some_units'
+            file_content[k + '/attr/comment'] = 'comment'
+            file_content[k + '/attr/standard_name'] = 'standard_name'
+            file_content[k + '/attr/long_name'] = 'long_name'
+            file_content[k + '/attr/valid_min'] = 0
+            file_content[k + '/attr/valid_max'] = 65534
+            file_content[k + '/attr/_FillValue'] = 65534
+            file_content[k + '/shape'] = DEFAULT_FILE_SHAPE
 
-    file_content['l2p_flags'] = np.zeros(
-        (1, DEFAULT_FILE_SHAPE[0], DEFAULT_FILE_SHAPE[1]),
-        dtype=np.uint16)
+        file_content['l2p_flags'] = np.zeros(
+            (1, DEFAULT_FILE_SHAPE[0], DEFAULT_FILE_SHAPE[1]),
+            dtype=np.uint16)
 
-    return file_content
-
-
-class FakeNetCDF4FileHandler(NetCDF4FileHandler):
-    def __init__(self, filename, filename_info, filetype_info, **kwargs):
-        super(NetCDF4FileHandler, self).__init__(filename, filename_info, filetype_info)
-        self.file_content = get_test_content(filename, filename_info, filetype_info)
-        self.file_content.update(kwargs)
+        return file_content
 
 
 class TestACSPOReader(unittest.TestCase):
+    """Test ACSPO Reader"""
     yaml_file = "acspo.yaml"
 
     def setUp(self):
+        """Wrap NetCDF4 file handler with our own fake handler"""
         from satpy.config import config_search_paths
         self.reader_configs = config_search_paths(os.path.join('readers', self.yaml_file))
         self.p = mock.patch('satpy.readers.netcdf_utils.NetCDF4FileHandler', FakeNetCDF4FileHandler)
         self.fake_base_handler = self.p.start()
 
     def tearDown(self):
+        """Stop wrapping the NetCDF4 file handler"""
         self.p.stop()
 
     def test_init(self):
@@ -127,7 +126,7 @@ class TestACSPOReader(unittest.TestCase):
 
 
 def suite():
-    """The test suite for test_viirs_l1b.
+    """The test suite for test_acspo.
     """
     loader = unittest.TestLoader()
     mysuite = unittest.TestSuite()
