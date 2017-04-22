455780e2e3584b1b33ce1b35101ff8b54ccbc40e
davidh-ssec <david.hoese@ssec.wisc.edu>
2017-04-21 21:55:12 -0500

    Attempt to fix AMSR2 L1B reader tests

--- satpy/readers/amsr2_l1b.py
+++ satpy/readers/amsr2_l1b.py
@@ -14,15 +14,17 @@ class AMSR2L1BFileHandler(HDF5FileHandler):
         raise NotImplementedError()
 
     def get_shape(self, ds_id, ds_info):
+        """Get output shape of specified dataset"""
         var_path = ds_info['file_key']
         shape = self[var_path + '/shape']
         if ((ds_info.get('standard_name') == "longitude" or
                      ds_info.get('standard_name') == "latitude") and
                     ds_id.resolution == 10000):
-            return shape[0], shape[1] / 2
+            return shape[0], int(shape[1] / 2)
         return shape
 
     def get_dataset(self, ds_id, ds_info, out=None):
+        """Get output data and metadata of specified dataset"""
         var_path = ds_info['file_key']
         fill_value = ds_info.get('fill_value', 65535)
         dtype = ds_info.get('dtype', np.float32)
