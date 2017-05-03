00b8a04689c45d72311f987c3498ae8e720c482f
Martin Raspaud <martin.raspaud@smhi.se>
2017-05-03 20:24:59 +0200

    Add reducer8 compositor

--- satpy/composites/ahi.py
+++ satpy/composites/ahi.py
@@ -25,7 +25,6 @@
 import logging
 
 from pyresample.geometry import AreaDefinition
-
 from satpy.composites import CompositeBase
 from satpy.dataset import Dataset
 
@@ -88,6 +87,35 @@ class Reducer4(CompositeBase):
         (band,) = projectables
 
         factor = 4
+
+        LOG.info('Reducing datasize by a factor %d.', factor)
+
+        proj = Dataset(band[::factor, ::factor], copy=False, **band.info)
+        # newshape = (band.shape[0] / factor, factor,
+        #            band.shape[1] / factor, factor)
+        # proj = Dataset(band.reshape(newshape).mean(axis=3).mean(axis=1),
+        #               copy=False, **band.info)
+
+        old_area = proj.info['area']
+        proj.info['area'] = AreaDefinition(old_area.area_id,
+                                           old_area.name,
+                                           old_area.proj_id,
+                                           old_area.proj_dict,
+                                           old_area.x_size / factor,
+                                           old_area.y_size / factor,
+                                           old_area.area_extent)
+        proj.info['resolution'] *= factor
+        self.apply_modifier_info(band, proj)
+        return proj
+
+
+class Reducer8(CompositeBase):
+    """Reduce the size of the composite."""
+
+    def __call__(self, projectables, optional_datasets=None, **info):
+        (band,) = projectables
+
+        factor = 8
 
         LOG.info('Reducing datasize by a factor %d.', factor)
 
