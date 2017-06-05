d98e3b4ad3dfd86419a2bc0c75a5dc3550428de7
davidh-ssec <david.hoese@ssec.wisc.edu>
2017-06-05 10:16:21 -0500

    Add CLAVR-x reader

--- satpy/readers/hdf4_utils.py
+++ satpy/readers/hdf4_utils.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2016.
-
+# Copyright (c) 2017.
+#
 # Author(s):
-
+#
 #
 #   David Hoese <david.hoese@ssec.wisc.edu>
 #
-
+#
 # This file is part of satpy.
-
+#
 # satpy is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
-
+#
 # satpy is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
-
-"""Helpers for reading hdf5-based files.
+"""Helpers for reading hdf4-based files.
 
 """
 import logging
-import h5py
+
+from pyhdf.SD import SD, SDC, SDS
 import numpy as np
 import six
 
@@ -35,17 +35,18 @@ from satpy.readers.file_handlers import BaseFileHandler
 LOG = logging.getLogger(__name__)
 
 
-class HDF5FileHandler(BaseFileHandler):
+class HDF4FileHandler(BaseFileHandler):
     """Small class for inspecting a HDF5 file and retrieve its metadata/header data.
     """
 
     def __init__(self, filename, filename_info, filetype_info):
-        super(HDF5FileHandler, self).__init__(filename, filename_info, filetype_info)
+        super(HDF4FileHandler, self).__init__(filename, filename_info, filetype_info)
         self.file_content = {}
-        file_handle = h5py.File(self.filename, 'r')
-        file_handle.visititems(self.collect_metadata)
-        self._collect_attrs('', file_handle.attrs)
-        file_handle.close()
+        file_handle = SD(self.filename, SDC.READ)
+        self._collect_attrs('', file_handle.attributes())
+        for k, v in file_handle.datasets().items():
+            self.collect_metadata(k, file_handle.select(k))
+        del file_handle
 
     def _collect_attrs(self, name, attrs):
         for key, value in six.iteritems(attrs):
@@ -61,16 +62,18 @@ class HDF5FileHandler(BaseFileHandler):
                 self.file_content["{}/attr/{}".format(name, key)] = value
 
     def collect_metadata(self, name, obj):
-        if isinstance(obj, h5py.Dataset):
+        if isinstance(obj, SDS):
             self.file_content[name] = obj
-            self.file_content[name + "/shape"] = obj.shape
-        self._collect_attrs(name, obj.attrs)
+            info = obj.info()
+            self.file_content[name + "/shape"] = info[2] if isinstance(info[2], (int, float)) else tuple(info[2])
+        self._collect_attrs(name, obj.attributes())
 
     def __getitem__(self, key):
         val = self.file_content[key]
-        if isinstance(val, h5py.Dataset):
+        if isinstance(val, SDS):
             # these datasets are closed and inaccessible when the file is closed, need to reopen
-            return h5py.File(self.filename, 'r')[key].value
+            info = val.info()
+            return SD(self.filename, SDC.READ).select(info[0])
         return val
 
     def __contains__(self, item):
