34b7ca7721652d3f1233b712b2bbe293a8fbe1a4
Martin Raspaud <martin.raspaud@smhi.se>
2017-08-22 13:06:39 +0200

    Check area compatibility before merging channels in RGBCompositor

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -435,15 +435,20 @@ class RGBCompositor(CompositeBase):
         if len(projectables) != 3:
             raise ValueError("Expected 3 datasets, got %d" %
                              (len(projectables), ))
+
         try:
             the_data = np.rollaxis(
                 np.ma.dstack([projectable for projectable in projectables]),
                 axis=2)
         except ValueError:
             raise IncompatibleAreas
-        # info = projectables[0].info.copy()
-        # info.update(projectables[1].info)
-        # info.update(projectables[2].info)
+        else:
+            areas = [projectable.info.get('area', None)
+                     for projectable in projectables]
+            areas = filter(lambda x: x is not None, areas)
+            if areas and areas.count(areas[0]) != len(areas):
+                raise IncompatibleAreas
+
         info = combine_info(*projectables)
         info.update(self.info)
         # FIXME: should this be done here ?
