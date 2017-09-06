7a351ff8209a0d87e57c56d2ceaed2e506cdd16a
davidh-ssec <david.hoese@ssec.wisc.edu>
2017-09-06 10:17:54 -0500

    Link libhdf4-alt-dev libraries to original names on travis

--- travis/linux_install.sh
+++ travis/linux_install.sh
@@ -5,5 +5,9 @@ set -ex
 # needed to build Python binding for GDAL
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
+# the libhdf4-alt-dev package puts libraries in a weird place
+# it says so that it doesn't conflict with netcdf...let's see what happens
+sudo ln -s libmfhdfalt.so /usr/lib/libmfhdf.so
+sudo ln -s libdfalt.so /usr/lib/libdf.so
 
 set +ex
