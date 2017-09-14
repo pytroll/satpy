313e7906cb5a08acc35128e7e1171d009e4b92ef
Adam.Dybbroe <adam.dybbroe@smhi.se>
2017-09-14 14:14:45 +0200

    Add unittest for cira_stretch and fix it for numpy >=1.13
    Signed-off-by: Adam.Dybbroe <adam.dybbroe@smhi.se>

--- satpy/tests/__init__.py
+++ satpy/tests/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2014 Martin Raspaud
+# Copyright (c) 2014, 2017 Martin Raspaud
 
 # Author(s):
 
@@ -28,7 +28,9 @@ import sys
 from satpy.tests import (reader_tests, test_dataset, test_file_handlers,
                          test_helper_functions, test_readers, test_resample,
                          test_scene, test_utils, test_writers,
-                         test_yaml_reader, writer_tests)
+                         test_yaml_reader, writer_tests,
+                         test_enhancements)
+
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -53,6 +55,7 @@ def suite():
     mysuite.addTests(writer_tests.suite())
     mysuite.addTests(test_file_handlers.suite())
     mysuite.addTests(test_utils.suite())
+    mysuite.addTests(test_enhancements.suite())
 
     return mysuite
 
