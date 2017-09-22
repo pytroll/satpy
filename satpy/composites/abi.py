Fix ABI true color averaging

satpy/composites/abi.py
@@ -34,7 +34,7 @@ def four_element_average(d):
     """Average every 4 elements (2x2) in a 2D array"""
     rows, cols = d.shape
     new_shape = (int(rows / 2.), 2, int(cols / 2.), 2)
-    return np.mean(d.reshape(new_shape), axis=(1, 3))
+    return np.ma.mean(d.reshape(new_shape), axis=(1, 3))
 
 
 class TrueColor2km(RGBCompositor):
@@ -59,10 +59,10 @@ class TrueColor(RGBCompositor):
         r = c02
         b = np.repeat(np.repeat(c01, 2, axis=0), 2, axis=1)
         c03_high = np.repeat(np.repeat(c03, 2, axis=0), 2, axis=1)
+        g = (b + r) / 2 * 0.93 + 0.07 * c03_high
 
         low_res_red = four_element_average(r)
         low_res_red = np.repeat(np.repeat(low_res_red, 2, axis=0), 2, axis=1)
         ratio = r / low_res_red
-        g = (b + r) / 2 * 0.93 + 0.07 * c03_high
 
         return super(TrueColor, self).__call__((r, g * ratio, b * ratio), **info)