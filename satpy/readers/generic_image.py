5409a29319b0b42a92dcc1bb3df643e3de8c1ab3
lorenzo clementi <lorenzo.clementi@meteoswiss.ch>
2017-09-25 09:01:22 +0200

    Added some doc

--- satpy/readers/generic_image.py
+++ satpy/readers/generic_image.py
@@ -1,3 +1,27 @@
+# Author(s):
+
+#   Lorenzo Clementi <lorenzo.clementi@meteoswiss.ch>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Reader for generic image (e.g. gif, png, jpg).
+
+It returns a dataset without coordinates and calibration.
+"""
+
+
 from PIL import Image
 import numpy as np
 import os
@@ -5,9 +29,6 @@ import logging
 from satpy.dataset import Dataset, DatasetID
 from satpy.readers.file_handlers import BaseFileHandler
 from satpy.readers.yaml_reader import FileYAMLReader
-from pyresample import utils
-from pyresample.geometry import AreaDefinition
-from mpop.projector import get_area_def
 
 logger = logging.getLogger(__name__)
 
