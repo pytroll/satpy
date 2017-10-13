9e2935eefbfddae163691154cdc9462331c2fb10
davidh-ssec <david.hoese@ssec.wisc.edu>
2017-10-12 20:38:38 -0500

    Fix resampling when data mask needs to be applied to geolocation kdtree

--- satpy/resample.py
+++ satpy/resample.py
@@ -168,8 +168,11 @@ class BaseResampler(object):
 
         :param mask_area: Provide data mask to `precompute` method to mask invalid data values in geolocation.
         """
-        if mask_area and hasattr(data, "mask"):
-            kwargs.setdefault("mask", data.mask)
+        if mask_area and hasattr(data, "mask") or hasattr(data, 'isnull'):
+            if hasattr(data, 'mask'):
+                kwargs.setdefault('mask', data.mask)
+            elif hasattr(data, 'isnull'):
+                kwargs.setdefault("mask", data)
         cache_id = self.precompute(cache_dir=cache_dir, **kwargs)
         return self.compute(data, cache_id=cache_id, **kwargs)
 
@@ -621,22 +624,29 @@ def mask_source_lonlats(source_def, mask):
     # the data may have additional masked pixels
     # let's compare them to see if we can use the same area
     # assume lons and lats mask are the same
-    if np.any(mask) and isinstance(source_geo_def, SwathDefinition):
-        # copy the source area and use it for the rest of the calculations
-        LOG.debug("Copying source area to mask invalid dataset points")
-        source_geo_def = deepcopy(source_geo_def)
-        lons, lats = source_geo_def.get_lonlats()
-        if np.ndim(mask) == 3:
-            # FIXME: we should treat 3d arrays (composites) layer by layer!
-            mask = np.sum(mask, axis=2)
-            # FIXME: pyresample doesn't seem to like this
-            # lons = np.tile(lons, (1, 1, mask.shape[2]))
-            # lats = np.tile(lats, (1, 1, mask.shape[2]))
+    if isinstance(source_geo_def, SwathDefinition):
+        if np.issubsctype(mask.dtype, np.bool):
+            # copy the source area and use it for the rest of the calculations
+            LOG.debug("Copying source area to mask invalid dataset points")
+            # source_geo_def = deepcopy(source_geo_def)
+            # lons, lats = source_geo_def.get_lonlats()
+            if np.ndim(mask) == 3:
+                # FIXME: we should treat 3d arrays (composites) layer by layer!
+                mask = np.sum(mask, axis=2)
+                # FIXME: pyresample doesn't seem to like this
+                # lons = np.tile(lons, (1, 1, mask.shape[2]))
+                # lats = np.tile(lats, (1, 1, mask.shape[2]))
 
-        # use the same data, but make a new mask (i.e. don't affect the original masked array)
-        # the ma.array function combines the undelying mask with the new
-        # one (OR)
-        source_geo_def.lons = np.ma.array(lons, mask=mask)
-        source_geo_def.lats = np.ma.array(lats, mask=mask)
+            # use the same data, but make a new mask (i.e. don't affect the original masked array)
+            # the ma.array function combines the undelying mask with the new
+            # one (OR)
+            source_geo_def.lons = source_geo_def.lons.where(mask)
+            source_geo_def.lats = source_geo_def.lats.where(mask)
+            # source_geo_def.lons = np.ma.array(lons, mask=mask)
+            # source_geo_def.lats = np.ma.array(lats, mask=mask)
+        else:
+            import xarray.ufuncs as xu
+            source_geo_def.lons = source_geo_def.lons.where(~xu.isnan(mask))
+            source_geo_def.lats = source_geo_def.lats.where(~xu.isnan(mask))
 
     return source_geo_def
