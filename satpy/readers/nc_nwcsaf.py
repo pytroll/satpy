5bd216d27b368c82101b0fc41d769c6b866d5305
Martin Raspaud <martin.raspaud@smhi.se>
2017-10-16 13:18:22 +0200

    Do scaling and masking by hand in nwcsaf reader

--- satpy/readers/nc_nwcsaf.py
+++ satpy/readers/nc_nwcsaf.py
@@ -28,8 +28,9 @@ import logging
 from datetime import datetime
 
 import numpy as np
-
 import xarray as xr
+
+from pyresample.utils import get_area_def
 from satpy.dataset import Dataset
 from satpy.readers.file_handlers import BaseFileHandler
 
@@ -62,9 +63,10 @@ class NcNWCSAF(BaseFileHandler):
                                        filetype_info)
         self.nc = xr.open_dataset(filename,
                                   decode_cf=True,
-                                  mask_and_scale=True,
+                                  mask_and_scale=False,
                                   engine='h5netcdf',
                                   chunks={'nx': 1000, 'ny': 1000, 'time': 10})
+
         self.nc = self.nc.rename({'nx': 'x', 'ny': 'y'})
 
         self.pps = False
@@ -85,6 +87,28 @@ class NcNWCSAF(BaseFileHandler):
 
         logger.debug('Reading %s.', dsid.name)
         variable = self.nc[dsid.name]
+
+        if 'scale_factor' in variable.attrs or 'add_offset' in variable.attrs:
+            scale = variable.attrs.get('scale_factor', 1)
+            offset = variable.attrs.get('add_offset', 0)
+            if np.issubdtype((scale + offset).dtype, np.floating):
+                if '_FillValue' in variable.attrs:
+                    variable = variable.where(
+                        variable != variable.attrs['_FillValue'])
+                if 'valid_range' in variable.attrs:
+                    variable = variable.where(
+                        variable <= variable.attrs['valid_range'][1])
+                    variable = variable.where(
+                        variable >= variable.attrs['valid_range'][0])
+                if 'valid_max' in variable.attrs:
+                    variable = variable.where(
+                        variable <= variable.attrs['valid_max'])
+                if 'valid_min' in variable.attrs:
+                    variable = variable.where(
+                        variable >= variable.attrs['valid_min'])
+
+            variable = variable * scale + offset
+
         variable.attrs.update({'platform_name': self.platform_name,
                                'sensor': self.sensor})
 
