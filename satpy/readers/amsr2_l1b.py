030dbd63a0d23fe685b70c2bc96579a67995f32d
davidh-ssec <david.hoese@ssec.wisc.edu>
2017-11-22 20:18:34 -0600

    Fix AMRS2 reader to work with xarray

--- satpy/readers/amsr2_l1b.py
+++ satpy/readers/amsr2_l1b.py
@@ -10,8 +10,19 @@ from satpy.readers.hdf5_utils import HDF5FileHandler
 
 
 class AMSR2L1BFileHandler(HDF5FileHandler):
-    def get_metadata(self, m_key):
-        raise NotImplementedError()
+    def get_metadata(self, ds_id, ds_info):
+        var_path = ds_info['file_key']
+        info = getattr(self[var_path], 'attrs', {})
+        info.update(ds_info)
+        info.update({
+            "units": self[var_path + "/attr/UNIT"],
+            "platform": self["/attr/PlatformShortName"].item(),
+            "sensor": self["/attr/SensorShortName"].item(),
+            "start_orbit": int(self["/attr/StartOrbitNumber"].item()),
+            "end_orbit": int(self["/attr/StopOrbitNumber"].item()),
+        })
+        info.update(ds_id.to_dict())
+        return info
 
     def get_shape(self, ds_id, ds_info):
         """Get output shape of specified dataset"""
@@ -23,38 +34,21 @@ class AMSR2L1BFileHandler(HDF5FileHandler):
             return shape[0], int(shape[1] / 2)
         return shape
 
-    def get_dataset(self, ds_id, ds_info, out=None):
+    def get_dataset(self, ds_id, ds_info):
         """Get output data and metadata of specified dataset"""
         var_path = ds_info['file_key']
         fill_value = ds_info.get('fill_value', 65535)
-        dtype = ds_info.get('dtype', np.float32)
-
-        if out is None:
-            shape = self.get_shape(ds_id, ds_info)
-            out = np.ma.empty(shape, dtype=dtype)
-            out.mask = np.zeros(shape, dtype=np.bool)
+        metadata = self.get_metadata(ds_id, ds_info)
 
         data = self[var_path]
         if ((ds_info.get('standard_name') == "longitude" or
              ds_info.get('standard_name') == "latitude") and
                 ds_id.resolution == 10000):
-            # FIXME: Lower frequency channels need CoRegistration parameters
-            # applied
-            out.mask[:] = data[:, ::2] == fill_value
-            out.data[:] = data[:, ::2].astype(dtype) * self[var_path + "/attr/SCALE FACTOR"]
+            # FIXME: Lower frequency channels need CoRegistration parameters applied
+            data = data[:, ::2] * self[var_path + "/attr/SCALE FACTOR"]
         else:
-            out.mask[:] = data == fill_value
-            out.data[:] = data.astype(dtype) * self[var_path + "/attr/SCALE FACTOR"]
+            data = data * self[var_path + "/attr/SCALE FACTOR"]
+        data = data.where(data != fill_value)
+        data.attrs.update(metadata)
+        return data
 
-        i = getattr(out, 'info', {})
-        i.update(ds_info)
-        i.update({
-            "units": self[var_path + "/attr/UNIT"],
-            "platform": self["/attr/PlatformShortName"].item(),
-            "sensor": self["/attr/SensorShortName"].item(),
-            "start_orbit": int(self["/attr/StartOrbitNumber"].item()),
-            "end_orbit": int(self["/attr/StopOrbitNumber"].item()),
-        })
-        i.update(ds_id.to_dict())
-        cls = ds_info.pop("container", Dataset)
-        return cls(out.data, mask=out.mask, copy=False, **i)
