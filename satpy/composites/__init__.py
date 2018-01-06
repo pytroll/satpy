a5fc281bd0b8ce5c0e3a4bf98167be37d7f83fdc
Adam.Dybbroe <adam.dybbroe@smhi.se>
2018-01-06 17:59:03 +0100

    Explain how the 3.x reflectance needs to be derived before getting the emissive part
    Signed-off-by: Adam.Dybbroe <adam.dybbroe@smhi.se>

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -439,6 +439,10 @@ class NIREmissivePartFromReflectance(NIRReflectance):
         Not supposed to be used for wavelength outside [3, 4] µm.
         """
         self._init_refl3x(projectables)
+        # Derive the sun-zenith angles, and use the nir and thermal ir
+        # brightness tempertures and derive the reflectance using
+        # PySpectral. The reflectance is stored internally in PySpectral and
+        # needs to be derived first in order to get the emissive part.
         _ = self._get_reflectance(projectables, optional_datasets)
         _nir, _ = projectables
         proj = Dataset(self._refl3x.emissive_part_3x(), **_nir.info)
