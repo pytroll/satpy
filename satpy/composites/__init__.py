ee3469b847ba72a214e56fd11f98de725895c7c6
Martin Raspaud <martin.raspaud@smhi.se>
2018-01-25 14:51:28 +0100

    Fix CO2Corrector composite

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -536,17 +536,13 @@ class CO2Corrector(CompositeBase):
         LOG.info('Applying CO2 correction')
         dt_co2 = (ir_108 - ir_134) / 4.0
         rcorr = ir_108**4 - (ir_108 - dt_co2)**4
-        t4_co2corr = ir_039**4 + rcorr
-        t4_co2corr = np.ma.where(t4_co2corr > 0.0, t4_co2corr, 0)
-        t4_co2corr = t4_co2corr**0.25
+        t4_co2corr = (ir_039**4 + rcorr).clip(0.0) ** 0.25
 
-        info = ir_039.attrs.copy()
+        t4_co2corr.attrs = ir_039.attrs.copy()
 
-        proj = Dataset(t4_co2corr, mask=t4_co2corr.mask, **info)
+        self.apply_modifier_info(ir_039, t4_co2corr)
 
-        self.apply_modifier_info(ir_039, proj)
-
-        return proj
+        return t4_co2corr
 
 
 class DifferenceCompositor(CompositeBase):
