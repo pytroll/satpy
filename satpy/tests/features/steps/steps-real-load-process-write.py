dac859b3487e62c91023e82c382319e37c4b4ee0
Martin Raspaud <martin.raspaud@smhi.se>
2018-01-25 14:22:15 +0100

    Fix pep8 style

--- satpy/tests/features/steps/steps-real-load-process-write.py
+++ satpy/tests/features/steps/steps-real-load-process-write.py
@@ -23,12 +23,10 @@
 """
 
 import os
-import sys
 from glob import glob
 
-from behave import use_step_matcher, given, when, then
+from behave import given, when, then
 
-from contextlib import contextmanager
 from tempfile import NamedTemporaryFile
 
 import numpy as np
@@ -36,12 +34,13 @@ from PIL import Image
 
 
 def fft_proj_rms(a1, a2):
-    """Compute the RMS of differences between FFT vectors of a1
+    """Compute the RMS of differences between two images.
+
+    Compute the RMS of differences between two FFT vectors of a1
     and projection of FFT vectors of a2.
     This metric is sensitive to large scale changes and image noise but
     insensitive to small rendering differences.
     """
-
     ms = 0
 
     # for i in range(a1.shape[-1]):
@@ -55,8 +54,8 @@ def fft_proj_rms(a1, a2):
     p2 = np.arctan2(fr2.imag, fr2.real)
 
     theta = p2 - p1
-    l = ps2 * np.cos(theta)
-    ms += np.sum(((l - ps1) ** 2)) / float(ps1.size)
+    l_factor = ps2 * np.cos(theta)
+    ms += np.sum(((l_factor - ps1) ** 2)) / float(ps1.size)
 
     rms = np.sqrt(ms)
 
@@ -64,21 +63,24 @@ def fft_proj_rms(a1, a2):
 
 
 def assert_images_match(image1, image2, threshold=0.1):
+    """Assert that images are matching."""
     img1 = np.asarray(Image.open(image1))
     img2 = np.asarray(Image.open(image2))
     rms = fft_proj_rms(img1, img2)
     assert rms <= threshold, "Images {0} and {1} don't match: {2}".format(
         image1, image2, rms)
 
+
 @given(u'{dformat} data is available')
 def step_impl(context, dformat):
     data_path = os.path.join('test_data', dformat)
     data_available = os.path.exists(data_path)
     if not data_available:
-        context.scenario.skip(reason="No {dformat} test data available".format(dformat=dformat))
+        context.scenario.skip(reason="No test data available for " + dformat)
     else:
         context.dformat = dformat
         context.data_path = data_path
+
 
 @when(u'the user loads the {composite} composite')
 def step_impl(context, composite):
@@ -89,16 +91,19 @@ def step_impl(context, composite):
     context.scn = scn
     context.composite = composite
 
+
 @when(u'the user resamples the data to {area}')
 def step_impl(context, area):
     context.lscn = context.scn.resample(area)
     context.area = area
+
 
 @when(u'the user saves the composite to disk')
 def step_impl(context):
     with NamedTemporaryFile(suffix='.png', delete=False) as tmp_file:
         context.lscn.save_dataset(context.composite, filename=tmp_file.name)
         context.new_filename = tmp_file.name
+
 
 @then(u'the resulting image should match the reference image')
 def step_impl(context):
@@ -107,7 +112,9 @@ def step_impl(context):
     assert os.path.exists(ref_filename), "Missing reference file."
     assert_images_match(ref_filename, context.new_filename)
 
+
 def after_scenario(context, scenario):
+    """Run after each scenario."""
     del scenario
     if hasattr(context, 'new_filename'):
         os.remove(context.new_filename)
