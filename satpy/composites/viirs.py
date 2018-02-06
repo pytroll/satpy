0f74df4631207594d49f93189d1cf17a8a49bbc7
davidh-ssec <david.hoese@ssec.wisc.edu>
2018-02-06 10:26:22 -0600

    Remove unused `VIIRSTrueColor` compositor
    The same can be accomplished with the RGBCompositor

--- satpy/composites/viirs.py
+++ satpy/composites/viirs.py
@@ -54,26 +54,6 @@ class VIIRSFog(CompositeBase):
         return fog
 
 
-class VIIRSTrueColor(CompositeBase):
-
-    def __call__(self, projectables, nonprojectables=None, **info):
-        if len(projectables) != 3:
-            raise ValueError("Expected 3 datasets, got %d" %
-                             (len(projectables), ))
-
-        # Collect information that is the same between the projectables
-        info = combine_metadata(*projectables)
-        # Update that information with configured information (including name)
-        info.update(self.attrs)
-        # Force certain pieces of metadata that we *know* to be true
-        info["wavelength"] = None
-        info["mode"] = self.attrs.get("mode", "RGB")
-        return Dataset(data=np.rollaxis(
-            np.ma.dstack([projectable for projectable in projectables]),
-            axis=2),
-            **info)
-
-
 class RatioSharpenedRGB(RGBCompositor):
 
     def __init__(self, *args, **kwargs):
