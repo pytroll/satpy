Fix ABI L1B reader to use 64-bit scaling factors for X/Y variables

satpy/readers/abi_l1b.py
@@ -53,13 +53,32 @@ class NC_ABI_L1B(BaseFileHandler):
         # xarray's default netcdf4 engine
         self.nc = xr.open_dataset(filename,
                                   decode_cf=True,
-                                  mask_and_scale=True,
+                                  mask_and_scale=False,
                                   chunks={'x': CHUNK_SIZE, 'y': CHUNK_SIZE})
         self.nc = self.nc.rename({'t': 'time'})
         platform_shortname = filename_info['platform_shortname']
         self.platform_name = PLATFORM_NAMES.get(platform_shortname)
         self.sensor = 'abi'
         self.nlines, self.ncols = self.nc["Rad"].shape
+
+    def __getitem__(self, item):
+        """Wrapper around `self.nc[item]`.
+
+        Some datasets use a 32-bit float scaling factor like the 'x' and 'y'
+        variables which causes inaccurate unscaled data values. This method
+        forces the scale factor to a 64-bit float first.
+
+        """
+        data = self.nc[item]
+        factor = data.attrs.get('scale_factor')
+        offset = data.attrs.get('add_offset')
+        fill = data.attrs.get('_FillValue')
+        if factor is not None:
+            # make sure the factor is a 64-bit float
+            data = data * float(factor) + offset
+        if fill is not None:
+            data = data.where(data != fill)
+        return data
 
     def get_shape(self, key, info):
         """Get the shape of the data."""
@@ -69,8 +88,7 @@ class NC_ABI_L1B(BaseFileHandler):
                     xslice=slice(None), yslice=slice(None)):
         """Load a dataset."""
         logger.debug('Reading in get_dataset %s.', key.name)
-
-        radiances = self.nc["Rad"][yslice, xslice]  # .expand_dims('time')
+        radiances = self['Rad'][yslice, xslice]
 
         if key.calibration == 'reflectance':
             logger.debug("Calibrating to reflectances")
@@ -88,9 +106,9 @@ class NC_ABI_L1B(BaseFileHandler):
 
         res.attrs.update({'platform_name': self.platform_name,
                           'sensor': self.sensor,
-                          'satellite_latitude': float(self.nc['nominal_satellite_subpoint_lat']),
-                          'satellite_longitude': float(self.nc['nominal_satellite_subpoint_lon']),
-                          'satellite_altitude': float(self.nc['nominal_satellite_height'])})
+                          'satellite_latitude': float(self['nominal_satellite_subpoint_lat']),
+                          'satellite_longitude': float(self['nominal_satellite_subpoint_lon']),
+                          'satellite_altitude': float(self['nominal_satellite_height'])})
         res.attrs.update(key.to_dict())
 
         return res
@@ -107,10 +125,12 @@ class NC_ABI_L1B(BaseFileHandler):
 
         # x and y extents in m
         h = float(h)
-        x_l = h * self.nc['x'][0]
-        x_r = h * self.nc['x'][-1]
-        y_l = h * self.nc['y'][-1]
-        y_u = h * self.nc['y'][0]
+        x = self['x']
+        y = self['y']
+        x_l = h * x[0]
+        x_r = h * x[-1]
+        y_l = h * y[-1]
+        y_u = h * y[0]
         x_half = (x_r - x_l) / (self.ncols - 1) / 2.
         y_half = (y_u - y_l) / (self.nlines - 1) / 2.
         area_extent = (x_l - x_half, y_l - y_half, x_r + x_half, y_u + y_half)
@@ -136,8 +156,8 @@ class NC_ABI_L1B(BaseFileHandler):
 
     def _vis_calibrate(self, data):
         """Calibrate visible channels to reflectance."""
-        solar_irradiance = self.nc['esun']
-        esd = self.nc["earth_sun_distance_anomaly_in_AU"].astype(float)
+        solar_irradiance = self['esun']
+        esd = self["earth_sun_distance_anomaly_in_AU"].astype(float)
 
         factor = np.pi * esd * esd / solar_irradiance
 
@@ -149,10 +169,10 @@ class NC_ABI_L1B(BaseFileHandler):
 
     def _ir_calibrate(self, data):
         """Calibrate IR channels to BT."""
-        fk1 = float(self.nc["planck_fk1"])
-        fk2 = float(self.nc["planck_fk2"])
-        bc1 = float(self.nc["planck_bc1"])
-        bc2 = float(self.nc["planck_bc2"])
+        fk1 = float(self["planck_fk1"])
+        fk2 = float(self["planck_fk2"])
+        bc1 = float(self["planck_bc1"])
+        bc2 = float(self["planck_bc2"])
 
         res = (fk2 / xu.log(fk1 / data + 1) - bc1) / bc2
         res.attrs = data.attrs