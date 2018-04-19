c9bf896339427c4adac16f9a7508bcfd182d0920
davidh-ssec <david.hoese@ssec.wisc.edu>
2018-04-19 08:42:05 -0500

    Add 'python_requires' to setup.py to specify python support
    (cherry picked from commit 8366964)

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(name=NAME,
       zip_safe=False,
       install_requires=requires,
       tests_require=test_requires,
+      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
       extras_require={
           # Readers:
           'xRIT': ['mipp >= 0.6.0'],
