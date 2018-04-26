6bf25ac94a839ab557d29be405f6e664f58d6a68
Sauli Joro <metno@tcevbox-a50626.pc.eumetsat.int>
2018-04-26 13:18:23 +0000

    Move calibration coefficients under msg_base.py
    Signed-off-by: Sauli Joro <metno@tcevbox-a50626.pc.eumetsat.int>

--- satpy/readers/hrit_msg.py
+++ satpy/readers/hrit_msg.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2010-2017
+# Copyright (c) 2010-2017 PyTroll Community
 
 # Author(s):
 
 #   Martin Raspaud <martin.raspaud@smhi.se>
+#   Adam Bybbroe <adam.dybbroe@smhi.se>
+#   Sauli Joro <sauli.joro@eumetsat.int>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -41,6 +43,7 @@ from satpy.readers.hrit_base import (HRITFileHandler, ancillary_text,
                                      image_data_function, make_time_cds_short,
                                      time_cds_short)
 
+from satpy.readers.msg_base import (CHANNEL_NAMES, CALIB, SATNUM, BTFIT)
 import satpy.readers.msg_base as mb
 
 logger = logging.getLogger('hrit_msg')
@@ -599,179 +602,6 @@ class HRITMSGEpilogueFileHandler(HRITFileHandler):
 
         pacqtime['ForwardScanEnd'] = end
         pacqtime['ForwardScanStart'] = start
-
-
-# Reflectance factor for visible bands
-HRV_F = 25.15
-VIS006_F = 20.76
-VIS008_F = 23.30
-IR_016_F = 19.73
-
-SATNUM = {321: "8",
-          322: "9",
-          323: "10",
-          324: "11"}
-
-# Calibration coefficients from
-# 'A Planned Change to the MSG Level 1.5 Image Product Radiance Definition',
-# "Conversion from radiances to reflectances for SEVIRI warm channels"
-# EUM/MET/TEN/12/0332 , and "The Conversion from Effective Radiances to
-# Equivalent Brightness Temperatures" EUM/MET/TEN/11/0569
-
-CALIB = {}
-
-
-# Meteosat 8
-
-CALIB[321] = {'HRV': {'F': 78.7599 / np.pi},
-              'VIS006': {'F': 65.2296 / np.pi},
-              'VIS008': {'F': 73.0127 / np.pi},
-              'IR_016': {'F': 62.3715 / np.pi},
-              'IR_039': {'VC': 2567.33,
-                         'ALPHA': 0.9956,
-                         'BETA': 3.41},
-              'WV_062': {'VC': 1598.103,
-                         'ALPHA': 0.9962,
-                         'BETA': 2.218},
-              'WV_073': {'VC': 1362.081,
-                         'ALPHA': 0.9991,
-                         'BETA': 0.478},
-              'IR_087': {'VC': 1149.069,
-                         'ALPHA': 0.9996,
-                         'BETA': 0.179},
-              'IR_097': {'VC': 1034.343,
-                         'ALPHA': 0.9999,
-                         'BETA': 0.06},
-              'IR_108': {'VC': 930.647,
-                         'ALPHA': 0.9983,
-                         'BETA': 0.625},
-              'IR_120': {'VC': 839.66,
-                         'ALPHA': 0.9988,
-                         'BETA': 0.397},
-              'IR_134': {'VC': 752.387,
-                         'ALPHA': 0.9981,
-                         'BETA': 0.578}}
-
-# Meteosat 9
-
-CALIB[322] = {'HRV': {'F': 79.0113 / np.pi},
-              'VIS006': {'F': 65.2065 / np.pi},
-              'VIS008': {'F': 73.1869 / np.pi},
-              'IR_016': {'F': 61.9923 / np.pi},
-              'IR_039': {'VC': 2568.832,
-                         'ALPHA': 0.9954,
-                         'BETA': 3.438},
-              'WV_062': {'VC': 1600.548,
-                         'ALPHA': 0.9963,
-                         'BETA': 2.185},
-              'WV_073': {'VC': 1360.330,
-                         'ALPHA': 0.9991,
-                         'BETA': 0.47},
-              'IR_087': {'VC': 1148.620,
-                         'ALPHA': 0.9996,
-                         'BETA': 0.179},
-              'IR_097': {'VC': 1035.289,
-                         'ALPHA': 0.9999,
-                         'BETA': 0.056},
-              'IR_108': {'VC': 931.7,
-                         'ALPHA': 0.9983,
-                         'BETA': 0.64},
-              'IR_120': {'VC': 836.445,
-                         'ALPHA': 0.9988,
-                         'BETA': 0.408},
-              'IR_134': {'VC': 751.792,
-                         'ALPHA': 0.9981,
-                         'BETA': 0.561}}
-
-# Meteosat 10
-
-CALIB[323] = {'HRV': {'F': 78.9416 / np.pi},
-              'VIS006': {'F': 65.5148 / np.pi},
-              'VIS008': {'F': 73.1807 / np.pi},
-              'IR_016': {'F': 62.0208 / np.pi},
-              'IR_039': {'VC': 2547.771,
-                         'ALPHA': 0.9915,
-                         'BETA': 2.9002},
-              'WV_062': {'VC': 1595.621,
-                         'ALPHA': 0.9960,
-                         'BETA': 2.0337},
-              'WV_073': {'VC': 1360.337,
-                         'ALPHA': 0.9991,
-                         'BETA': 0.4340},
-              'IR_087': {'VC': 1148.130,
-                         'ALPHA': 0.9996,
-                         'BETA': 0.1714},
-              'IR_097': {'VC': 1034.715,
-                         'ALPHA': 0.9999,
-                         'BETA': 0.0527},
-              'IR_108': {'VC': 929.842,
-                         'ALPHA': 0.9983,
-                         'BETA': 0.6084},
-              'IR_120': {'VC': 838.659,
-                         'ALPHA': 0.9988,
-                         'BETA': 0.3882},
-              'IR_134': {'VC': 750.653,
-                         'ALPHA': 0.9982,
-                         'BETA': 0.5390}}
-
-# Meteosat 11
-
-CALIB[324] = {'HRV': {'F': 79.0035 / np.pi},
-              'VIS006': {'F': 65.2656 / np.pi},
-              'VIS008': {'F': 73.1692 / np.pi},
-              'IR_016': {'F': 61.9416 / np.pi},
-              'IR_039': {'VC': 2555.280,
-                         'ALPHA': 0.9916,
-                         'BETA': 2.9438},
-              'WV_062': {'VC': 1596.080,
-                         'ALPHA': 0.9959,
-                         'BETA': 2.0780},
-              'WV_073': {'VC': 1361.748,
-                         'ALPHA': 0.9990,
-                         'BETA': 0.4929},
-              'IR_087': {'VC': 1147.433,
-                         'ALPHA': 0.9996,
-                         'BETA': 0.1731},
-              'IR_097': {'VC': 1034.851,
-                         'ALPHA': 0.9998,
-                         'BETA': 0.0597},
-              'IR_108': {'VC': 931.122,
-                         'ALPHA': 0.9983,
-                         'BETA': 0.6256},
-              'IR_120': {'VC': 839.113,
-                         'ALPHA': 0.9988,
-                         'BETA': 0.4002},
-              'IR_134': {'VC': 748.585,
-                         'ALPHA': 0.9981,
-                         'BETA': 0.5635}}
-
-# Polynomial coefficients for spectral-effective BT fits
-BTFIT = {}
-# [A, B, C]
-BTFIT['IR_039'] = [0.0, 1.011751900, -3.550400]
-BTFIT['WV_062'] = [0.00001805700, 1.000255533, -1.790930]
-BTFIT['WV_073'] = [0.00000231818, 1.000668281, -0.456166]
-BTFIT['IR_087'] = [-0.00002332000, 1.011803400, -1.507390]
-BTFIT['IR_097'] = [-0.00002055330, 1.009370670, -1.030600]
-BTFIT['IR_108'] = [-0.00007392770, 1.032889800, -3.296740]
-BTFIT['IR_120'] = [-0.00007009840, 1.031314600, -3.181090]
-BTFIT['IR_134'] = [-0.00007293450, 1.030424800, -2.645950]
-
-C1 = 1.19104273e-5
-C2 = 1.43877523
-
-CHANNEL_NAMES = {1: "VIS006",
-                 2: "VIS008",
-                 3: "IR_016",
-                 4: "IR_039",
-                 5: "WV_062",
-                 6: "WV_073",
-                 7: "IR_087",
-                 8: "IR_097",
-                 9: "IR_108",
-                 10: "IR_120",
-                 11: "IR_134",
-                 12: "HRV"}
 
 
 class HRITMSGFileHandler(HRITFileHandler):
