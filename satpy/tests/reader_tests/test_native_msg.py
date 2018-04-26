6dad58272bccf2a1fbd38451396a9fffb3efa13d
Adam.Dybbroe <adam.dybbroe@smhi.se>
2018-04-26 14:31:09 +0200

    Restructure settings of the channel_index_list and the available_channels
    Signed-off-by: Adam.Dybbroe <adam.dybbroe@smhi.se>

--- satpy/tests/reader_tests/test_native_msg.py
+++ satpy/tests/reader_tests/test_native_msg.py
@@ -129,8 +129,8 @@ class TestNativeMSGFileHandler(unittest.TestCase):
         self.reader.platform_name = 'Meteosat-10'
         self.reader.platform_id = 323
         self.reader.header = hdr
-        self.reader.channel_index_list = CHANNEL_INDEX_LIST
-        #self.reader.available_channels = AVAILABLE_CHANNELS
+        self.reader.available_channels = AVAILABLE_CHANNELS
+        self.reader._channel_index_list = CHANNEL_INDEX_LIST
 
     def test_convert_to_radiance(self):
         """Test the conversion from counts to radiance method"""
@@ -156,10 +156,11 @@ class TestNativeMSGFileHandler(unittest.TestCase):
         data = self.reader._ir_calibrate(data, key_name)
         assertNumpyArraysEqual(data.data, IR_108_TBS)
 
-    def test_get_available_channels(self):
-        # We need a small test here!
-        # Fixme!
-        pass
+    # def test_get_available_channels(self):
+    #     # We need a small test here!
+    #     # Fixme!
+
+    #     pass
 
     def tearDown(self):
         pass
