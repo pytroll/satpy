ecef03a2abd47fe579df3446b5b611b8ad7c43af
wroberts <wroberts4@wisc.edu>
2018-08-17 14:45:22 -0500

    Make crefl_utils.py less redundant

--- satpy/tests/test_crefl_utils.py
+++ satpy/tests/test_crefl_utils.py
@@ -9,7 +9,7 @@ class TestCreflUtils(unittest.TestCase):
         from satpy.composites.crefl_utils import get_atm_variables_abi
         sphalb, rhoray, TtotraytH2O, tOG = get_atm_variables_abi(0.17690244, 6.123234e-17, 530.61332168, 405.,
                                                                  0.0043149700000000004, 0.0037296,
-                                                                  0.014107995000000002, 0.052349, 21.71342113,
+                                                                 0.014107995000000002, 0.052349, 21.71342113,
                                                                  77.14385758, 56.21456696)
         if abs(np.array(sphalb) - 0.045213532544630494) >= 1e-10:
             raise AssertionError('{} is not within {} of {}'.format(np.array(sphalb), 1e-10, 0.045213532544630494))
