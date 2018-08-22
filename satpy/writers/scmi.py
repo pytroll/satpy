c5533547e2d59d8c73609461df85606e25cc5273
R.K.Garcia <rkgarcia@wisc.edu>
2018-08-22 00:02:11 -0500

    Define fixed grid in scmi.yaml that matches the current AHI-H08 FGF (???) Massage magic keys to be more magical, but not yet magic enough Loads input but missing awips_info['source_name'], more research tomorrow on what that is, why it's important, and how to provide it

--- satpy/writers/scmi.py
+++ satpy/writers/scmi.py
@@ -811,6 +811,8 @@ class SCMIWriter(Writer):
             return awips_info
         except KeyError as e:
             LOG.error("Could not get information on dataset from backend configuration file")
+            # LOG.debug(repr(awips_info))
+            # LOG.debug(repr(ds_info))
             raise
 
     def save_dataset(self, dataset, **kwargs):
