cca8b526d3f8abb0b807cd36c29a57df066ae6a5
Martin Raspaud <martin.raspaud@smhi.se>
2018-09-14 09:57:24 +0200

    Import satpy's version later in sphinx's config

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,12 +16,12 @@
 
 import os
 import sys
-from satpy import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.append(os.path.abspath('../../'))
+from satpy import __version__  # noqa
 
 
 class Mock(object):
