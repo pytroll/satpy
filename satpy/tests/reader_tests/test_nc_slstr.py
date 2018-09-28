8e18543d231b4e52e288623d71cb502b5c064455
Ulrik Egede <u.egede@imperial.ac.uk>
2018-09-28 17:37:31 +0100

    Addressed comments on tests

--- satpy/tests/reader_tests/test_nc_slstr.py
+++ satpy/tests/reader_tests/test_nc_slstr.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """Module for testing the satpy.readers.nc_slstr module.
 """
-
 try:
     import unittest.mock as mock
 except ImportError:
@@ -10,59 +9,44 @@ except ImportError:
 from unittest import TestCase
 
 
-def mock_dataset():
-    """Create a mock xarray"""
-    return mock.MagicMock()
-
-
 class TestSLSTRReader(TestCase):
 
     @mock.patch('xarray.open_dataset')
-    def test_instantiate(self, mocked_Dataset):
+    def test_instantiate(self, mocked_dataset):
 
-        mocked_Dataset.return_value = mock_dataset()
+        mocked_dataset.return_value = mock.MagicMock()
 
         from satpy.readers.nc_slstr import NCSLSTR1B, NCSLSTRGeo, NCSLSTRAngles, NCSLSTRFlag
+        from satpy import DatasetID
 
-        class Dummy():
-            def __init__(self):
-                self.name = 'foo'
+        ds_id = DatasetID(name='foo')
+        filename_info = {'mission_id': 'S3A', 'dataset_name': 'foo', 'start_time': 0, 'end_time': 0}
 
-        class Base():
-            def copy(self):
-                return None
-
-            def __getitem__(self, a):
-                return 'S3A'
-
-            def update(self, a):
-                return
-
-        test = NCSLSTR1B('somedir/S1_radiance_an.nc', Base(), 'c')
+        test = NCSLSTR1B('somedir/S1_radiance_an.nc', filename_info, 'c')
         assert(test.view == 'n')
         assert(test.stripe == 'a')
-        test.get_dataset(Dummy(), Base())
-        mocked_Dataset.assert_called()
-        mocked_Dataset.reset_mock()
+        test.get_dataset(ds_id, filename_info)
+        mocked_dataset.assert_called()
+        mocked_dataset.reset_mock()
 
-        test = NCSLSTR1B('somedir/S1_radiance_co.nc', Base(), 'c')
+        test = NCSLSTR1B('somedir/S1_radiance_co.nc', filename_info, 'c')
         assert(test.view == 'o')
         assert(test.stripe == 'c')
-        test.get_dataset(Dummy(), Base())
-        mocked_Dataset.assert_called()
-        mocked_Dataset.reset_mock()
+        test.get_dataset(ds_id, filename_info)
+        mocked_dataset.assert_called()
+        mocked_dataset.reset_mock()
 
-        test = NCSLSTRGeo('somedir/S1_radiance_an.nc', Base(), 'c')
-        test.get_dataset(Dummy(), Base())
-        mocked_Dataset.assert_called()
-        mocked_Dataset.reset_mock()
+        test = NCSLSTRGeo('somedir/S1_radiance_an.nc', filename_info, 'c')
+        test.get_dataset(ds_id, filename_info)
+        mocked_dataset.assert_called()
+        mocked_dataset.reset_mock()
 
-        test = NCSLSTRAngles('somedir/S1_radiance_an.nc', Base(), 'c')
-        mocked_Dataset.assert_called()
-        mocked_Dataset.reset_mock()
+        test = NCSLSTRAngles('somedir/S1_radiance_an.nc', filename_info, 'c')
+        mocked_dataset.assert_called()
+        mocked_dataset.reset_mock()
 
-        test = NCSLSTRFlag('somedir/S1_radiance_an.nc', Base(), 'c')
+        test = NCSLSTRFlag('somedir/S1_radiance_an.nc', filename_info, 'c')
         assert(test.view == 'n')
         assert(test.stripe == 'a')
-        mocked_Dataset.assert_called()
-        mocked_Dataset.reset_mock()
+        mocked_dataset.assert_called()
+        mocked_dataset.reset_mock()
