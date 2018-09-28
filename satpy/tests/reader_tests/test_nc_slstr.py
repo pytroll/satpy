51baef98d4485e9d264d4d409408a4e00ff8ce8e
Ulrik Egede <u.egede@imperial.ac.uk>
2018-09-28 15:40:53 +0100

    Updated tests

--- satpy/tests/reader_tests/test_nc_slstr.py
+++ satpy/tests/reader_tests/test_nc_slstr.py
@@ -24,21 +24,47 @@ class TestSLSTRReader(TestCase):
 
         from satpy.readers.nc_slstr import NCSLSTR1B, NCSLSTRGeo, NCSLSTRAngles, NCSLSTRFlag
 
-        class base():
+        class Dummy():
+            def __init__(self):
+                self.name = 'foo'
+
+        class Base():
             def copy(self):
                 return None
 
             def __getitem__(self, a):
                 return 'S3A'
 
-        test = NCSLSTR1B('somedir/S1_radiance_an.nc', base(), 'c')
+            def update(self, a):
+                return
+
+        test = NCSLSTR1B('somedir/S1_radiance_an.nc', Base(), 'c')
         assert(test.view == 'n')
         assert(test.stripe == 'a')
+        test.get_dataset(Dummy(), Base())
+        mocked_Dataset.assert_called()
+        mocked_Dataset.reset_mock()
 
-        test = NCSLSTR1B('somedir/S1_radiance_co.nc', base(), 'c')
+        test = NCSLSTR1B('somedir/S1_radiance_co.nc', Base(), 'c')
         assert(test.view == 'o')
         assert(test.stripe == 'c')
+        test.get_dataset(Dummy(), Base())
+        mocked_Dataset.assert_called()
+        mocked_Dataset.reset_mock()
 
-        NCSLSTRGeo('somedir/S1_radiance_an.nc', base(), 'c')
-        NCSLSTRAngles('somedir/S1_radiance_an.nc', base(), 'c')
-        NCSLSTRFlag('somedir/S1_radiance_an.nc', base(), 'c')
+        test = NCSLSTRGeo('somedir/S1_radiance_an.nc', Base(), 'c')
+        test.get_dataset(Dummy(), Base())
+        mocked_Dataset.assert_called()
+        mocked_Dataset.reset_mock()
+        
+        
+        test = NCSLSTRAngles('somedir/S1_radiance_an.nc', Base(), 'c')
+        mocked_Dataset.assert_called()
+        mocked_Dataset.reset_mock()
+
+        test = NCSLSTRFlag('somedir/S1_radiance_an.nc', Base(), 'c')
+        assert(test.view == 'n')
+        assert(test.stripe == 'a')
+        mocked_Dataset.assert_called()
+        mocked_Dataset.reset_mock()
+        
