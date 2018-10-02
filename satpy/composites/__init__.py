d7d200a8d8f1d9bc8c56d7e1ef4d31e4b176c6be
Jorge Hummberto Bravo Méndez <jh.herk@gmail.com>
2018-10-02 18:47:45 -0500

    blank line without whitespace

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -1281,4 +1281,3 @@ class VolcanicGas(GenericCompositor):
                                              projectables[3]),
                                             *args, **kwargs)
         return res
-      
