ceb2974b9e518b76d29f1be79f94f66ea3357cdf
Kat Kolman <kkolman@gufi.ssec.wisc.edu>
2018-12-18 15:18:00 -0600

    Modified __init__.py so all tests run

--- satpy/tests/enhancement_tests/__init__.py
+++ satpy/tests/enhancement_tests/__init__.py
@@ -3,7 +3,7 @@
 
 import sys
 
-from satpy.tests.enhancement_tests import test_enhancements
+from satpy.tests.enhancement_tests import test_enhancements, test_viirs
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -15,5 +15,6 @@ def suite():
     """Test suite for all enhancement tests"""
     mysuite = unittest.TestSuite()
     mysuite.addTests(test_enhancements.suite())
+    mysuite.addTests(test_viirs.suite())
 
     return mysuite
