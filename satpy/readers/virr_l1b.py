50fdda2fa49de82a5dcf0ee6db21d62d776e6315
wroberts <wroberts4@wisc.edu>
2019-01-07 14:37:54 -0600

    Add tests for VIRR reader and simplify logic

--- satpy/readers/virr_l1b.py
+++ satpy/readers/virr_l1b.py
@@ -1,10 +1,10 @@
 from datetime import datetime
+from satpy.readers.hdf5_utils import HDF5FileHandler
+from pyspectral.blackbody import blackbody_wn_rad2temp as rad2temp
 import numpy as np
 import xarray as xr
 import dask.array as da
 import logging
-from satpy.readers.hdf5_utils import HDF5FileHandler
-from pyspectral.blackbody import blackbody_wn_rad2temp as rad2temp
 
 
 class VIRR_L1B(HDF5FileHandler):
@@ -13,39 +13,29 @@ class VIRR_L1B(HDF5FileHandler):
     def __init__(self, filename, filename_info, filetype_info):
         super(VIRR_L1B, self).__init__(filename, filename_info, filetype_info)
         logging.info('day/night flag for {0}: {1}'.format(filename, self['/attr/Day Or Night Flag']))
-        # for key, val in self.file_content.items():
-        #     print(key + ':', val)
-        # print()
-        self.geolocation_extension = filetype_info['geolocation_extension']
+        self.geolocation_prefix = filetype_info['geolocation_prefix']
         self.platform_id = filename_info['platform_id']
-        self.l1b_extension = 'Data/'
+        self.l1b_prefix = 'Data/'
         self.wave_number = 'Emissive_Centroid_Wave_Number'
         if filename_info['platform_id'] == 'FY3B':
-            self.l1b_extension = ''
+            self.l1b_prefix = ''
             self.wave_number = 'Emmisive_Centroid_Wave_Number'
 
     def get_dataset(self, dataset_id, ds_info):
-        file_key = self.geolocation_extension + ds_info.get('file_key', dataset_id.name)
+        file_key = self.geolocation_prefix + ds_info.get('file_key', dataset_id.name)
         if self.platform_id == 'FY3B':
             file_key = file_key.replace('Data/', '')
-        # print('DAY/NIGHT: ', self['/attr/Day Or Night Flag'])
-        # print(file_key)
-        # print(self.filename_info)
-        # print(self.filetype_info)
-        # print()
         data = self.get(file_key)
         if data is None:
-            raise ValueError('{0} cannot be found')
+            raise ValueError('File key "{0}" could not be found in file {1}'.format(file_key, self.filename))
         band_index = ds_info.get('band_index')
         if band_index is not None:
             data = data[band_index]
-            new_dims = {old: new for old, new in zip(data.dims, ('y', 'x'))}
-            data = data.rename(new_dims)
             data = data.where((data >= self[file_key + '/attr/valid_range'][0]) &
                               (data <= self[file_key + '/attr/valid_range'][1]))
             if 'E' in dataset_id.name:
-                slope = self[self.l1b_extension + 'Emissive_Radiance_Scales'].data[:, band_index][:, np.newaxis]
-                intercept = self[self.l1b_extension + 'Emissive_Radiance_Offsets'].data[:, band_index][:, np.newaxis]
+                slope = self[self.l1b_prefix + 'Emissive_Radiance_Scales'].data[:, band_index][:, np.newaxis]
+                intercept = self[self.l1b_prefix + 'Emissive_Radiance_Offsets'].data[:, band_index][:, np.newaxis]
                 radiance_data = rad2temp(self['/attr/' + self.wave_number][band_index] * 100,
                                          (data * slope + intercept) * 1e-5)
                 data = xr.DataArray(da.from_array(radiance_data, data.chunks),
@@ -55,18 +45,21 @@ class VIRR_L1B(HDF5FileHandler):
                 intercept = self['/attr/RefSB_Cal_Coefficients'][1::2]
                 data = data * slope[band_index] + intercept[band_index]
         else:
-            data = data.attrs['Intercept'] + data.attrs['Slope'] * data
-
+            data = data.where((data >= self[file_key + '/attr/valid_range'][0]) &
+                              (data <= self[file_key + '/attr/valid_range'][1]))
+            data = self[file_key + '/attr/Intercept'] + self[file_key + '/attr/Slope'] * data
+        new_dims = {old: new for old, new in zip(data.dims, ('y', 'x'))}
+        data = data.rename(new_dims)
         data.attrs.update({'platform_name': self['/attr/Satellite Name'],
                            'sensor': self['/attr/Sensor Identification Code']})
         data.attrs.update(ds_info)
-        if self[file_key].attrs.get('units') is not None:
-            data.attrs.update({'units': self[file_key].attrs['units'].decode("utf-8")})
+        units = self.get(file_key + '/attr/units')
+        if units is not None and str(units).lower() != 'none':
+            data.attrs.update({'units': self.get(file_key + '/attr/units')})
         elif data.attrs.get('calibration') == 'reflectance':
             data.attrs.update({'units': '%'})
         else:
             data.attrs.update({'units': 1})
-        # print(data)
         return data
 
     @property
