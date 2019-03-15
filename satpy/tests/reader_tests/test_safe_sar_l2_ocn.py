a08dd896b1e4be296180d4aedb82df466ba8746a
Trygve Aspenes <trygveas@met.no>
2019-03-15 10:35:27 +0100

    Another try with the test

--- satpy/tests/reader_tests/test_safe_sar_l2_ocn.py
+++ satpy/tests/reader_tests/test_safe_sar_l2_ocn.py
@@ -19,12 +19,13 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Module for testing the satpy.readers.safe_rsae_l2_ocn module.
+"""Module for testing the satpy.readers.safe_sar_l2_ocn module.
 """
 import os
 import sys
 import numpy as np
 import xarray as xr
+from satpy.tests.reader_tests.test_netcdf_utils import FakeNetCDF4FileHandler
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -36,25 +37,61 @@ try:
 except ImportError:
     import mock
 
-
 class TestSAFENC(unittest.TestCase):
     """Test various SAFE SAR L2 OCN file handlers."""
-
-    @mock.patch('xarray.open_dataset')
-    def test_init(self, mocked_dataset):
-        """Test basic init with no extra parameters."""
+    @mock.patch('satpy.readers.safe_sar_l2_ocn.xr')
+    @mock.patch.multiple('satpy.readers.safe_sar_l2_ocn.SAFENC',
+                         __abstractmethods__=set())
+    def setUp(self, xr_):
         from satpy.readers.safe_sar_l2_ocn import SAFENC
-        from satpy import DatasetID
 
-        ds_id = DatasetID(name='foo')
-        filename_info = {'mission_id': 'S3A', 'product_type': 'foo',
-                         'start_time': 0, 'end_time': 0,
-                         'fstart_time': 0, 'fend_time': 0,
-                         'polarization': 'vv'}
+        # Mock file access to return a fake dataset.
+        self.dummy3d = np.zeros((1, 2, 2))
+        self.dummy2d = np.zeros((2, 2))
+        self.band = 1
+        self.nc = xr.Dataset(
+            {'owiWindSpeed': xr.DataArray(self.dummy2d, dims=('owiAzSize', 'owiRaSize')),
+             'owilon': xr.DataArray(data=self.dummy2d,  dims=('owiAzSize', 'owiRaSize')),
+             'owilat': xr.DataArray(data=self.dummy2d, dims=('owiAzSize', 'owiRaSize'))},
+            attrs={'_FillValue': np.nan})
+        xr_.open_dataset.return_value = self.nc
 
-        test = SAFENC('S1A_IW_OCN__2SDV_20190228T075834_20190228T075849_026127_02EA43_8846.SAFE/measurement/'
-                      's1a-iw-ocn-vv-20190228t075741-20190228t075800-026127-02EA43-001.nc', filename_info, 'c')
-        mocked_dataset.assert_called()
+        # Instantiate reader using the mocked open_dataset() method. Also, make
+        # the reader believe all abstract methods have been implemented.
+        self.reader = SAFENC(filename='dummy',
+                             filename_info={'start_time': 0,
+                                            'end_time': 0,
+                                            'fstart_time': 0,
+                                            'fend_time': 0,
+                                            'polarization': 'vv'},
+                             filetype_info={})
+
+
+    def test_init(self):
+        """Tests reader initialization"""
+        self.assertEqual(self.reader.start_time, 0)
+        self.assertEqual(self.reader.end_time, 0)
+        self.assertEqual(self.reader.fstart_time, 0)
+        self.assertEqual(self.reader.fend_time, 0)
+
+#    @mock.patch('xarray.open_dataset')
+#    def test_init(self, mocked_dataset):
+#        """Test basic init with no extra parameters."""
+#        from satpy.readers.safe_sar_l2_ocn import SAFENC
+#        from satpy import DatasetID
+#
+#        print(mocked_dataset)
+#        ds_id = DatasetID(name='foo')
+#        filename_info = {'mission_id': 'S3A', 'product_type': 'foo',
+#                         'start_time': 0, 'end_time': 0,
+#                         'fstart_time': 0, 'fend_time': 0,
+#                         'polarization': 'vv'}
+#
+#        test = SAFENC('S1A_IW_OCN__2SDV_20190228T075834_20190228T075849_026127_02EA43_8846.SAFE/measurement/'
+#                      's1a-iw-ocn-vv-20190228t075741-20190228t075800-026127-02EA43-001.nc', filename_info, 'c')
+#        print(test)
+#        mocked_dataset.assert_called()
+#        test.get_dataset(ds_id, filename_info)
 
 
 def suite():
