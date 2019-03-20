6674ca544beec5a526ea2a65bbd9205f122db1b3
rdaruwala <rdaruwala@gmail.com>
2019-03-20 15:56:11 -0400

    Add documentation

--- satpy/readers/viirs_active_fires.py
+++ satpy/readers/viirs_active_fires.py
@@ -1,3 +1,29 @@
+# Copyright (c) 2019 SatPy Developers
+#
+# Author(s):
+#
+#   Rohan Daruwala <rdaruwala@wisc.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+"""VIIRS Active Fires reader
+*********************
+This module implements readers for VIIRS Active Fires NetCDF and
+ASCII files.
+"""
+
 from satpy.readers.netcdf_utils import NetCDF4FileHandler
 from satpy.readers.file_handlers import BaseFileHandler
 import os
--- satpy/tests/reader_tests/test_viirs_fires.py
+++ satpy/tests/reader_tests/test_viirs_fires.py
@@ -1,3 +1,29 @@
+# Copyright (c) 2019 SatPy Developers
+#
+# Author(s):
+#
+#   Rohan Daruwala <rdaruwala@wisc.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+"""VIIRS Active Fires Tests
+*********************
+This module implements tests for VIIRS Active Fires NetCDF and ASCII file
+readers.
+"""
+
 import sys
 import os
 import numpy as np
