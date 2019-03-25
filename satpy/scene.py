6ace85c1347abf9e23172409447653f83f8d506d
Martin Raspaud <martin.raspaud@smhi.se>
2019-03-25 21:47:38 +0100

    Do not pass `filter_parameters` to the filehandler creation

--- satpy/scene.py
+++ satpy/scene.py
@@ -195,6 +195,8 @@ class Scene(MetadataObject):
                                 reader=None,
                                 reader_kwargs=None):
         """Find readers and return their instances."""
+        reader_kwargs = reader_kwargs.copy()
+        reader_kwargs.pop('filter_parameters', None)
         return load_readers(filenames=filenames,
                             reader=reader,
                             reader_kwargs=reader_kwargs,
