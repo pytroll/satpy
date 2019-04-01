a0a3367b9409d47522912abb951f5008177ae37e
David Hoese <david.hoese@ssec.wisc.edu>
2019-03-31 21:16:20 -0500

    Add get_us_midlatitude_cyclone_abi demo function

--- satpy/demo/__init__.py
+++ satpy/demo/__init__.py
@@ -37,28 +37,41 @@ including:
     This is method is mainly meant when tutorials are taught at the SSEC
     using a Jupyter Hub server.
 
+To use these functions, do:
+
+    >>> from satpy import Scene, demo
+    >>> filenames = demo.get_us_midlatitude_cyclone_abi()
+    >>> scn = Scene(reader='abi_l1b', filenames=filenames)
+
 """
 
 import os
 import logging
 
-try:
-    import gcsfs
-except ImportError:
-    gcsfs = None
-
 LOG = logging.getLogger(__name__)
 
 
+def makedirs(directory, exist_ok=False):
+    """Python 2.7 friendly os.makedirs.
+
+    After python 2.7 is dropped, just use `os.makedirs` with `existsok=True`.
+    """
+    try:
+        os.makedirs(directory)
+    except OSError:
+        if not exist_ok:
+            raise
+
+
 def get_us_midlatitude_cyclone_abi(base_dir='.', method=None, force=False):
-    """Get GOES-16 ABI data from March 14th 00:00Z.
+    """Get GOES-16 ABI (CONUS sector) data from March 14th 00:00Z.
 
     Args:
         base_dir (str): Base directory for downloaded files.
         method (str): Force download method for the data if not already cached.
             Allowed options are: 'gcsfs'. Default of ``None`` will
             choose the best method based on environment settings.
-        force (bool): Force re-download of data regardless of its existense on
+        force (bool): Force re-download of data regardless of its existence on
             the local system. Warning: May delete non-demo files stored in
             download directory.
 
@@ -68,16 +81,9 @@ def get_us_midlatitude_cyclone_abi(base_dir='.', method=None, force=False):
     if method not in ['gcsfs']:
         raise NotImplementedError("Demo data download method '{}' not "
                                   "implemented yet.".format(method))
-    fs = gcsfs.GCSFileSystem(token='anon')
-    filenames = []
-    for fn in fs.glob('gs://gcp-public-data-goes-16/ABI-L1b-RadC/2019/073/00/*0002*.nc'):
-        ondisk_fn = os.path.basename(fn)
-        ondisk_pathname = os.path.join(base_dir, ondisk_fn)
-        filenames.append(ondisk_pathname)
-        LOG.info("Downloading: {}".format(ondisk_pathname))
 
-        if force and os.path.isfile(ondisk_pathname):
-            os.remove(ondisk_pathname)
-        elif os.path.isfile(ondisk_pathname):
-            continue
-        fs.get('gs://' + fn, ondisk_pathname)
+    from .google_cloud_platform import get_bucket_files
+    patterns = ['gs://gcp-public-data-goes-16/ABI-L1b-RadC/2019/073/00/*0002*.nc']
+    subdir = os.path.join(base_dir, 'abi_l1b', '20190314_us_midlatitude_cyclone')
+    makedirs(subdir, exist_ok=True)
+    return get_bucket_files(patterns, subdir, force=force)
