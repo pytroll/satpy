a5c1e32eff17c3cf7c67cd8f2d1a5aa543076ca2
David Hoese <david.hoese@ssec.wisc.edu>
2019-03-31 21:16:20 -0500

    Add get_us_midlatitude_cyclone_abi demo function

--- satpy/demo/google_cloud_platform.py
+++ satpy/demo/google_cloud_platform.py
@@ -16,7 +16,54 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import logging
+
+LOG = logging.getLogger(__name__)
+
 
 def is_google_cloud_instance():
     from urllib.request import urlopen
     return urlopen('http://metadata.google.internal').headers.get('Metadata-Flavor') == 'Google'
+
+
+def get_bucket_files(glob_pattern, base_dir, force=False):
+    """Helper function to download files from Google Cloud Storage.
+
+    Args:
+        glob_pattern (str or list): Glob pattern string or series of patterns
+            used to search for on Google Cloud Storage. The pattern should
+            include the "gs://" protocol prefix.
+        base_dir (str): Root directory to place downloaded files on the local
+            system.
+        force (bool): Force re-download of data regardless of its existence on
+            the local system. Warning: May delete non-demo files stored in
+            download directory.
+
+    """
+    try:
+        import gcsfs
+    except ImportError:
+        raise RuntimeError("Missing 'gcsfs' dependency for GCS download.")
+
+    if isinstance(glob_pattern, str):
+        glob_pattern = [glob_pattern]
+
+    fs = gcsfs.GCSFileSystem(token='anon')
+    filenames = []
+    for gp in glob_pattern:
+        for fn in fs.glob(gp):
+            ondisk_fn = os.path.basename(fn)
+            ondisk_pathname = os.path.join(base_dir, ondisk_fn)
+            filenames.append(ondisk_pathname)
+            LOG.info("Downloading: {}".format(ondisk_pathname))
+
+            if force and os.path.isfile(ondisk_pathname):
+                os.remove(ondisk_pathname)
+            elif os.path.isfile(ondisk_pathname):
+                continue
+            fs.get('gs://' + fn, ondisk_pathname)
+
+    if not filenames:
+        raise OSError("No files could be found or downloaded.")
+    return filenames
