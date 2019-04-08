97e2474edd731fe4533de535eb2fd1b61ced602a
Martin Raspaud <martin.raspaud@smhi.se>
2019-04-08 10:47:40 +0200

    Mask out invalid values in precip_clouds

--- satpy/tests/compositor_tests/__init__.py
+++ satpy/tests/compositor_tests/__init__.py
@@ -550,6 +550,7 @@ class TestCloudTopHeightCompositor(unittest.TestCase):
     """Test the CloudTopHeightCompositor."""
 
     def test_call(self):
+        """Test the CloudTopHeight composite generation."""
         from satpy.composites.cloud_products import CloudTopHeightCompositor
         cmap_comp = CloudTopHeightCompositor('test_cmap_compositor')
         palette = xr.DataArray(np.array([[0, 0, 0], [127, 127, 127], [255, 255, 255]]),
@@ -566,6 +567,34 @@ class TestCloudTopHeightCompositor(unittest.TestCase):
                          [0., 0.49803922, np.nan]],
                         [[0., 0.49803922, 0.],
                          [0., 0.49803922, np.nan]]])
+        np.testing.assert_allclose(res, exp)
+
+
+class TestPrecipCloudsCompositor(unittest.TestCase):
+    """Test the PrecipClouds compositor."""
+
+    def test_call(self):
+        """Test the precip composite generation."""
+        from satpy.composites.cloud_products import PrecipCloudsRGB
+        cmap_comp = PrecipCloudsRGB('test_precip_compositor')
+
+        data_light = xr.DataArray(np.array([[80, 70, 60, 0], [20, 30, 40, 255]], dtype=np.uint8),
+                                  dims=['y', 'x'], attrs={'_FillValue': 255})
+        data_moderate = xr.DataArray(np.array([[60, 50, 40, 0], [20, 30, 40, 255]], dtype=np.uint8),
+                                     dims=['y', 'x'], attrs={'_FillValue': 255})
+        data_intense = xr.DataArray(np.array([[40, 30, 20, 0], [20, 30, 40, 255]], dtype=np.uint8),
+                                    dims=['y', 'x'], attrs={'_FillValue': 255})
+        data_flags = xr.DataArray(np.array([[0, 0, 4, 0], [0, 0, 0, 0]], dtype=np.uint8),
+                                  dims=['y', 'x'])
+        res = cmap_comp([data_light, data_moderate, data_intense, data_flags])
+
+        exp = np.array([[[0.24313725, 0.18235294, 0.12156863, np.nan],
+                         [0.12156863, 0.18235294, 0.24313725, np.nan]],
+                        [[0.62184874, 0.51820728, 0.41456583, np.nan],
+                         [0.20728291, 0.31092437, 0.41456583, np.nan]],
+                        [[0.82913165, 0.7254902, 0.62184874, np.nan],
+                         [0.20728291, 0.31092437, 0.41456583, np.nan]]])
+
         np.testing.assert_allclose(res, exp)
 
 
