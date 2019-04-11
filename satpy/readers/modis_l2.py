370d895f2c6bad8de24d88af05b392250f8c1ac5
Lucas <lucas.thibaut.meyer@gmail.com>
2019-04-11 17:06:47 +0100

    Add quality assurance dataset
    Add tests for the dimension of QA dataset

--- satpy/readers/modis_l2.py
+++ satpy/readers/modis_l2.py
@@ -19,6 +19,23 @@ logger = logging.getLogger(__name__)
 
 class ModisL2HDFFileHandler(HDFEOSGeoReader):
 
+    def _select_hdf_dataset(self, hdf_dataset_name, byte_dimension):
+        """Load a dataset from HDF EOS file whose information is contained in a byte in
+        the given dimension.
+
+        """
+        hdf_dataset = self.sd.select(hdf_dataset_name)
+        if byte_dimension == 0:
+            dataset = xr.DataArray(from_sds(hdf_dataset, chunks=CHUNK_SIZE),
+                                   dims=['i', 'y', 'x']).astype(np.uint8)
+        elif byte_dimension == 2:
+            dataset = xr.DataArray(from_sds(hdf_dataset, chunks=CHUNK_SIZE),
+                                   dims=['y', 'x', 'i']).astype(np.uint8)
+            # Reorder dimensions for consistency
+            dataset = dataset.transpose('i', 'y', 'x')
+
+        return dataset
+
     def get_dataset(self, dataset_id, dataset_info):
 
         dataset_name = dataset_id.name
@@ -27,7 +44,9 @@ class ModisL2HDFFileHandler(HDFEOSGeoReader):
         dataset_name_in_file = dataset_info['file_key']
 
         # The dataset asked correspond to a given set of bits of the HDF EOS dataset
-        if 'bits' in dataset_info and 'byte' in dataset_info:
+        if 'byte' in dataset_info and 'byte_dimension' in dataset_info:
+            byte_dimension = dataset_info['byte_dimension']  # Where the information is stored
+            dataset = self._select_hdf_dataset(dataset_name_in_file, byte_dimension)
 
             def bits_strip(bit_start, bit_count, value):
                 """Extract specified bit from bit representation of integer value.
@@ -50,9 +69,6 @@ class ModisL2HDFFileHandler(HDFEOSGeoReader):
                 bit_mask = pow(2, bit_start + bit_count) - 1
                 return np.right_shift(np.bitwise_and(value, bit_mask), bit_start)
 
-            hdf_dataset = self.sd.select(dataset_name_in_file)
-            dataset = xr.DataArray(from_sds(hdf_dataset, chunks=CHUNK_SIZE),
-                                   dims=['i', 'y', 'x']).astype(np.uint8)
             bit_start = dataset_info['bits'][0]
             bit_count = dataset_info['bits'][1]
             dataset = bits_strip(bit_start, bit_count, dataset[dataset_info['byte'], :, :])
