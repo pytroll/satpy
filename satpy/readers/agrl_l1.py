8e392d8374e473d7a8913fdbdd46ce428d61a132
Xin Zhang <xinzhang1215@gmail.com>
2019-05-06 16:49:09 +0800

    edit py file according to coding guidelines

--- satpy/readers/agrl_l1.py
+++ satpy/readers/agrl_l1.py
@@ -28,18 +28,17 @@ import logging
 import numpy as np
 from datetime import datetime
 from pyresample import geometry
-
-from satpy import CHUNK_SIZE
 from satpy.readers.hdf5_utils import HDF5FileHandler
 
 logger = logging.getLogger(__name__)
 
 # info of 500 m, 1 km, 2 km and 4 km data
 _resolution_list = [500, 1000, 2000, 4000]
-_COFF_list       = [10991.5, 5495.5, 2747.5, 1373.5]
-_CFAC_list       = [81865099.0, 40932549.0, 20466274.0, 10233137.0]
-_LOFF_list       = [10991.5, 5495.5, 2747.5, 1373.5]
-_LFAC_list       = [81865099.0, 40932549.0, 20466274.0, 10233137.0]
+_COFF_list = [10991.5, 5495.5, 2747.5, 1373.5]
+_CFAC_list = [81865099.0, 40932549.0, 20466274.0, 10233137.0]
+_LOFF_list = [10991.5, 5495.5, 2747.5, 1373.5]
+_LFAC_list = [81865099.0, 40932549.0, 20466274.0, 10233137.0]
+
 
 class HDF_AGRL_L1(HDF5FileHandler):
 
@@ -49,17 +48,17 @@ class HDF_AGRL_L1(HDF5FileHandler):
     def get_dataset(self, dataset_id, ds_info):
         """Load a dataset."""
         logger.debug('Reading in get_dataset %s.', dataset_id.name)
-        file_key =  ds_info.get('file_key', dataset_id.name)
+        file_key = ds_info.get('file_key', dataset_id.name)
         data = self.get(file_key)
 
         # convert bytes to string
-        data.attrs['long_name']  = data.attrs['long_name'].decode("utf-8")
+        data.attrs['long_name'] = data.attrs['long_name'].decode("utf-8")
         data.attrs['band_names'] = data.attrs['band_names'].decode("utf-8")
 
         # fill_value = data.attrs['FillValue']
         data = data.where((data >= data.attrs['valid_range'][0]) &
-                          (data <= data.attrs['valid_range'][1]))    
-        
+                          (data <= data.attrs['valid_range'][1]))
+
         # calibration
         calibration = ds_info['calibration']
 
@@ -69,9 +68,9 @@ class HDF_AGRL_L1(HDF5FileHandler):
             logger.debug("Calibrating to reflectances")
             # using the corresponding SCALE and OFFSET
             cal_coef = 'CALIBRATION_COEF(SCALE+OFFSET)'
-            slope    = self.get(cal_coef)[:,0][int(file_key[-2:])-1].values
-            offset   = self.get(cal_coef)[:,1][int(file_key[-2:])-1].values
-            data     = self.dn2reflectance(data, slope, offset)
+            slope = self.get(cal_coef)[:, 0][int(file_key[-2:])-1].values
+            offset = self.get(cal_coef)[:, 1][int(file_key[-2:])-1].values
+            data = self.dn2reflectance(data, slope, offset)
         elif calibration == 'brightness_temperature':
             logger.debug("Calibrating to brightness_temperature")
             # the value of dn is the index of brightness_temperature
@@ -80,7 +79,7 @@ class HDF_AGRL_L1(HDF5FileHandler):
         data.attrs.update({'platform_name': self['/attr/Satellite Name'],
                            'sensor': self['/attr/Sensor Identification Code']})
         data.attrs.update(ds_info)
-        units = self.get(file_key + '/attr/units')
+
         if data.attrs.get('calibration') == 'brightness_temperature':
             data.attrs.update({'units': 'K'})
         elif data.attrs.get('calibration') == 'reflectance':
@@ -93,23 +92,23 @@ class HDF_AGRL_L1(HDF5FileHandler):
     def get_area_def(self, key):
         # Coordination Group for Meteorological Satellites LRIT/HRIT Global Specification
         # https://www.cgms-info.org/documents/cgms-lrit-hrit-global-specification-(v2-8-of-30-oct-2013).pdf
-        res  = key.resolution
+        res = key.resolution
         coff = _COFF_list[_resolution_list.index(res)]
         loff = _LOFF_list[_resolution_list.index(res)]
         cfac = _CFAC_list[_resolution_list.index(res)]
         lfac = _LFAC_list[_resolution_list.index(res)]
-        a    = 6378137.0  # equator radius (m)
-        b    = 6356752.3  # polar radius(m)
-        H    = 42164000.0 # the distance between spacecraft and centre of earth (m)
-        h    = H - a      # the altitude of satellite
+        a = 6378137.0  # equator radius (m)
+        b = 6356752.3  # polar radius(m)
+        H = 42164000.0 # the distance between spacecraft and centre of earth (m)
+        h = H - a      # the altitude of satellite
 
-        lon_0  = self.file_content['/attr/NOMCenterLon']
+        lon_0 = self.file_content['/attr/NOMCenterLon']
         nlines = self.file_content['/attr/RegLength']
-        ncols  = self.file_content['/attr/RegWidth']
+        ncols = self.file_content['/attr/RegWidth']
 
         cols = 0
         left_x = (cols - coff) * (2.**16 / cfac)
-        cols += ncols -1
+        cols += ncols - 1
         right_x = (cols - coff) * (2.**16 / cfac)
 
         lines = self.file_content['/attr/Begin Line Number']
@@ -148,11 +147,10 @@ class HDF_AGRL_L1(HDF5FileHandler):
         Returns:
             Reflectance [%]
         """
-        ref  = dn * slope + offset
-        ref *= 100 # set unit to %
+        ref = dn * slope + offset
+        ref *= 100  # set unit to %
 
         return ref.clip(min=0)
-
 
     def calibrate(self, data, cal_key):
         """Calibrate digital number (DN) to brightness_temperature
@@ -167,7 +165,7 @@ class HDF_AGRL_L1(HDF5FileHandler):
         # assign 0 to nan values temporarily
         data.values = data.fillna(0).values.astype(int)
         # dn is the index of CAL table
-        data.values = np.take(self.get(cal_key).values,data.values)
+        data.values = np.take(self.get(cal_key).values, data.values)
         # restore none value
         data.values[nan_index] = np.nan
 
@@ -177,7 +175,7 @@ class HDF_AGRL_L1(HDF5FileHandler):
     def start_time(self):
         start_time = self['/attr/Observing Beginning Date'] + 'T' + self['/attr/Observing Beginning Time'] + 'Z'
         return datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S.%fZ')
-    
+
     @property
     def end_time(self):
         end_time = self['/attr/Observing Ending Date'] + 'T' + self['/attr/Observing Ending Time'] + 'Z'
