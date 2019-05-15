9e1503eff0ef808ccf56fb8b9510cc1886a90c48
Gerrit Holl <gerrit.holl@gmail.com>
2019-05-15 10:06:54 -0500

    Add space character to make sticklr-ci happy
    Add an extra space character before inline comment to hopefully make
    strickr-ci happy

--- satpy/tests/reader_tests/test_avhrr_l1b_gaclac.py
+++ satpy/tests/reader_tests/test_avhrr_l1b_gaclac.py
@@ -20,7 +20,7 @@ from unittest import TestCase, main, TestLoader, TestSuite
 import numpy as np
 try:
     from unittest import mock
-except ImportError: # python 2
+except ImportError:  # python 2
     import mock
 
 GAC_PATTERN = 'NSS.GHRR.{platform_id:2s}.D{start_time:%y%j.S%H%M}.E{end_time:%H%M}.B{orbit_number:05d}{end_orbit_last_digits:02d}.{station:2s}'  # noqa
