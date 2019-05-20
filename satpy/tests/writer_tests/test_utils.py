46ee4da717720c9c696dee47bae18596a9b0b81b
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2019-05-20 15:18:23 +0000

    Fix license

--- satpy/tests/writer_tests/test_utils.py
+++ satpy/tests/writer_tests/test_utils.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2019 Satpy developers
 
-# This file is part of satpy.
+# This file is part of Satpy.
 
-# satpy is free software: you can redistribute it and/or modify it under the
+# Satpy is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 
-# satpy is distributed in the hope that it will be useful, but WITHOUT ANY
+# Satpy is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License along with
-# satpy.  If not, see <http://www.gnu.org/licenses/>.
+# Satpy. If not, see <http://www.gnu.org/licenses/>.
 """Tests for writer utilities"""
 
 
