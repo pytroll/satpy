ef01ffde1be4d54fc9d76b1af22588007fd2e316
Panu Lahtinen <pnuu+git@iki.fi>
2019-05-27 10:44:31 +0300

    Ensure coordinates are as dask arrays

--- satpy/resample.py
+++ satpy/resample.py
@@ -834,7 +834,8 @@ class BucketResamplerBase(BaseResampler):
     def precompute(self, **kwargs):
         """Create X and Y indices and store them for later use."""
         LOG.debug("Initializing bucket resampler.")
-        source_lons, source_lats = self.source_geo_def.get_lonlats()
+        source_lons, source_lats = self.source_geo_def.get_lonlats(
+            chunks=CHUNK_SIZE)
         self.resampler = bucket.BucketResampler(self.target_geo_def,
                                                 source_lons,
                                                 source_lats)
