e3255da19d3ee0920f8105648223c94c546d1e4f
David Hoese <david.hoese@ssec.wisc.edu>
2019-06-10 16:08:14 -0500

    Update copyright header in compositors, readers, and enhancements

--- satpy/readers/scatsat1_l2b.py
+++ satpy/readers/scatsat1_l2b.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017.
+# Copyright (c) 2017 Satpy developers
 #
-# Author(s):
-
-#   Eysteinn Már Sigurðsson <eysteinn@vedur.is>
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This file is part of satpy.
+#
+# satpy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# satpy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# satpy.  If not, see <http://www.gnu.org/licenses/>.
 """ScatSat-1 L2B Reader, distributed by Eumetsat in HDF5 format
 """
 
