ad89fb2447e56a089549f1cf8747fff7fd212502
Tommy Jasmin <tommy.jasmin@ssec.wisc.edu>
2019-06-11 12:52:06 -0500

    Make sure tests actually run

--- satpy/tests/reader_tests/test_tropomi_l2.py
+++ satpy/tests/reader_tests/test_tropomi_l2.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import numpy as np
 from satpy.tests.reader_tests.test_netcdf_utils import FakeNetCDF4FileHandler
 from satpy.readers.netcdf_utils import netCDF4
+import xarray as xr
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -39,10 +40,7 @@ class FakeNetCDF4FileHandlerTL2(FakeNetCDF4FileHandler):
             file_content = {
                 '/attr/time_coverage_start': dt_s.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
                 '/attr/time_coverage_end': dt_e.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
-                '/attr/institution': 'KNMI',
-                '/attr/orbit_number': 3821,
-                '/attr/sensor': 'TROPOMI',
-                '/attr/platform': 'S5P',
+                '/attr/platform_shortname': 'S5P',
             }
 
             file_content['PRODUCT/latitude'] = DEFAULT_FILE_DATA
@@ -65,10 +63,14 @@ class FakeNetCDF4FileHandlerTL2(FakeNetCDF4FileHandler):
                         file_content[key] = DataArray(val, dims=('y', 'x'))
                     else:
                         file_content[key] = DataArray(val)
+            file_content['PRODUCT/latitude'].attrs['_FillValue'] = -999.0
+            file_content['PRODUCT/longitude'].attrs['_FillValue'] = -999.0
+            if 'NO2' in filename:
+                file_content['PRODUCT/nitrogen_dioxide_total_column'].attrs['_FillValue'] = -999.0
+            if 'SO2' in filename:
+                file_content['PRODUCT/sulfurdioxide_total_vertical_column'].attrs['_FillValue'] = -999.0
 
         else:
-            print('filetype_info is: ', filetype_info)
-            print('filetype is: ', filetype_info['file_type'])
             assert False
 
         return file_content
@@ -104,60 +106,37 @@ class TestTROPOMIL2Reader(unittest.TestCase):
         # make sure we have some files
         self.assertTrue(r.file_handlers)
 
-    def test_load_available_datasets(self, configured_datasets=None):
-        """Test loading datasets."""
+    def test_load_no2(self):
+        """Load NO2 dataset"""
         from satpy.readers import load_reader
-        from satpy import DatasetID
-        print("test_load_available_datasets begin...")
         r = load_reader(self.reader_configs)
-        loadables = r.select_files_from_pathnames([
-            'S5P_OFFL_L2__NO2____20180709T170334_20180709T184504_03821_01_010002_20180715T184729.nc',
+        with mock.patch('satpy.readers.tropomi_l2.netCDF4.Variable', xr.DataArray):
+            loadables = r.select_files_from_pathnames([
+                'S5P_OFFL_L2__NO2____20180709T170334_20180709T184504_03821_01_010002_20180715T184729.nc',
             ])
-        r.create_filehandlers(loadables)
+            r.create_filehandlers(loadables)
+        ds = r.load(['nitrogen_dioxide_total_column'])
+        self.assertEqual(len(ds), 1)
+        for d in ds.values():
+            self.assertEqual(d.attrs['platform_shortname'], 'S5P')
+            self.assertIn('area', d.attrs)
+            self.assertIsNotNone(d.attrs['area'])
 
-        # Determine shape of the geolocation data (lat/lon)
-        lat_shape = None
-        for var_name, val in self.file_content.items():
-            if var_name == 'PRODUCT/latitude':
-                lat_shape = self[var_name + "/shape"]
-                break
-
-        handled_variables = set()
-
-        # update previously configured datasets
-        for is_avail, ds_info in (configured_datasets or []):
-            if is_avail is not None:
-                yield is_avail, ds_info
-            var_name = ds_info.get('file_key', ds_info['name'])
-            matches = self.file_type_matches(ds_info['file_type'])
-            if matches and var_name in self:
-                handled_variables.add(var_name)
-                new_info = ds_info.copy()
-                yield True, new_info
-            elif is_avail is None:
-                yield is_avail, ds_info
-
-        for var_name, val in self.file_content.items():
-            if isinstance(val, netCDF4.Variable):
-                var_shape = self[var_name + "/shape"]
-                if var_shape == lat_shape:
-                    if var_name in handled_variables:
-                        continue
-                    handled_variables.add(var_name)
-                    last_index_separator = var_name.rindex('/')
-                    last_index_separator = last_index_separator + 1
-                    var_name_no_path = var_name[last_index_separator:]
-                    new_info = {
-                        'name': var_name_no_path,
-                        'file_key': var_name,
-                        'coordinates': ['longitude', 'latitude'],
-                        'file_type': self.filetype_info['file_type'],
-                        'resolution': None,
-                    }
-                    datasets = r.load([DatasetID(new_info)])
-
-        print("num datasets: ", len(datasets))
-        self.assertEqual(len(datasets), 7)
+    def test_load_so2(self):
+        """Load SO2 dataset"""
+        from satpy.readers import load_reader
+        r = load_reader(self.reader_configs)
+        with mock.patch('satpy.readers.tropomi_l2.netCDF4.Variable', xr.DataArray):
+            loadables = r.select_files_from_pathnames([
+                'S5P_OFFL_L2__SO2____20181224T055107_20181224T073237_06198_01_010105_20181230T150634.nc',
+            ])
+            r.create_filehandlers(loadables)
+        ds = r.load(['sulfurdioxide_total_vertical_column'])
+        self.assertEqual(len(ds), 1)
+        for d in ds.values():
+            self.assertEqual(d.attrs['platform_shortname'], 'S5P')
+            self.assertIn('area', d.attrs)
+            self.assertIsNotNone(d.attrs['area'])
 
 
 def suite():
