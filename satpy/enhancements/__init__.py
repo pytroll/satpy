aefe7a42e8426c2c4acda38125efedc01aa48ccd
David Hoese <david.hoese@ssec.wisc.edu>
2019-06-12 13:20:27 -0500

    Add unconfigured_noop enhancement for RGBs

--- satpy/enhancements/__init__.py
+++ satpy/enhancements/__init__.py
@@ -30,6 +30,15 @@ def noop(img, **kwargs):
     return img
 
 
+def unconfigured_noop(img, **kwargs):
+    """Do nothing and warn that no enhancement was configured."""
+    import warnings
+    name = img.data.attrs.get('name', '<unknown>')
+    warnings.warn("No enhancement configured for '{}'. Will not stretch and "
+                  "assuming already 0-1 normalized.".format(name))
+    return noop(img, **kwargs)
+
+
 def stretch(img, **kwargs):
     """Perform stretch."""
     return img.stretch(**kwargs)
