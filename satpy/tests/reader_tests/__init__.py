f39637dec84796e0d622faa45cab5c3556a14316
Yufei Zhu <zhuyufei@hotmail.com>
2019-06-13 16:34:11 +0000

    modified test scripts and reader list for the name changing of abi l2 reader

--- satpy/tests/reader_tests/__init__.py
+++ satpy/tests/reader_tests/__init__.py
@@ -41,7 +41,7 @@ from satpy.tests.reader_tests import (test_abi_l1b, test_hrit_base,
                                       test_safe_sar_l2_ocn, test_viirs_edr_active_fires,
                                       test_hdfeos_base, test_modis_l2,
                                       test_electrol_hrit, test_mersi2_l1b,
-                                      test_avhrr_l1b_gaclac, test_nc_abi_l2)
+                                      test_avhrr_l1b_gaclac, test_abi_l2_nc)
 
 
 if sys.version_info < (2, 7):
@@ -92,6 +92,6 @@ def suite():
     mysuite.addTests(test_electrol_hrit.suite())
     mysuite.addTests(test_mersi2_l1b.suite())
     mysuite.addTests(test_avhrr_l1b_gaclac.suite())
-    mysuite.addTests(test_nc_abi_l2.suite())
+    mysuite.addTests(test_abi_l2_nc.suite())
 
     return mysuite
