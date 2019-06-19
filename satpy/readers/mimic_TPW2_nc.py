41a3378082fab1a35d4baeefaa94a8b7160acfe1
Joleen Feltz <joleen.feltz@ssec.wisc.edu>
2019-06-19 08:30:56 -0500

    BUG FIX:  add get_dataset and get_area_def missing from original commit.  Add tpwGrid to yaml because           the automatic detection is not adding the tpwGrid variable to the handled_variables outside of           debugging mode in pycharm.  This is still not a model of code to follow.

--- satpy/readers/mimic_TPW2_nc.py
+++ satpy/readers/mimic_TPW2_nc.py
@@ -31,8 +31,13 @@ This module implements readers for MIMIC_TPW2
 netcdf files.
 """
 
+import xarray as xr
+import numpy as np
 from satpy.readers.netcdf_utils import NetCDF4FileHandler, netCDF4
+from pyresample.geometry import AreaDefinition
 
+import logging
+logger = logging.getLogger(__name__)
 
 class MIMIC_TPW2FileHandler(NetCDF4FileHandler):
     """NetCDF4 reader for MIMC TPW 2.0
@@ -42,68 +47,114 @@ class MIMIC_TPW2FileHandler(NetCDF4FileHandler):
         super(MIMIC_TPW2FileHandler, self).__init__(
             filename, filename_info, filetype_info)
 
-    def get_dataset(self, dataset_id, ds_info):
-        var_path = ds_info.get('file_key', dataset_id.name)
-        metadata = self.get_metadata(dataset_id, ds_info)
+    def get_dataset(self, ds_id, info):
+        logger.debug("Getting data for: %s", ds_id.name)
+        file_key = info.get('file_key', ds_id.name)
+        data = self[file_key]
+        data.attrs = self.get_metadata(data, info)
 
-        data = self[var_path]
-        data.attrs.update(metadata)
-
-        if 'latArr' in data.dims:
-            data = data.rename({'latArr': 'y', 'lonArr': 'x'})
         return data
+
+    def get_area_def(self,dsid):
+        latlon=np.meshgrid(self['lonArr'], self['latArr'])
+        width=self['lonArr/shape'][0]
+        height=self['latArr/shape'][0]
+        lower_left_y = latlon[1][0, width-1]
+        lower_left_x = latlon[0][0, width-1]
+
+        upper_right_x = latlon[0][height-1, 0]
+        upper_right_y = latlon[1][height-1, 0]
+
+        area_extent=(lower_left_x, lower_left_y, upper_right_x, upper_right_y)
+        description = "MIMIC TPW2 Mercator Projection"
+        area_id = 'mimic'
+        proj_id = 'mimic'
+        proj_dict = {'proj': 'merc', 'resolution': self._calc_area_resolution(), 'units': 'degrees'}
+        area_def = AreaDefinition(area_id, description, proj_id, proj_dict, width, height, area_extent)
+        return area_def
+
+    def get_metadata(self, data, info):
+        metadata = {}
+        metadata.update(data.attrs)
+        metadata.update(info)
+        metadata.update({
+            'platform_shortname': 'aggregated microwave',
+            'sensor': 'mimic',
+            'start_time': self.start_time,
+            'end_time': self.end_time,
+        })
+
+        return metadata
+
+    def _calc_area_resolution(self):
+        #y_res = abs(self['latArr'][0] - self['latArr'][1])
+        x_res = abs(self['lonArr'][0] - self['lonArr'][1])
+
+        return x_res
 
     def available_datasets(self, configured_datasets=None):
         """Automatically determine datasets provided by this file"""
         # Determine shape of the geolocation data (lat/lon)
-        lat_shape = self["latArr"].shape
-        lon_shape = self["lonArr"].shape
+        lat_shape = self.file_content.get('latArr/shape')
+        lon_shape = self.file_content.get('lonArr/shape')
+        res = self._calc_area_resolution()
+        #Read the lat/lon variables?
         handled_variables = set()
 
-        # Update previously configured datasets
-        # Only geolocation variables, others generated dynamically
+        # update previously configured datasets
+        logger.debug("Starting previously configured variables loop...")
         for is_avail, ds_info in (configured_datasets or []):
+
+            # some other file handler knows how to load this
             if is_avail is not None:
                 yield is_avail, ds_info
+
             var_name = ds_info.get('file_key', ds_info['name'])
+            # logger.debug("Evaluating previously configured variable: %s", var_name)
             matches = self.file_type_matches(ds_info['file_type'])
-            # Can provide this dataset and more info
+            # we can confidently say that we can provide this dataset and can
+            # provide more info
             if matches and var_name in self:
+                logger.debug("Handling previously configured variable: %s", var_name)
+                handled_variables.add(var_name)
+                new_info = ds_info.copy()  # don't mess up the above yielded
+                yield True, new_info
+            elif is_avail is None:
+                # if we didn't know how to handle this dataset and no one else did
+                # then we should keep it going down the chain
+                yield is_avail, ds_info
+
+            if matches and var_name in self.file_content.items():
                 handled_variables.add(var_name)
                 new_info = ds_info.copy()
                 yield True, new_info
             elif is_avail is None:
                 yield is_avail, ds_info
 
-        # Sift through groups and variables for data matching lat/lon shape
+        # Iterate over dataset contents
         for var_name, val in self.file_content.items():
             # Only evaluate variables
+            logger.debug(var_name)
             if isinstance(val, netCDF4.Variable):
+                logger.debug("Evaluating new variable: %s", var_name)
                 var_shape = self[var_name + "/shape"]
+                logger.debug("Dims:{}".format(var_shape))
                 if var_shape == (lat_shape[0], lon_shape[0]):
-                    # Skip anything already configured
-                    if var_name in handled_variables:
+                    logger.debug("Found valid additional dataset: %s", var_name)
+                    # Skip anything we have already configured
+                    if (var_name in handled_variables):
+                        logger.debug("Already handled, skipping: %s", var_name)
                         continue
                     handled_variables.add(var_name)
-                    # Create new ds_info object. Copy over some attributes,
-                    # tune others or set from values in file.
-                    new_info = ds_info.copy()
-                    new_info.update({
-                        'name': var_name.lower(),
-                        'resolution': None,
-                        'units': self[var_name].units,
-                        'long_name': var_name,
-                        'standard_name':  None,
+                    # Create new ds_info object
+                    new_info = {
+                        'name': var_name,
                         'file_key': var_name,
                         'coordinates': ['latArr', 'lonArr'],
-                    })
+                        'file_type': self.filetype_info['file_type'],
+                        'resolution': res,
+                    }
                     yield True, new_info
-
-    def get_metadata(self, data, ds_info):
-        metadata = {}
-
-        return metadata
-
     @property
     def start_time(self):
         return self.filename_info['start_time']
