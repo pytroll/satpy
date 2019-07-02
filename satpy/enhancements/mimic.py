c1475f37dec91835d5fbde73fba1d21095e561fd
Joleen Feltz <joleen.feltz@ssec.wisc.edu>
2019-07-02 10:03:17 -0500

    Add a function to colorize the mimic data with the colors described in the mimic yaml

---
+++ satpy/enhancements/mimic.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Copyright (c) 2018-2019 Satpy developers
+#
+# This file is part of satpy.
+#
+# satpy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# satpy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# satpy.  If not, see <http://www.gnu.org/licenses/>.
+
+from trollimage.colormap import Colormap
+
+
+def total_precipitable_water(img, **kwargs):
+    """Palettizes images from VIIRS flood data.
+
+    This modifies the image's data so the correct colors
+    can be applied to it, and then palettizes the image.
+    """
+    palette = kwargs['palettes']
+    palette['colors'] = tuple(map(tuple, palette['colors']))
+
+    cm = Colormap(*palette['colors'])
+    img.palettize(cm)
