121a76f1e826a6c35ef89c14798275e2c6923d32
simonrp84 <simon.proud@physics.ox.ac.uk>
2019-09-06 20:47:56 +0100

    Add test to compositor that only updates filename with SAYPY_ANCDIR if the static image is present in that directory

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -1418,7 +1418,9 @@ class StaticImageCompositor(GenericCompositor):
         from satpy import Scene
         # Check if filename exists, if not then try from SATPY_ANCPATH
         if (not os.path.isfile(self.filename)):
-            self.filename = os.path.join(get_environ_ancpath(), self.filename)
+                tmp_filename = os.path.join(get_environ_ancpath(), self.filename)
+                if (os.path.isfile(tmp_filename)):
+                        self.filename = tmp_filename
         scn = Scene(reader='generic_image', filenames=[self.filename])
         scn.load(['image'])
         img = scn['image']
