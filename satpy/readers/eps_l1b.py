c80f1362a30a81955f387d41ce27b94d9e913859
Martin Raspaud <martin.raspaud@smhi.se>
2019-10-09 08:57:29 +0200

    Fix copyright notice in eps l1b reader

--- satpy/readers/eps_l1b.py
+++ satpy/readers/eps_l1b.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017 Satpy developers
+# Copyright (c) 2012-2019 Satpy developers
 #
 # This file is part of satpy.
 #
@@ -15,25 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
-
-# Copyright (c) 2012, 2013, 2014 Martin Raspaud
-
-# Author(s):
-
-#   Martin Raspaud <martin.raspaud@smhi.se>
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """Reader for eps level 1b data. Uses xml files as a format description."""
 
