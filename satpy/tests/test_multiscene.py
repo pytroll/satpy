8a4e63a23e67ad29f79e01e8bc7b25023753e8e5
David Hoese <david.hoese@ssec.wisc.edu>
2019-10-13 20:54:42 -0500

    Revert using real Client in MultiScene tests

--- satpy/tests/test_multiscene.py
+++ satpy/tests/test_multiscene.py
@@ -218,7 +218,6 @@ class TestMultiSceneSave(unittest.TestCase):
     def test_save_mp4_distributed(self):
         """Save a series of fake scenes to an mp4 video."""
         from satpy import MultiScene
-        from dask.distributed import LocalCluster, Client
         area = _create_test_area()
         scenes = _create_test_scenes(area=area)
 
@@ -238,17 +237,13 @@ class TestMultiSceneSave(unittest.TestCase):
             self.base_dir,
             'test_save_mp4_{name}_{start_time:%Y%m%d_%H}_{end_time:%Y%m%d_%H}.mp4')
         writer_mock = mock.MagicMock()
-        cluster = LocalCluster(n_workers=1)
-        client = Client(cluster)
-        # client_mock = mock.MagicMock()
-        # client_mock.compute.side_effect = lambda x: tuple(v.compute() for v in x)
-        # client_mock.gather.side_effect = lambda x: x
+        client_mock = mock.MagicMock()
+        client_mock.compute.side_effect = lambda x: tuple(v.compute() for v in x)
+        client_mock.gather.side_effect = lambda x: x
         with mock.patch('satpy.multiscene.imageio.get_writer') as get_writer:
             get_writer.return_value = writer_mock
             # force order of datasets by specifying them
-            mscn.save_animation(fn, client=client, datasets=['ds1', 'ds2', 'ds3'])
-        client.close()
-        cluster.close()
+            mscn.save_animation(fn, client=client_mock, datasets=['ds1', 'ds2', 'ds3'])
 
         # 2 saves for the first scene + 1 black frame
         # 3 for the second scene
@@ -264,18 +259,14 @@ class TestMultiSceneSave(unittest.TestCase):
             self.base_dir,
             'test_save_mp4_{name}_{start_time:%Y%m%d_%H}_{end_time:%Y%m%d_%H}.mp4')
         writer_mock = mock.MagicMock()
-        # client_mock = mock.MagicMock()
-        # client_mock.compute.side_effect = lambda x: tuple(v.compute() for v in x)
-        # client_mock.gather.side_effect = lambda x: x
-        cluster = LocalCluster(n_workers=1)
-        client = Client(cluster)
+        client_mock = mock.MagicMock()
+        client_mock.compute.side_effect = lambda x: tuple(v.compute() for v in x)
+        client_mock.gather.side_effect = lambda x: x
         with mock.patch('satpy.multiscene.imageio.get_writer') as get_writer, \
                 mock.patch('satpy.multiscene.get_client', mock.Mock(side_effect=ValueError("No client"))):
             get_writer.return_value = writer_mock
             # force order of datasets by specifying them
             mscn.save_animation(fn, datasets=['ds1', 'ds2', 'ds3'])
-        client.close()
-        cluster.close()
 
         # 2 saves for the first scene + 1 black frame
         # 3 for the second scene
