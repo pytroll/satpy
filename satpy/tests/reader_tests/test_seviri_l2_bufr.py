9f36202be6bfd8ef99bc1ee6c4784ed752a63233
Colin.Duff@eumetsat.int <colin.duff@eumetsat.int>
2019-10-31 16:26:20 +0100

    Updated so that the bufr reader only returns data/lat/lon as vectors, no mapping/sampling takes place

--- satpy/tests/reader_tests/test_seviri_l2_bufr.py
+++ satpy/tests/reader_tests/test_seviri_l2_bufr.py
@@ -22,8 +22,6 @@ import sys
 
 import numpy as np
 
-import dask.array as da
-
 if sys.version_info < (2, 7):
     import unittest2 as unittest
 else:
@@ -44,10 +42,18 @@ class TestMSGBufr(unittest.TestCase):
         from satpy.readers.seviri_l2_bufr import MSGBUFRFileHandler
         import eccodes as ec
         buf1 = ec.codes_bufr_new_from_samples('BUFR4_local_satellite')
-        ec.codes_set(buf1, 'unpack', 1)
-        sampl = np.random.randint(low=250, high=350, size=(128,))
-        ec.codes_set_array(buf1, '#1#brightnessTemperature', sampl)
-
+        ec.codes_set(buf1, 'unpac'
+                           'k', 1)
+        samp1 = np.random.uniform(low=250, high=350, size=(128,))
+        samp2 = np.random.uniform(low=-60, high=60, size=(128,))
+        samp3 = np.random.uniform(low=10, high=60, size=(128,))
+        # write the bufr test data twice as we want to read in and the concatenate the data in the reader
+        ec.codes_set_array(buf1, '#1#brightnessTemperature', samp1)
+        ec.codes_set_array(buf1, '#1#brightnessTemperature', samp1)
+        ec.codes_set_array(buf1, 'latitude', samp2)
+        ec.codes_set_array(buf1, 'latitude', samp2)
+        ec.codes_set_array(buf1, 'longitude', samp3)
+        ec.codes_set_array(buf1, 'longitude', samp3)
         info = {'satellite': 'meteosat9', 'subsat': 'E0000',
                 'start_time': '201909180000',
                 'key': '#1#brightnessTemperature', 'units': 'm',
@@ -56,24 +62,27 @@ class TestMSGBufr(unittest.TestCase):
                 }
         info2 = {'file_type':  'seviri_l2_bufr_csr'}
 
+        fh = MSGBUFRFileHandler(None, info, info2)
         m = mock.mock_open()
         with mock.patch('satpy.readers.seviri_l2_bufr.open', m, create=True):
-            with mock.patch('satpy.readers.seviri_l2_bufr.MSGBUFRFileHandler.get_array') as arr:
-                samplat = da.from_array(np.random.uniform(low=-60, high=60, size=(128,)))
-                arr.return_value = samplat
-                fh = MSGBUFRFileHandler(None, info, info2)
-
             with mock.patch('eccodes.codes_bufr_new_from_file',
-                            side_effect=[buf1, None]) as ec1:
+                            side_effect=[buf1, buf1, None, buf1, buf1, None, buf1, buf1, None]) as ec1:
                 ec1.return_value = ec1.side_effect
                 with mock.patch('eccodes.codes_set') as ec2:
                     ec2.return_value = 1
                     with mock.patch('eccodes.codes_release') as ec5:
                         ec5.return_value = 1
                         z = fh.get_dataset(None, info)
-                        # There may be some error introduced due to floating point
-                        # rounding so an error of 2 should be ok
-                        self.assertTrue(abs(np.nanmean(z) - np.nanmean(sampl) < 2.0))
+                        # concatenate the original test arrays as
+                        # get dataset will have read and concatented the data
+                        x1 = np.concatenate((samp1, samp1), axis=0)
+                        x2 = np.concatenate((samp2, samp2), axis=0)
+                        x3 = np.concatenate((samp3, samp3), axis=0)
+                        np.testing.assert_array_equal(z.values, x1)
+                        np.testing.assert_array_equal(z.coords['latitude'].values, x2)
+                        np.testing.assert_array_equal(z.coords['longitude'].values, x3)
+                        self.assertEqual(z.attrs['satellite'], info['satellite'])
+                        self.assertEqual(z.attrs['standard_name'], info['standard_name'])
 
     def test_msg_bufr(self):
         """Call the test function."""
