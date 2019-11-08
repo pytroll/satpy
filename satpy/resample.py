20dd18ea209134e8b0db71842f38bce3fe2c4c52
David Hoese <david.hoese@ssec.wisc.edu>
2019-11-08 14:46:52 -0600

    Fix default radius_of_influence for lon/lat AreaDefintions

--- satpy/resample.py
+++ satpy/resample.py
@@ -464,30 +464,14 @@ class KDTreeResampler(BaseResampler):
 
         """
         from pyresample.kd_tree import XArrayResamplerNN
-
         del kwargs
-        source_geo_def = self.source_geo_def
-
         if mask is not None and cache_dir is not None:
             LOG.warning("Mask and cache_dir both provided to nearest "
                         "resampler. Cached parameters are affected by "
                         "masked pixels. Will not cache results.")
             cache_dir = None
-        # TODO: move this to pyresample
-        if radius_of_influence is None:
-            try:
-                radius_of_influence = source_geo_def.lons.resolution * 3
-            except AttributeError:
-                try:
-                    radius_of_influence = max(abs(source_geo_def.pixel_size_x),
-                                              abs(source_geo_def.pixel_size_y)) * 3
-                except AttributeError:
-                    radius_of_influence = 1000
 
-            except TypeError:
-                radius_of_influence = 10000
-
-        kwargs = dict(source_geo_def=source_geo_def,
+        kwargs = dict(source_geo_def=self.source_geo_def,
                       target_geo_def=self.target_geo_def,
                       radius_of_influence=radius_of_influence,
                       neighbours=1,
