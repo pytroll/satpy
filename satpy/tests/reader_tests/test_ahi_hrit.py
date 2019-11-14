1500c00b789fa4078663b3c725974e66c84fb19f
Martin Raspaud <martin.raspaud@smhi.se>
2019-11-14 11:31:43 +0100

    Fix typo

--- satpy/tests/reader_tests/test_ahi_hrit.py
+++ satpy/tests/reader_tests/test_ahi_hrit.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
-"""The hrit msg reader tests package."""
+"""The hrit ahi reader tests package."""
 
 import sys
 import numpy as np
