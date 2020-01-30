57d0355e384c6911d4f67e7211627d5808e6eee4
Colin.Duff@eumetsat.int <colin.duff@eumetsat.int>
2020-01-30 11:01:10 +0100

    Updated to resolve conflicts with Saulis changes

--- satpy/tests/reader_tests/test_seviri_l2_bufr.py
+++ satpy/tests/reader_tests/test_seviri_l2_bufr.py
@@ -52,7 +52,7 @@ DATASET_INFO = {
 }
 
 DATASET_ATTRS = {
-    'spacecraft_name': 'MET08',
+    'platform_name': 'MET08',
     'ssp_lon': 41.5,
     'seg_size': 16
 }
@@ -110,8 +110,8 @@ class TestSeviriL2Bufr(unittest.TestCase):
                         # get dataset will have read and concatented the data
                         x1 = np.concatenate((samp1, samp1), axis=0)
                         np.testing.assert_array_equal(z.values, x1)
-                        self.assertEqual(z.attrs['spacecraft_name'],
-                                         DATASET_ATTRS['spacecraft_name'])
+                        self.assertEqual(z.attrs['platform_name'],
+                                         DATASET_ATTRS['platform_name'])
                         self.assertEqual(z.attrs['ssp_lon'],
                                          DATASET_ATTRS['ssp_lon'])
                         self.assertEqual(z.attrs['seg_size'],
