6ea792fc03e5975fa693a9e231f3703a561ac5f1
Martin Raspaud <martin.raspaud@smhi.se>
2020-03-10 08:46:12 +0100

    Clean up ABI lrit reader

--- satpy/readers/abi_lrit.py
+++ satpy/readers/abi_lrit.py
@@ -23,7 +23,7 @@ References:
 """
 
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import numpy as np
 import xarray as xr
@@ -31,7 +31,6 @@ import dask.array as da
 
 from pyresample import geometry
 
-from satpy.readers.eum_base import time_cds_short
 from satpy.readers.hrit_base import (HRITFileHandler, ancillary_text,
                                      annotation_header, base_hdr_map,
                                      image_data_function)
@@ -43,7 +42,7 @@ class CalibrationError(Exception):
     pass
 
 
-logger = logging.getLogger('hrit_goes')
+logger = logging.getLogger(__name__)
 
 # Geometric constants [meters]
 EQUATOR_RADIUS = 6378169.00
@@ -91,153 +90,6 @@ goes_hdr_map.update({7: key_header,
                      130: header_structure_record,
                      131: rice_compression,
                      })
-
-
-orbit_coef = np.dtype([('StartTime', time_cds_short),
-                       ('EndTime', time_cds_short),
-                       ('X', '>f8', (8, )),
-                       ('Y', '>f8', (8, )),
-                       ('Z', '>f8', (8, )),
-                       ('VX', '>f8', (8, )),
-                       ('VY', '>f8', (8, )),
-                       ('VZ', '>f8', (8, ))])
-
-attitude_coef = np.dtype([('StartTime', time_cds_short),
-                          ('EndTime', time_cds_short),
-                          ('XofSpinAxis', '>f8', (8, )),
-                          ('YofSpinAxis', '>f8', (8, )),
-                          ('ZofSpinAxis', '>f8', (8, ))])
-
-cuc_time = np.dtype([('coarse', 'u1', (4, )),
-                     ('fine', 'u1', (3, ))])
-
-
-sgs_time = np.dtype([('century', 'u1'),
-                     ('year', 'u1'),
-                     ('doy1', 'u1'),
-                     ('doy_hours', 'u1'),
-                     ('hours_mins', 'u1'),
-                     ('mins_secs', 'u1'),
-                     ('secs_msecs', 'u1'),
-                     ('msecs', 'u1')])
-
-
-def make_sgs_time(sgs_time_array):
-    """Make sgs time."""
-    year = ((sgs_time_array['century'] >> 4) * 1000 +
-            (sgs_time_array['century'] & 15) * 100 +
-            (sgs_time_array['year'] >> 4) * 10 +
-            (sgs_time_array['year'] & 15))
-    doy = ((sgs_time_array['doy1'] >> 4) * 100 +
-           (sgs_time_array['doy1'] & 15) * 10 +
-           (sgs_time_array['doy_hours'] >> 4))
-    hours = ((sgs_time_array['doy_hours'] & 15) * 10 +
-             (sgs_time_array['hours_mins'] >> 4))
-    mins = ((sgs_time_array['hours_mins'] & 15) * 10 +
-            (sgs_time_array['mins_secs'] >> 4))
-    secs = ((sgs_time_array['mins_secs'] & 15) * 10 +
-            (sgs_time_array['secs_msecs'] >> 4))
-    msecs = ((sgs_time_array['secs_msecs'] & 15) * 100 +
-             (sgs_time_array['msecs'] >> 4) * 10 +
-             (sgs_time_array['msecs'] & 15))
-    return (datetime(int(year), 1, 1) +
-            timedelta(days=int(doy - 1),
-                      hours=int(hours),
-                      minutes=int(mins),
-                      seconds=int(secs),
-                      milliseconds=int(msecs)))
-
-
-satellite_status = np.dtype([("TagType", "<u4"),
-                             ("TagLength", "<u4"),
-                             ("SatelliteID", "<u8"),
-                             ("SatelliteName", "S256"),
-                             ("NominalLongitude", "<f8"),
-                             ("SatelliteCondition", "<u4"),
-                             ("TimeOffset", "<f8")])
-
-image_acquisition = np.dtype([("TagType", "<u4"),
-                              ("TagLength", "<u4"),
-                              ("Status", "<u4"),
-                              ("StartDelay", "<i4"),
-                              ("Cel", "<f8")])
-
-
-gvar_float = '>i4'
-
-
-def make_gvar_float(float_val):
-    """Make gvar float."""
-    sign = 1
-    if float_val < 0:
-        float_val = -float_val
-        sign = -1
-
-    exp = (float_val >> 24) - 64
-    mant = float_val & ((1 << 24) - 1)
-    if mant == 0:
-        return 0.
-    res = sign * mant * 2.0**(-24 + exp * 4)
-    return res
-
-
-radiometric_processing = np.dtype([("TagType", "<u4"),
-                                   ("TagLength", "<u4"),
-                                   ("RPSummary",
-                                    [("Impulse", "<u4"),
-                                     ("IsStrNoiseCorrection", "<u4"),
-                                        ("IsOptic", "<u4"),
-                                        ("IsBrightnessAligment", "<u4")]),
-                                   ("OpticCorrection",
-                                    [("Degree", "<i4"),
-                                     ("A", "<f8", (16, ))]),
-                                   ("RPQuality",
-                                    [("EffDinRange", "<f8"),
-                                     ("EathDarkening", "<f8"),
-                                        ("Zone", "<f8"),
-                                        ("Impulse", "<f8"),
-                                        ("Group", "<f8"),
-                                        ("DefectCount", "<u4"),
-                                        ("DefectProcent", "<f8"),
-                                        ("S_Noise_DT_Preflight", "<f8"),
-                                        ("S_Noise_DT_Bort", "<f8"),
-                                        ("S_Noise_DT_Video", "<f8"),
-                                        ("S_Noise_DT_1_5", "<f8"),
-                                        ("CalibrStability", "<f8"),
-                                        ("TemnSKO", "<f8", (2, )),
-                                        ("StructSKO", "<f8", (2, )),
-                                        ("Struct_1_5", "<f8"),
-                                        ("Zone_1_ 5", "<f8"),
-                                        ("RadDif", "<f8")])])
-
-geometric_processing = np.dtype([("TagType", "<u4"),
-                                 ("TagLength", "<u4"),
-                                 ("TGeomNormInfo",
-                                  [("IsExist", "<u4"),
-                                   ("IsNorm", "<u4"),
-                                      ("SubLon", "<f8"),
-                                      ("TypeProjection", "<u4"),
-                                      ("PixInfo", "<f8", (4, ))]),
-                                 ("SatInfo",
-                                  [("TISO",
-                                    [("T0", "<f8"),
-                                     ("dT", "<f8"),
-                                        ("ASb", "<f8"),
-                                        ("Evsk", "<f8", (3, 3, 4)),
-                                        ("ARx", "<f8", (4, )),
-                                        ("ARy", "<f8", (4, )),
-                                        ("ARz", "<f8", (4, )),
-                                        ("AVx", "<f8", (4, )),
-                                        ("AVy", "<f8", (4, )),
-                                        ("AVz", "<f8", (4, ))]),
-                                      ("Type", "<i4")]),
-                                 ("TimeProcessing", "<f8"),
-                                 ("ApriorAccuracy", "<f8"),
-                                 ("RelativeAccuracy", "<f8", (2, ))])
-
-
-C1 = 1.19104273e-5
-C2 = 1.43877523
 
 SPACECRAFTS = {
     b'G16': "GOES-16",
