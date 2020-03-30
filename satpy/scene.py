e02cb4018986710900ef3333422d9df8d74a86be
David Hoese <david.hoese@ssec.wisc.edu>
2020-03-30 10:28:10 -0500

    Fix Scene.crop using PROJ definition to create target area definition

--- satpy/scene.py
+++ satpy/scene.py
@@ -468,9 +468,10 @@ class Scene(MetadataObject):
                 'crop_area', 'crop_area', 'crop_latlong',
                 {'proj': 'latlong'}, 100, 100, ll_bbox)
         elif xy_bbox is not None:
+            crs = src_area.crs if hasattr(src_area, 'crs') else src_area.proj_dict
             dst_area = AreaDefinition(
                 'crop_area', 'crop_area', 'crop_xy',
-                src_area.proj_dict, src_area.x_size, src_area.y_size,
+                crs, src_area.x_size, src_area.y_size,
                 xy_bbox)
         x_slice, y_slice = src_area.get_area_slices(dst_area)
         return src_area[y_slice, x_slice], y_slice, x_slice
