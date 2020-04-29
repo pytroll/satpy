16271b1adac1e919a503ea507767d34c673ebc89
Gerrit Holl <gerrit.holl@dwd.de>
2020-04-29 09:39:34 +0200

    PEP8 fixes

--- satpy/readers/yaml_reader.py
+++ satpy/readers/yaml_reader.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
 """Base classes and utilities for all readers configured by YAML files."""
+import glob
 import itertools
 import logging
 import os
