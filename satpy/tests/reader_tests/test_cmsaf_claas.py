b1e7dabf1192a32ad8bc215afea7ce29ee92bdae
Gerrit Holl <gerrit.holl@dwd.de>
2020-05-08 18:40:21 +0200

    Started unit test for CLAAS reader
    Started tests for CLAASv2 reader, still work in progress.

---
+++ satpy/tests/reader_tests/test_cmsaf_claas.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Satpy developers
+#
+# This file is part of satpy.
+#
+# satpy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# satpy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# satpy.  If not, see <http://www.gnu.org/licenses/>.
+"""Tests for the 'claasv2' reader."""
+
+import os
+import numpy as np
+import xarray as xr
+import numpy.testing
+import pytest
+from unittest import mock
+from satpy.tests.reader_tests.test_netcdf_utils import FakeNetCDF4FileHandler
+
+
+class FakeNetCDF4FileHandler2(FakeNetCDF4FileHandler):
+    """Class for faking the NetCDF4 Filehandler."""
+
+    def _get_global_attributes(self):
+        data = {}
+        attrs = {
+                "CMSAF_proj4_params": "+a=6378169.0 +h=35785831.0 "
+                                      "+b=6356583.8 +lon_0=0 +proj=geos",
+                "CMSAF_area_extent": np.array(
+                    [-5456233.41938636, -5453233.01608472,
+                     5453233.01608472, 5456233.41938636])
+                }
+        for (k, v) in attrs.items():
+            data["/attr/" + k] = v
+        return data
+
+    def _get_data(self):
+        data = {
+                "cph": xr.DataArray(
+                    np.empty((self._nrows, self._ncols)), dims=("y", "x")),
+                "ctt": xr.DataArray(
+                    np.empty((self._nrows, self._ncols)), dims=("y", "x")),
+                "time_bnds": xr.DataArray(
+                    [[12436.91666667, 12436.92534722]],
+                    dims=("time", "time_bnds"))}
+        return data
+
+    def _get_dimensions(self):
+        data = {
+                "/dimension/x": self._nrows,
+                "/dimension/y": self._ncols,
+                "/dimension/time": 1,
+                "/dimension/time_bnds": 2,
+                }
+        return data
+
+    def get_test_content(self, filename, filename_info, filetype_info):
+        """Get the content of the test data."""
+        # mock global attributes
+        # - root groups global
+        # - other groups global
+        # mock data variables
+        # mock dimensions
+        #
+        # ... but only what satpy is using ...
+
+        D = {}
+        D.update(self._get_data())
+        D.update(self._get_dimensions())
+        D.update(self._get_global_attributes())
+        return D
+
+
+@pytest.fixture
+def reader_configs():
+    """Return reader configs for FCI."""
+
+    from satpy.config import config_search_paths
+    return config_search_paths(
+        os.path.join("readers", "cmsaf_claas_v2.yaml"))
+
+
+class TestClaasv2Readerr:
+    """Test for the CLAAS v2 reader."""
+
+    yaml_file = "cmsaf_claas_v2.yaml"
+
+    _alt_handler = FakeNetCDF4FileHandler2
+
+    @pytest.fixture(autouse=True, scope="class")
+    def fake_handler(self):
+        """Wrap NetCDF4 FileHandler with our own fake handler."""
+        # implementation strongly inspired by test_viirs_l1b.py
+        from satpy.readers.cmsaf_claas_v2 import Claasv2
+        p = mock.patch.object(
+                Claasv2,
+                "__bases__",
+                (self._alt_handler,))
+        with p:
+            p.is_local = True
+            yield p
+
+    def test_file_pattern(self, reader_configs):
+        """Test file pattern matching."""
+        from satpy.readers import load_reader
+
+        filenames = [
+                "CTXin20040120091500305SVMSG01MD.nc",
+                "CTXin20040120093000305SVMSG01MD.nc",
+                "CTXin20040120094500305SVMSG01MD.nc",
+                "abcde52034294023489248MVSSG03DD.nc"]
+
+        reader = load_reader(reader_configs)
+        files = reader.select_files_from_pathnames(filenames)
+        # only 3 out of 4 above should match
+        assert len(files) == 3
+
+    def test_load(self, reader_configs):
+        """Test loading."""
+        from satpy import DatasetID
+        from satpy.readers import load_reader
+
+        # testing two filenames to test correctly combined
+        filenames = [
+            "CTXin20040120091500305SVMSG01MD.nc",
+            "CTXin20040120093000305SVMSG01MD.nc"]
+
+        reader = load_reader(reader_configs)
+        loadables = reader.select_files_from_pathnames(filenames)
+        reader.create_filehandlers(loadables)
+        res = reader.load(
+                [DatasetID(name=name) for name in ["cph", "ctt"]])
+        assert 2 == len(res)
