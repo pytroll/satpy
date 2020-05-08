e5dec1ed351bf5417d480a418bd1b72e76f1fce2
andream <andrea.meraner@eumetsat.int>
2020-05-08 10:42:53 +0000

    undo flake8/Pycharm changes to other code parts

--- satpy/scene.py
+++ satpy/scene.py
@@ -882,7 +882,8 @@ class Scene(MetadataObject):
         """Compute all the composites contained in `requirements`."""
         if nodes is None:
             required_nodes = self.wishlist - set(self.datasets.keys())
-            nodes = set(self.dep_tree.trunk(nodes=required_nodes)) - set(self.datasets.keys())
+            nodes = set(self.dep_tree.trunk(nodes=required_nodes)) - \
+                set(self.datasets.keys())
         return self._read_composites(nodes)
 
     def _remove_failed_datasets(self, keepables):
