141fd40cf75d8376a136b43e2b3168e4b5edbe2a
Martin Raspaud <martin.raspaud@smhi.se>
2020-06-09 13:03:24 +0200

    Make grib reader use DataQuery

--- satpy/readers/grib.py
+++ satpy/readers/grib.py
@@ -32,7 +32,7 @@ from datetime import datetime
 
 from satpy import CHUNK_SIZE
 from satpy.readers.file_handlers import BaseFileHandler
-from satpy.dataset import ModifierTuple, DataID
+from satpy.dataset import DataQuery
 import pygrib
 
 LOG = logging.getLogger(__name__)
@@ -41,23 +41,6 @@ LOG = logging.getLogger(__name__)
 CF_UNITS = {
     'none': '1',
 }
-
-grib_id_keys_config = {'name': {
-    'required': True,
-},
-    'level': None,
-    'resolution': None,
-    'modifiers': {
-    'required': True,
-    'default': ModifierTuple(),
-    'type': ModifierTuple,
-},
-}
-
-
-def make_dsid(**items):
-    """Make a data id."""
-    return DataID(grib_id_keys_config, **items)
 
 
 class GRIBFileHandler(BaseFileHandler):
@@ -93,8 +76,9 @@ class GRIBFileHandler(BaseFileHandler):
     def _analyze_messages(self, grib_file):
         grib_file.seek(0)
         for idx, msg in enumerate(grib_file):
-            msg_id = make_dsid(name=msg['shortName'],
-                               level=msg['level'])
+            msg_id = DataQuery(name=msg['shortName'],
+                               level=msg['level'],
+                               modifiers=tuple())
             ds_info = {
                 'message': idx + 1,
                 'name': msg['shortName'],
@@ -110,7 +94,7 @@ class GRIBFileHandler(BaseFileHandler):
             id_keys = [keys[k]['id_key'] for k in ordered_keys]
             msg_info = dict(zip(ordered_keys, id_vals))
             ds_info = dict(zip(id_keys, id_vals))
-            msg_id = make_dsid(**ds_info)
+            msg_id = DataQuery(**ds_info)
             ds_info = msg_id.to_dict()
             ds_info.update(msg_info)
             ds_info['file_type'] = self.filetype_info['file_type']
