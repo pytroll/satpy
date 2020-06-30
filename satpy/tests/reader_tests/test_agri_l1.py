7e2f13af926feea0aa0ab46ea8bfc2dfa71fc7b4
Xin Zhang <xinzhang1215@gmail.com>
2020-06-30 16:38:45 +0800

    Set the actual input and check the orbital_parameters type

--- satpy/tests/reader_tests/test_agri_l1.py
+++ satpy/tests/reader_tests/test_agri_l1.py
@@ -184,16 +184,16 @@ class FakeHDF5FileHandler2(FakeHDF5FileHandler):
     def get_test_content(self, filename, filename_info, filetype_info):
         """Mimic reader input file content."""
         global_attrs = {
-            '/attr/NOMCenterLat': np.array([0.0]),
-            '/attr/NOMCenterLon': np.array([104.7]),
-            '/attr/NOMSatHeight': np.array([3.5786E7]),
-            '/attr/dEA': np.array([6378.14]),
-            '/attr/dObRecFlat': np.array([298.257223563]),
+            '/attr/NOMCenterLat': np.array(0.0),
+            '/attr/NOMCenterLon': np.array(104.7),
+            '/attr/NOMSatHeight': np.array(3.5786E7),
+            '/attr/dEA': np.array(6378.14),
+            '/attr/dObRecFlat': np.array(298.257223563),
             '/attr/OBIType': 'REGC',
-            '/attr/RegLength': np.array([2.0]),
-            '/attr/RegWidth': np.array([5.0]),
-            '/attr/Begin Line Number': np.array([0]),
-            '/attr/End Line Number': np.array([1]),
+            '/attr/RegLength': np.array(2.0),
+            '/attr/RegWidth': np.array(5.0),
+            '/attr/Begin Line Number': np.array(0),
+            '/attr/End Line Number': np.array(1),
             '/attr/Observing Beginning Date': '2019-06-03', '/attr/Observing Beginning Time': '00:30:01.807',
             '/attr/Observing Ending Date': '2019-06-03', '/attr/Observing Ending Time': '00:34:07.572',
             '/attr/Satellite Name': 'FY4A', '/attr/Sensor Identification Code': 'AGRI', '/attr/Sensor Name': 'AGRI',
@@ -294,6 +294,8 @@ class Test_HDF_AGRI_L1_cal(unittest.TestCase):
 
         # check whether the data type of orbital_parameters is float
         orbital_parameters = res[band_names[0]].attrs['orbital_parameters']
+        for attr in orbital_parameters:
+            self.assertEqual(type(orbital_parameters[attr]), float)
         self.assertEqual(orbital_parameters['satellite_nominal_latitude'], 0.)
         self.assertEqual(orbital_parameters['satellite_nominal_longitude'], 104.7)
         self.assertEqual(orbital_parameters['satellite_nominal_altitude'], 3.5786E7)
