97b82466b1ed57057337795a553d50cd4cf098e7
andream <andrea.meraner@eumetsat.int>
2020-07-23 17:04:39 +0200

    replace interleaving with stacking and reshaping

--- satpy/readers/seviri_l1b_native.py
+++ satpy/readers/seviri_l1b_native.py
@@ -443,13 +443,7 @@ class NativeMSGFileHandler(BaseFileHandler, SEVIRICalibrationHandler):
             data0 = dec10216(raw0.flatten())
             data0 = data0.reshape(shape_layer)
 
-            data = np.zeros(shape)
-            idx = range(0, shape[0], 3)
-            data[idx, :] = data0
-            idx = range(1, shape[0], 3)
-            data[idx, :] = data1
-            idx = range(2, shape[0], 3)
-            data[idx, :] = data2
+            data = da.stack((data0, data1, data2), axis=1).reshape(shape)
 
         xarr = xr.DataArray(data, dims=['y', 'x']).where(data != 0).astype(np.float32)
 
