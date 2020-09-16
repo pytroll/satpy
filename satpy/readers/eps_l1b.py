b6236e99a66f7e11fc1afa37c0760ccc11a6f65a
Martin Raspaud <martin.raspaud@smhi.se>
2020-09-16 10:57:54 +0200

    Minimise delayed code in eps reader

--- satpy/readers/eps_l1b.py
+++ satpy/readers/eps_l1b.py
@@ -187,17 +187,20 @@ class EPSAVHRRFile(BaseFileHandler):
             keys += val.dtype.fields.keys()
         return keys
 
-    @delayed(nout=2, pure=True)
     def _get_full_lonlats(self, lons, lats):
         nav_sample_rate = self["NAV_SAMPLE_RATE"]
         if nav_sample_rate == 20 and self.pixels == 2048:
-            from geotiepoints import metop20kmto1km
-            return metop20kmto1km(lons, lats)
+            return self._interpolate_20km_to_1km(lons, lats)
         else:
             raise NotImplementedError("Lon/lat expansion not implemented for " +
                                       "sample rate = " + str(nav_sample_rate) +
                                       " and earth views = " +
                                       str(self.pixels))
+
+    @delayed(nout=2, pure=True)
+    def _interpolate_20km_to_1km(self, lons, lats):
+        from geotiepoints import metop20kmto1km
+        return metop20kmto1km(lons, lats)
 
     def get_full_lonlats(self):
         """Get the interpolated lons/lats."""
