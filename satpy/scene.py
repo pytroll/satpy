a01e1e10743a145211865d211fd2f5d7c8efa8b9
Martin Raspaud <martin.raspaud@smhi.se>
2020-12-16 14:41:39 +0100

    Add aggregation tests

--- satpy/scene.py
+++ satpy/scene.py
@@ -640,8 +640,8 @@ class Scene:
                            'mean', 'sum', 'min', 'max', 'median', 'argmin',
                            'argmax', 'prod', 'std', 'var'.
                            'mean' is the default.
-            boundary: Not implemented.
-            side: Not implemented.
+            boundary: See xarray.DataArray.coarsen, 'trim' by default.
+            side: See xarray.DataArray.coarsen, 'left' by default.
             dim_kwargs: the size of the windows to aggregate.
 
         Returns:
