dff630add9f53ac79d5453d9e174aff4595600a0
Eysteinn <eysteinn@vedur.is>
2021-01-21 12:19:57 +0000

    Fix flake8 warnings.

--- satpy/tests/reader_tests/test_ascat_l2_soilmoisture_bufr.py
+++ satpy/tests/reader_tests/test_ascat_l2_soilmoisture_bufr.py
@@ -36,7 +36,7 @@ def create_message():
     lat, lon = np.meshgrid(np.linspace(63, 65, nlat), np.linspace(-30, -20, nlon))
     lat = np.round(np.ravel(lat), 4)
     lon = np.round(np.ravel(lon), 4)
-    rstate=np.random.RandomState(0)
+    rstate = np.random.RandomState(0)
     surfaceSoilMoisture = np.round(rstate.rand(samples)*100, 1)
     surfaceSoilMoisture[0] = -1e+100
     retmsg = {
