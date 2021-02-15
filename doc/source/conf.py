50ddcc6c0c1a5fc3495f874c252626946dae0096
David Hoese <david.hoese@ssec.wisc.edu>
2021-02-15 11:27:51 -0600

    Fix deprecated sphinx html_context usage in conf.py (#1545)
    * Fix deprecated sphinx html_context usage in conf.py
    
    * Fix job step names in website building in CI

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -180,11 +180,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
+html_css_files = [
+    'theme_overrides.css',  # override wide tables in RTD theme
+]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
