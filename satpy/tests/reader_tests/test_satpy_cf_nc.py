db34a63c85ea5c2efb453f804cc514f91783e050
Trygve Aspenes <trygveas@met.no>
2021-03-01 20:17:40 +0100

    Fix and add test

--- satpy/tests/reader_tests/test_satpy_cf_nc.py
+++ satpy/tests/reader_tests/test_satpy_cf_nc.py
@@ -267,7 +267,7 @@ class TestCFReader(unittest.TestCase):
     def test_read_prefixed_channels_by_user_include_prefix(self):
         """Check channels starting with digit is prefixed by user and include original name when saving."""
         scene = self._dataset_for_prefix_testing()
-        filename = 'testingcfwriter{:s}-viirs-mband-20201007075915-20201007080744.nc'.format(
+        filename = 'testingcfwriter2{:s}-viirs-mband-20201007075915-20201007080744.nc'.format(
             datetime.utcnow().strftime('%Y%j%H%M%S'))
         try:
             scene.save_datasets(writer='cf',
@@ -278,6 +278,29 @@ class TestCFReader(unittest.TestCase):
                                 pretty=True,
                                 include_orig_name=True,
                                 numeric_name_prefix='USER')
+            scn_ = Scene(reader='satpy_cf_nc',
+                         filenames=[filename])
+            scn_.load(['1'])
+            self.assertTrue(np.all(scn_['1'].data == scene['1'].data))
+            self.assertTrue(np.all(scn_['1'].coords['lon'] == scene['lon'].data))  # lon loded as coord
+
+        finally:
+            with suppress(PermissionError):
+                os.remove(filename)
+
+    def test_read_prefixed_channels_by_user_no_prefix(self):
+        """Check channels starting with digit is not prefixed by user."""
+        scene = self._dataset_for_prefix_testing()
+        filename = 'testingcfwriter2{:s}-viirs-mband-20201007075915-20201007080744.nc'.format(
+            datetime.utcnow().strftime('%Y%j%H%M%S'))
+        try:
+            scene.save_datasets(writer='cf',
+                                filename=filename,
+                                header_attrs={'instrument': 'avhrr'},
+                                engine='netcdf4',
+                                flatten_attrs=True,
+                                pretty=True,
+                                numeric_name_prefix='')
             scn_ = Scene(reader='satpy_cf_nc',
                          filenames=[filename])
             scn_.load(['1'])
