6666c4b15c1ba32d9408b1a4c354246b631512ea
ro <mail2benr@gmail.com>
2021-04-12 16:24:31 +0200

    refactor: add instrument to root attrs

--- satpy/writers/cf_writer.py
+++ satpy/writers/cf_writer.py
@@ -726,6 +726,10 @@ class CFWriter(Writer):
             if flatten_attrs:
                 header_attrs = flatten_dict(header_attrs)
             root.attrs = encode_attrs_nc(header_attrs)
+
+        # Add header attributes needed for satpy_cf_reader
+        root.attrs["instrument"] = datasets[0].attrs["sensor"]
+
         _history_create = 'Created by pytroll/satpy on {}'.format(datetime.utcnow())
         if 'history' in root.attrs:
             if isinstance(root.attrs['history'], list):
