21c9619f30a84472b8baeccbec1628fa8e2e78d3
Gerrit Holl <gerrit.holl@dwd.de>
2021-07-16 10:40:17 +0200

    Fix FCI benchmarks
    Add netCDF4 dependency to asv.conf.json and extend some of the benchmark
    tests, which appear to work now.  Seems to run with ``asv run -q --bench
    "fci_benchmarks.FCI.*"  benchmark-fci``, at least for testing purposes.

--- benchmarks/fci_benchmarks.py
+++ benchmarks/fci_benchmarks.py
@@ -22,6 +22,9 @@ Benchmarks for reading and processing data from the Meteosat Third Generation
 published by EUMETSAT in 2020.
 """
 
+import fnmatch
+import os
+
 import satpy
 import satpy.demo
 
@@ -32,30 +35,37 @@ class FCI:
     timeout = 600
     data_files = []
 
-    def setup_cache(self):
+    def setup(self):
         """Fetch the data files."""
         fns = self.get_filenames()
         cnt = len(fns)
-        if cnt == 41:
-            self.filenames = fns
-        elif cnt < 41:
-            self.filenames = satpy.demo.download_fci_test_data()
-        else:
+        if cnt > 41:
             raise ValueError(f"Expected 41 files, found {cnt:d}")
+        elif cnt < 41:
+            fns = satpy.demo.download_fci_test_data()
+        self.filenames = fnmatch.filter(fns, "*-CHK-BODY-*")
 
     def get_filenames(self):
         """Get filenames of FCI test data as already available."""
         p = satpy.demo._get_fci_test_data_dir()
         g = p.glob("UNCOMPRESSED/NOMINAL/*.nc")
-        return list(g)
+        return [os.fspath(fn) for fn in g]
 
-    def time_create_scene(self):
+    def time_create_scene_single_chunk(self):
+        """Time to create a scene with a single chunk."""
+        self.create_scene(slice(20, 21))
+
+    def time_create_scene_all_chunks(self):
         """Time to create a scene with all chunks."""
-        self.create_scene()
+        self.create_scene(slice(None))
 
-    def peakmem_create_scene(self):
-        """Peak memory usage of creating a scene."""
-        self.create_scene()
+    def peakmem_create_scene_single_chunk(self):
+        """Peak memory usage to create a scene with a single chunk."""
+        self.create_scene(slice(20, 21))
+
+    def peakmem_create_scene_all_chunks(self):
+        """Peak memory usage of creating a scene with all chunks."""
+        self.create_scene(slice(None))
 
     def time_load_channel(self):
         """Time to create a scene and load one channel."""
@@ -64,6 +74,16 @@ class FCI:
     def peakmem_load_channel(self):
         """Peak memory for creating a scene and loading one channel."""
         self.load_channel()
+
+    def time_compute_channel(self):
+        """Time to create a scene and load and compute one channel."""
+        sc = self.load_channel()
+        sc["ir_105"].compute()
+
+    def peakmem_compute_channel(self):
+        """Peak memory for creating a scene and loading and computing one channel."""
+        sc = self.load_channel()
+        sc["ir_105"].compute()
 
     def time_load_composite(self):
         """Time to create a scene and load a composite."""
@@ -74,22 +94,23 @@ class FCI:
         self.load_composite()
 
     def time_load_resample_save(self):
-        """Time to load, resample, and save."""
+        """Time to load all chunks, resample, and save."""
         self.load_resample_save()
 
-    def create_scene(self):
+    def create_scene(self, selection):
         """Create a scene with FCI, and return it."""
-        return satpy.Scene(filenames=self.filenames, reader="fci_l1c_nc")
+        return satpy.Scene(filenames=self.filenames[selection], reader="fci_l1c_nc")
 
     def load_channel(self):
         """Return a FCI scene with a loaded channel."""
-        sc = self.create_scene()
+        sc = self.create_scene(slice(None))
         sc.load(["ir_105"])
+        return sc
 
     def load_composite(self):
         """Return a FCI scene with a loaded composite."""
-        sc = self.create_scene()
-        sc.load(["overview"])
+        sc = self.create_scene(slice(None))
+        sc.load(["natural_color_raw"])
         return sc
 
     def load_resample_save(self):
