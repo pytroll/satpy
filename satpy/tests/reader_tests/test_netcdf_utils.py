9bc7531f64e2c216b212262bc1303ab8603cf0c4
tsukada-cs <tsukada.cs@gmail.com>
2021-07-23 16:18:34 +0900

    add test

--- satpy/tests/reader_tests/test_netcdf_utils.py
+++ satpy/tests/reader_tests/test_netcdf_utils.py
@@ -145,6 +145,16 @@ class TestNetCDF4FileHandler(unittest.TestCase):
         self.assertEqual(file_handler['/attr/test_attr_int'], 0)
         self.assertEqual(file_handler['/attr/test_attr_float'], 1.2)
 
+        global_attrs = {
+            '/attr/test_attr_str': 'test_string',
+            '/attr/test_attr_str_arr': 'test_string2',
+            '/attr/test_attr_int': 0,
+            '/attr/test_attr_float': 1.2
+            }
+        self.assertEqual(file_handler['/attr'], global_attrs)
+        self.assertEqual(file_handler['/attrs'], global_attrs)
+
+
         self.assertIsInstance(file_handler.get('ds2_f')[:], xr.DataArray)
         self.assertIsNone(file_handler.get('fake_ds'))
         self.assertEqual(file_handler.get('fake_ds', 'test'), 'test')
