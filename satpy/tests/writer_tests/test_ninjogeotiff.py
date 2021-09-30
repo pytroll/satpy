1d673bee8136e18213339dfdbef13d81e5de3b8b
Gerrit Holl <gerrit.holl@dwd.de>
2021-09-30 17:56:44 +0200

    Set up skeleton for NinJoGeoTIFF class
    Set up a first skeleton for the NinJoGeoTIFF class, so far with mostly a
    dummy implementation.  Feedback at this stage welcome.

--- satpy/tests/writer_tests/test_ninjogeotiff.py
+++ satpy/tests/writer_tests/test_ninjogeotiff.py
@@ -28,35 +28,109 @@ import xarray as xr
 @pytest.fixture
 def fake_datasets():
     """Create fake datasets for testing writing routines."""
+    from pyresample import create_area_def
+    shp = (100, 200)
+    fake_area = create_area_def(
+        "test-area",
+        {"proj": "eqc", "lat_ts": 0, "lat_0": 0, "lon_0": 0,
+         "x_0": 0, "y_0": 0, "ellps": "sphere", "units": "m",
+         "no_defs": None, "type": "crs"},
+        units="m",
+        shape=shp,
+        resolution=1000,
+        center=(0, 0))
     return [xr.DataArray(
-        da.zeros((100, 200), chunks=50),
+        da.zeros(shp, chunks=50),
         dims=("y", "x"),
         attrs={"name": "test",
-               "start_time": datetime.datetime(1985, 8, 13, 15, 0)})]
+               "start_time": datetime.datetime(1985, 8, 13, 15, 0),
+               "area": fake_area})]
+
+
+@pytest.fixture
+def ntg(fake_datasets):
+    """Create instance of NinJoTagGenerator class."""
+    from satpy.writers.ninjogeotiff import NinJoTagGenerator
+    return NinJoTagGenerator(
+            fake_datasets[0],
+            {"ChannelID": 900015,
+             "DataSource": "FIXME",
+             "DataType": "GPRN",
+             "PhysicUnit": "C",
+             "SatelliteNameID": 6400014})
+
+
+exp_tags = {"AxisIntercept": -88,
+            "CentralMeridian": 0.0,
+            "ChannelID": 900015,
+            "ColorDepth": 24,
+            "CreationDateID": 1632820093,
+            "DataSource": "FIXME",
+            "DataType": "GPRN",
+            "DateID": 1623581777,
+            "EarthRadiusLarge": 6378137.0,
+            "EarthRadiusSmall": 6356752.5,
+            "FileName": "papapath.tif",
+            "Gradient": 0.5,
+            "HeaderVersion": 2,
+            "IsAtmosphereCorrected": 0,
+            "IsBlackLineCorrection": 0,
+            "IsCalibrated": 1,
+            "IsNormalized": 0,
+            "Magic": "NINJO",
+            "MaxGrayValue": 255,
+            "MeridianEast": 45.0,
+            "MeridianWest": -135.0,
+            "MinGrayValue": 0,
+            "PhysicUnit": "C",
+            "PhysicValue": "unknown",
+            "Projection": "NPOL",
+            "ReferenceLatitude1": 60.0,
+            "ReferenceLatitude2": 0.0,
+            "SatelliteNameID": 6400014,
+            "TransparentPixel": 0,
+            "XMaximum": 200,
+            "XMinimum": 1,
+            "YMaximum": 100,
+            "YMinimum": 1}
 
 
 def test_ninjogeotiff(fake_datasets):
     """Test that it writes a GeoTIFF with the appropriate NinJo-tags."""
     from satpy.writers.ninjogeotiff import NinJoGeoTIFFWriter
     w = NinJoGeoTIFFWriter()
-    with unittest.mock.patch("satpy.writers.geotiff.GeoTIFFWriter.save_datasets") as swggs:
-        w.save_datasets(
-                fake_datasets,
+    with unittest.mock.patch("satpy.writers.geotiff.GeoTIFFWriter.save_dataset") as swggs:
+        w.save_dataset(
+                fake_datasets[0],
                 ninjo_tags=dict(
-                    physic_unit="C",
-                    sat_id="6400014",
-                    chan_id="900015",
-                    data_cat="GORN",
-                    data_source="EUMETCAST",
-                    ch_min_measurement_unit="-87.5",
-                    ch_max_measurement_unit="40"))
+                    PhysicUnit="C",
+                    SatelliteNameID=6400014,
+                    ChannelID=900015,
+                    DataType="GPRN",
+                    DataSource="FIXME"))
         swggs.assert_called_with(
-                fake_datasets,
-                tags={"ninjo_physic_unit": "C",
-                      "ninjo_sat_id": "6400014",
-                      "ninjo_chan_id": "900015",
-                      "ninjo_data_cat": "GORN",
-                      "ninjo_data_source": "EUMETCAST",
-                      "ninjo_ch_min_measurement_unit": "-87.5",
-                      "ninjo_ch_max_measurement_unit": "40",
-                      "ninjo_TransparentPixel": "0"})
+                fake_datasets[0],
+                tags={f"ninjo_{k:s}": v for (k, v) in exp_tags.items()})
+
+
+def test_calc_tags(fake_datasets):
+    """Test calculating all tags from dataset."""
+    from satpy.writers.ninjogeotiff import calc_tags_from_dataset
+    ds = fake_datasets[0]
+    tags = calc_tags_from_dataset(
+            ds,
+            {"ChannelID": 900015,
+             "DataSource": "FIXME",
+             "DataType": "GPRN",
+             "PhysicUnit": "C",
+             "SatelliteNameID": 6400014})
+    assert tags == exp_tags
+
+
+def test_calc_single_tag_by_name(ntg):
+    """Test calculating single tag from dataset."""
+    assert ntg.get_tag("Magic") == "NINJO"
+    assert ntg.get_tag("DataType") == "GPRN"
+    assert ntg.get_tag("IsCalibrated") == 1
+    with pytest.raises(ValueError):
+        ntg.get_tag("invalid")
--- satpy/writers/ninjogeotiff.py
+++ satpy/writers/ninjogeotiff.py
@@ -13,7 +13,12 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
-"""Writer for GeoTIFF images with tags for the NinJo visualization tool."""
+"""Writer for GeoTIFF images with tags for the NinJo visualization tool.
+
+Since NinJo version 7 (released spring 2022), NinJo is able to read standard
+GeoTIFF images, with required metadata encoded as a set of XML tags in the
+GDALMetadata TIFF tag.  Each of the XML tags must be prepended with 'NINJO_'.
+"""
 
 from .geotiff import GeoTIFFWriter
 
@@ -21,14 +26,189 @@ from .geotiff import GeoTIFFWriter
 class NinJoGeoTIFFWriter(GeoTIFFWriter):
     """Writer for GeoTIFFs with NinJo tags."""
 
-    def save_datasets(self, datasets, ninjo_tags):
-        """Save datasets along with NinJo tags.
+    def save_dataset(self, dataset, ninjo_tags):
+        """Save dataset along with NinJo tags.
 
-        Save datasets along with NinJo tags.  Interface as for GeoTIFF, except
+        Save dataset along with NinJo tags.  Interface as for GeoTIFF, except
         it takes an additional keyword argument ninjo_tags.  Those tags will be
         prepended with ninjo_ and added as GDALMetaData.
+
+        Args:
+            dataset (xr.DataArray): Data array to save.
+            ninjo_tags (Mapping[str, str|numeric): tags to add
         """
-        dynamic_tags = {"TransparentPixel": "0"}
-        super().save_datasets(
-                datasets,
-                tags={"ninjo_" + k: v for (k, v) in (ninjo_tags | dynamic_tags).items()})
+        tags = calc_tags_from_dataset(dataset, ninjo_tags)
+        super().save_dataset(
+                dataset,
+                tags={"ninjo_" + k: v for (k, v) in tags.items()})
+
+
+class NinJoTagGenerator:
+    """Class to calculate NinJo tags from content."""
+
+    # tags that never change
+    fixed_tags = {
+        "Magic": "NINJO",
+        "HeaderVersion": 2,
+        "XMinimum": 1,
+        "YMinimum": 1,
+        "PhysicValue": "unknown"}
+
+    # tags that should be passed directly by the user
+    passed_tags = {"ChannelID", "DataSource", "DataType", "PhysicUnit",
+                   "SatelliteNameID"}
+
+    # tags that are calculated dynamically
+    dynamic_tags = {
+        "AxisIntercept": "axis_intercept",
+        "CentralMeridian": "central_meridian",
+        "ColorDepth": "color_depth",
+        "CreationDateID": "creation_date_id",
+        "DateID": "date_id",
+        "EarthRadiusLarge": "earth_radius_large",
+        "EarthRadiusSmall": "earth_radius_small",
+        "FileName": "filename",
+        "Gradient": "gradient",
+        "IsAtmosphereCorrected": "atmosphere_corrected",
+        "IsBlackLineCorrection": "black_line_corrected",
+        "IsCalibrated": "is_calibrated",
+        "IsNormalized": "is_normalized",
+        "MaxGrayValue": "max_gray_value",
+        "MeridianEast": "meridian_east",
+        "MeridianWest": "meridian_west",
+        "MinGrayValue": "min_gray_value",
+        "Projection": "projection",
+        "ReferenceLatitude1": "ref_lat_1",
+        "ReferenceLatitude2": "ref_lat_2",
+        "TransparentPixel": "transparent_pixel",
+        "XMaximum": "xmaximum",
+        "YMaximum": "ymaximum"
+        }
+
+    def __init__(self, dataset, args):
+        """Initialise tag generator."""
+        self.dataset = dataset
+        self.args = args
+        self.tag_names = (self.fixed_tags.keys() |
+                          self.passed_tags |
+                          self.dynamic_tags.keys())
+
+    def get_all_tags(self):
+        """Get a dictionary with all tags for NinJo."""
+        D = {}
+        for tag in self.tag_names:
+            value = self.get_tag(tag)
+            D[tag] = value
+        return D
+
+    def get_tag(self, tag):
+        """Get value for NinJo tag."""
+        if tag in self.fixed_tags:
+            return self.fixed_tags[tag]
+        elif tag in self.passed_tags:
+            return self.args[tag]
+        elif tag in self.dynamic_tags:
+            return getattr(self, f"get_{self.dynamic_tags[tag]:s}")()
+        else:
+            raise ValueError(f"Unknown tag: {tag!s}")
+
+    def get_axis_intercept(self):
+        """Calculate the axis intercept."""
+        return -88  # FIXME: derive from content
+
+    def get_central_meridian(self):
+        """Calculate central meridian."""
+        return 0.0  # FIXME: derive from area
+
+    def get_color_depth(self):
+        """Return the color depth."""
+        return 24  # FIXME: derive from image type
+
+    def get_creation_date_id(self):
+        """Calculate the creation date ID."""
+        return 1632820093  # FIXME: derive from metadata
+
+    def get_date_id(self):
+        """Calculate the date ID."""
+        return 1623581777  # FIXME: derive from metadata
+
+    def get_earth_radius_large(self):
+        """Return the Earth semi-major axis."""
+        return 6378137.0  # FIXME: derive from area
+
+    def get_earth_radius_small(self):
+        """Return the Earth semi-minor axis."""
+        return 6356752.5  # FIXME: derive from area
+
+    def get_filename(self):
+        """Return the filename."""
+        return "papapath.tif"  # FIXME: derive
+
+    def get_gradient(self):
+        """Return the gradient."""
+        return 0.5  # FIXME: derive from content
+
+    def get_atmosphere_corrected(self):
+        """Return whether atmosphere is corrected."""
+        return 0  # FIXME: derive from metadata
+
+    def get_black_line_corrected(self):
+        """Return whether black line correction applied."""
+        return 0  # FIXME: derive from metadata
+
+    def get_is_calibrated(self):
+        """Return whether calibration has been applied."""
+        return 1  # FIXME: derive from metadata
+
+    def get_is_normalized(self):
+        """Return whether data have been normalized."""
+        return 0  # FIXME: derive from metadata
+
+    def get_max_gray_value(self):
+        """Calculate maximum gray value."""
+        return 255  # FIXME: calculate
+
+    def get_meridian_east(self):
+        """Calculate meridian east."""
+        return 45.0  # FIXME: derive from area
+
+    def get_meridian_west(self):
+        """Calculate meridian west."""
+        return -135.0  # FIXME: derive from area
+
+    def get_min_gray_value(self):
+        """Calculate minimum gray value."""
+        return 0  # FIXME: derive from content
+
+    def get_projection(self):
+        """Get projection."""
+        return "NPOL"  # FIXME: derive from area
+
+    def get_ref_lat_1(self):
+        """Get reference latitude one."""
+        return 60.0  # FIXME: derive from area
+
+    def get_ref_lat_2(self):
+        """Get reference latitude two."""
+        return 0  # FIXME: derive from area
+
+    def get_transparent_pixel(self):
+        """Get transparent pixel value."""
+        return 0  # FIXME: derive from arguments
+
+    def get_xmaximum(self):
+        """Get xmaximum."""
+        return self.dataset.sizes["x"]
+
+    def get_ymaximum(self):
+        """Get ymaximum."""
+        return self.dataset.sizes["y"]
+
+
+def calc_tags_from_dataset(dataset, args):
+    """Calculate NinJo tags from dataset.
+
+    For a dataset (xarray.DataArray), calculate content-dependent tags.
+    """
+    ntg = NinJoTagGenerator(dataset, args)
+    return ntg.get_all_tags()
