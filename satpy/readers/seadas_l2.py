a4879307ded06383a4b3d0f92b09f9d4ac5f7054
David Hoese <david.hoese@ssec.wisc.edu>
2021-11-22 15:50:11 -0600

    Add SEADAS L2 reader for Cholorphyll A product

---
+++ satpy/readers/seadas_l2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021 Satpy developers
+#
+# This file is part of satpy.
+#
+# satpy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# satpy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# satpy.  If not, see <http://www.gnu.org/licenses/>.
+"""Reader for SEADAS L2 products.
+
+This reader currently only supports MODIS and VIIRS Chlorophyll A from SEADAS.
+
+"""
+
+from datetime import datetime
+
+from .hdf4_utils import HDF4FileHandler
+
+TIME_FORMAT = "%Y%j%H%M%S"
+
+
+class SEADASL2HDFFileHandler(HDF4FileHandler):
+    """Simple handler of SEADAS L2 files."""
+
+    def _add_satpy_metadata(self, data):
+        data.attrs["sensor"] = self.sensor_names
+        data.attrs["platform_name"] = self._platform_name()
+        return data
+
+    def _platform_name(self):
+        platform = self["/attr/Mission"]
+        platform_dict = {'JPSS-1': 'NOAA-20',
+                         'JPSS-2': 'NOAA-21'}
+        return platform_dict.get(platform, platform)
+
+    @property
+    def start_time(self):
+        """Get the starting observation time of this file's data."""
+        start_time = self["/attr/Start Time"]
+        return datetime.strptime(start_time[:-3], TIME_FORMAT)
+
+    @property
+    def end_time(self):
+        """Get the ending observation time of this file's data."""
+        end_time = self["/attr/End Time"]
+        return datetime.strptime(end_time[:-3], TIME_FORMAT)
+
+    @property
+    def sensor_names(self):
+        """Get sensor for the current file's data."""
+        # Example: MODISA or VIIRSN or VIIRSJ1
+        sensor_name = self["/attr/Sensor Name"].lower()
+        if sensor_name.startswith("modis"):
+            return "modis"
+        return "viirs"
+
+    def get_dataset(self, data_id, dataset_info):
+        """Get DataArray for the specified DataID."""
+        file_key = dataset_info.get("file_key", data_id["name"])
+        data = self[file_key]
+        valid_range = data.attrs["valid_range"]
+        data = data.where(valid_range[0] <= data)
+        data = data.where(data <= valid_range[1])
+        data = self._add_satpy_metadata(data)
+        return data
