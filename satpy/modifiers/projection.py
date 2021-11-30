a9d5668a2c8f6aa4c763b4184ec54870c3fd53bc
Gerrit Holl <gerrit.holl@dwd.de>
2021-11-30 09:44:15 +0100

    Adapt parallax dependency from meteoswiss
    Adapt a dependency from the parallax module adopted from meteoswiss.  I
    expect adding this module is temporary.  I don't really know what it's
    for.

--- satpy/modifiers/projection.py
+++ satpy/modifiers/projection.py
@@ -1,3 +1,4 @@
+"""To be documented."""
 import warnings
 
 import numpy as np
@@ -7,10 +8,11 @@ from scipy.ndimage import map_coordinates
 
 
 def parse_area(area_def):
+    """To be documented."""
     if isinstance(area_def, AreaDefinition):
         area = area_def
     elif isinstance(area_def, dict):
-        with warnings.catch_warnings(): # suppress warnings
+        with warnings.catch_warnings():  # suppress warnings
             warnings.simplefilter("ignore", category=UserWarning)
             try:
                 area = AreaDefinition(**area_def)
@@ -19,13 +21,13 @@ def parse_area(area_def):
                 area = AreaDefinition(**area_def)
     else:
         print("*** Error, unknown area_def")
-                
-                
+
     return area
 
 
 def known_area(area_dict):
-    assert("type" in area_dict)
+    """To be documented."""
+    # assert("type" in area_dict)
     if area_dict["type"] == "swiss_ccs4":
         return ccs4_swiss_grid_area
     elif area_dict["type"] == "azimuthal_equidistant":
@@ -33,7 +35,10 @@ def known_area(area_dict):
 
 
 class GridProjection:
+    """To be documented."""
+
     def __init__(self, area_def):
+        """To be documented."""
         self.area = parse_area(area_def)
 
         self.grid_proj = pyproj.Proj(**self.area.proj_dict)
@@ -42,9 +47,10 @@ class GridProjection:
             self.latlon_proj, self.grid_proj)
 
     def __call__(self, lon, lat):
+        """To be documented."""
         # the pyproj call below does not handle empty inputs properly
-        if (not np.isscalar(lon)) and (len(lon) == 0) \
-            and (not np.isscalar(lat)) and (len(lat) == 0):
+        if ((not np.isscalar(lon)) and (len(lon) == 0)
+           and (not np.isscalar(lat)) and (len(lat) == 0)):
             return (np.copy(lon), np.copy(lat))
 
         # get cartesian projection values from longitude and latitude
@@ -54,17 +60,18 @@ class GridProjection:
         # borrowed from:
         # https://github.com/pytroll/pyresample/blob/master/pyresample/grid.py
         source_pixel_x = (self.area.pixel_offset_x +
-            source_x / self.area.pixel_size_x)
+                          source_x / self.area.pixel_size_x)
 
         source_pixel_y = (self.area.pixel_offset_y -
-            source_y / self.area.pixel_size_y)
+                          source_y / self.area.pixel_size_y)
 
         return (source_pixel_y, source_pixel_x)
 
     def inverse(self, y, x):
+        """To be documented."""
         # the pyproj call below does not handle empty inputs properly
-        if (not np.isscalar(y)) and (len(x) == 0) \
-            and (not np.isscalar(y)) and (len(x) == 0):
+        if ((not np.isscalar(y)) and (len(x) == 0)
+           and (not np.isscalar(y)) and (len(x) == 0)):
             return (np.copy(y), np.copy(x))
 
         proj_x = (x - self.area.pixel_offset_x) * self.area.pixel_size_x
@@ -72,10 +79,10 @@ class GridProjection:
 
         # get longitude and latitude from cartesian projection values
         return self.transformer.transform(proj_x, proj_y,
-            direction='inverse')
+                                          direction='inverse')
 
 
-# Adapted from: 
+# Adapted from:
 # https://github.com/meteoswiss-mdr/monti-pytroll/blob/master/etc/areas.def
 ccs4_swiss_grid_area = {
     "area_id": "ccs4",
@@ -87,13 +94,13 @@ ccs4_swiss_grid_area = {
     "area_extent": (255000.0, -160000.0, 965000.0, 480000.0)
 }
 
+
 def geostationary_area(
         *, area_id, description, a, b, lon_0, h,
-        nrow, ncol, coff, cfac, loff, lfac  
-    ):
-
+        nrow, ncol, coff, cfac, loff, lfac):
+    """To be documented."""
     # from mpop/satin/nwcsaf_msg.py
-    xur = (ncol - coff) * 2 ** 16 / (cfac * 1.0) 
+    xur = (ncol - coff) * 2 ** 16 / (cfac * 1.0)
     xur = np.deg2rad(xur) * h
     xll = (-1 - coff) * 2 ** 16 / (cfac * 1.0)
     xll = np.deg2rad(xll) * h
@@ -124,7 +131,9 @@ def geostationary_area(
     }
     return AreaDefinition(**area)
 
+
 def geostationary_area_alps():
+    """To be documented."""
     return geostationary_area(
         area_id="alps",
         description="Alps Region Geostationary Projection",
@@ -132,24 +141,23 @@ def geostationary_area_alps():
         lon_0=9.5, h=35785831.0,
         nrow=151, ncol=349,
         coff=204, cfac=13642337,
-        loff=1515, lfac=13642337        
+        loff=1515, lfac=13642337
     )
 
-def centered_aeqd_area(lon0, lat0, size, pixel_size=1000.0):
-    """Azimuthal equidistant projection centered on a given lon/lat point.
-    """
 
+def centered_aeqd_area(lon0, lat0, size, pixel_size=1000.0):
+    """Azimuthal equidistant projection centered on a given lon/lat point."""
     physical_size = (size[0]*pixel_size, size[1]*pixel_size)
 
     area = {
         "area_id": "aeqd:{:.3f}:{:.3f}:{}km:{}x{}".format(
             lon0, lat0, pixel_size, size[0], size[1]
         ),
-        "description": "Azimuthal Equidistant Projection " + \
-            "lon0={:.3f}, lat0={:.3f}, scale={}km, {}x{}".format(
+        "description": "Azimuthal Equidistant Projection "
+        "lon0={:.3f}, lat0={:.3f}, scale={}km, {}x{}".format(
                 lon0, lat0, pixel_size, size[0], size[1]
         ),
-        "proj_id": "aeqd:{:.3f}:{:.3f}".format(lon0,lat0),
+        "proj_id": "aeqd:{:.3f}:{:.3f}".format(lon0, lat0),
         "projection": {
             "proj": "aeqd",
             "lon_0": lon0,
@@ -158,9 +166,9 @@ def centered_aeqd_area(lon0, lat0, size, pixel_size=1000.0):
         "width": size[0],
         "height": size[1],
         "area_extent": (
-            -physical_size[0]/2, 
+            -physical_size[0]/2,
             -physical_size[1]/2,
-            physical_size[0]/2, 
+            physical_size[0]/2,
             physical_size[1]/2
         )
     }
@@ -169,7 +177,10 @@ def centered_aeqd_area(lon0, lat0, size, pixel_size=1000.0):
 
 
 class ImageMapper:
+    """To be documented."""
+
     def __init__(self, source_area, target_area):
+        """To be documented."""
         self.source_area = source_area
         self.target_area = target_area
         source_proj = GridProjection(source_area)
@@ -179,6 +190,7 @@ class ImageMapper:
             :target_area.width
         ]
         (self.y_tgt, self.x_tgt) = source_proj(*target_proj.inverse(y_src, x_src))
-       
+
     def __call__(self, image, **kwargs):
+        """To be documented."""
         return map_coordinates(image, (self.y_tgt, self.x_tgt), **kwargs)
