4f080e4344a023d12ef7c9a985c1362a027f54d2
simonrp84 <simon.proud@physics.ox.ac.uk>
2022-01-11 11:19:17 +0000

    Add reader for VIIRS level 2 products produced by the NOAA enterprise suite.

---
+++ satpy/readers/viirs_jrr.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022 Satpy developers
+#
+# This file is part of satpy.
+#
+# satpy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# satpy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# satpy.  If not, see <http://www.gnu.org/licenses/>.
+"""VIIRS NOAA enterprise L2 product reader.
+
+This module implements readers for the NOAA enterprise level 2 products for the
+VIIRS instrument. These replace the 'old' EDR products.
+"""
+
+import logging
+
+from satpy import CHUNK_SIZE
+from satpy.readers.file_handlers import BaseFileHandler
+
+import dask.array as da
+import numpy as np
+import xarray as xr
+
+# map platform attributes to Oscar standard name
+PLATFORM_MAP = {
+    "NPP": "Suomi-NPP",
+    "J01": "NOAA-20",
+    "J02": "NOAA-21"
+}
+
+LOG = logging.getLogger(__name__)
+
+
+class VIIRSJRRFileHandler(BaseFileHandler):
+    """NetCDF4 reader for VIIRS Active Fires."""
+
+    def __init__(self, filename, filename_info, filetype_info):
+        """Initialize the geo filehandler."""
+        super(VIIRSJRRFileHandler, self).__init__(filename, filename_info,
+                                                  filetype_info)
+        self.nc = xr.open_dataset(self.filename,
+                                  decode_cf=True,
+                                  mask_and_scale=True,
+                                  chunks={'Columns': CHUNK_SIZE,
+                                          'Rows': CHUNK_SIZE})
+        self.nc = self.nc.rename({'Columns': 'x', 'Rows': 'y'})
+        if 'Latitude' in self.nc:
+            self.nc['Latitude'].attrs.update({'standard_name': 'latitude'})
+        if 'Longitude' in self.nc:
+            self.nc['Longitude'].attrs.update({'standard_name': 'longitude'})
+
+    def get_dataset(self, dataset_id, info):
+        """Get the dataset."""
+        ds = self.nc[info['file_key']]
+
+        return ds
+
+    @property
+    def start_time(self):
+        """Get first date/time when observations were recorded."""
+        return self.filename_info['start_time']
+
+    @property
+    def end_time(self):
+        """Get last date/time when observations were recorded."""
+        return self.filename_info.get('end_time', self.start_time)
+
+    @property
+    def sensor_name(self):
+        """Name of sensor for this file."""
+        return self["sensor"]
+
+    @property
+    def platform_name(self):
+        """Name of platform/satellite for this file."""
+        return self["platform_name"]
