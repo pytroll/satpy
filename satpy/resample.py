6f31f377dacdf5c177da131bf2792a3e72692365
David Hoese <david.hoese@ssec.wisc.edu>
2022-01-24 11:11:57 -0600

    More native resampler refactoring

--- satpy/resample.py
+++ satpy/resample.py
@@ -1006,6 +1006,35 @@ class NativeResampler(BaseResampler):
                                   meta=np.array((), dtype=d.dtype),
                                   dtype=d.dtype, chunks=new_chunks)
 
+    @staticmethod
+    def replicate(d_arr, repeats):
+        """Repeat data pixels by the per-axis factors specified."""
+        # rechunk so new chunks are the same size as old chunks
+        c_size = max(x[0] for x in d_arr.chunks)
+
+        def _calc_chunks(c, c_size):
+            whole_chunks = [c_size] * int(sum(c) // c_size)
+            remaining = sum(c) - sum(whole_chunks)
+            if remaining:
+                whole_chunks += [remaining]
+            return tuple(whole_chunks)
+        new_chunks = [_calc_chunks(x, int(c_size // repeats[axis]))
+                      for axis, x in enumerate(d_arr.chunks)]
+        d_arr = d_arr.rechunk(new_chunks)
+
+        repeated_chunks = []
+        for axis, axis_chunks in enumerate(d_arr.chunks):
+            factor = repeats[axis]
+            if not factor.is_integer():
+                raise ValueError("Expand factor must be a whole number")
+            repeated_chunks.append(tuple(x * int(factor) for x in axis_chunks))
+        repeated_chunks = tuple(repeated_chunks)
+        d_arr = d_arr.map_blocks(_repeat_by_factor,
+                                 meta=np.array((), dtype=d_arr.dtype),
+                                 dtype=d_arr.dtype,
+                                 chunks=repeated_chunks)
+        return d_arr
+
     @classmethod
     def expand_reduce(cls, d_arr, repeats):
         """Expand reduce."""
@@ -1014,31 +1043,7 @@ class NativeResampler(BaseResampler):
         if all(x == 1 for x in repeats.values()):
             return d_arr
         if all(x >= 1 for x in repeats.values()):
-            # rechunk so new chunks are the same size as old chunks
-            c_size = max(x[0] for x in d_arr.chunks)
-
-            def _calc_chunks(c, c_size):
-                whole_chunks = [c_size] * int(sum(c) // c_size)
-                remaining = sum(c) - sum(whole_chunks)
-                if remaining:
-                    whole_chunks += [remaining]
-                return tuple(whole_chunks)
-            new_chunks = [_calc_chunks(x, int(c_size // repeats[axis]))
-                          for axis, x in enumerate(d_arr.chunks)]
-            d_arr = d_arr.rechunk(new_chunks)
-
-            repeated_chunks = []
-            for axis, axis_chunks in enumerate(d_arr.chunks):
-                factor = repeats[axis]
-                if not factor.is_integer():
-                    raise ValueError("Expand factor must be a whole number")
-                repeated_chunks.append(tuple(x * int(factor) for x in axis_chunks))
-            repeated_chunks = tuple(repeated_chunks)
-            d_arr = d_arr.map_blocks(_repeat_by_factor,
-                                     meta=np.array((), dtype=d_arr.dtype),
-                                     dtype=d_arr.dtype,
-                                     chunks=repeated_chunks)
-            return d_arr
+            return cls.replicate(d_arr, repeats)
         if all(x <= 1 for x in repeats.values()):
             # reduce
             y_size = 1. / repeats[0]
