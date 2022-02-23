c4c051a1a0fca7bcdd3947460ab22b286ab13b6e
David Hoese <david.hoese@ssec.wisc.edu>
2022-02-23 13:58:27 -0600

    Move more ahi hsd tests to pytest

--- satpy/tests/reader_tests/test_ahi_hsd.py
+++ satpy/tests/reader_tests/test_ahi_hsd.py
@@ -298,59 +298,53 @@ class TestAHIHSDFileHandler:
                 ref_mask = np.logical_not(get_geostationary_mask(fh.area).compute())
                 np.testing.assert_equal(mask, ref_mask)
 
-
-class TestAHIHSDFileHandlerUnittest(unittest.TestCase):
-    """Test case for the file reading using unittest."""
-
-    def setUp(self):
-        """Create a test file handler."""
-        with _fake_hsd_handler() as fh:
-            self.fh = fh
-
     def test_time_properties(self):
         """Test start/end/scheduled time properties."""
-        self.assertEqual(self.fh.start_time, datetime(2018, 10, 22, 3, 0))
-        self.assertEqual(self.fh.end_time, datetime(2018, 10, 22, 3, 0))
-        self.assertEqual(self.fh.observation_start_time, datetime(2018, 10, 22, 3, 0, 20, 596896))
-        self.assertEqual(self.fh.observation_end_time, datetime(2018, 10, 22, 3, 0, 53, 947296))
-        self.assertEqual(self.fh.scheduled_start_time, datetime(2018, 10, 22, 3, 0, 0, 0))
-        self.assertEqual(self.fh.scheduled_end_time, datetime(2018, 10, 22, 3, 0, 0, 0))
+        with _fake_hsd_handler() as fh:
+            assert fh.start_time == datetime(2018, 10, 22, 3, 0)
+            assert fh.end_time == datetime(2018, 10, 22, 3, 0)
+            assert fh.observation_start_time == datetime(2018, 10, 22, 3, 0, 20, 596896)
+            assert fh.observation_end_time == datetime(2018, 10, 22, 3, 0, 53, 947296)
+            assert fh.scheduled_start_time == datetime(2018, 10, 22, 3, 0, 0, 0)
+            assert fh.scheduled_end_time == datetime(2018, 10, 22, 3, 0, 0, 0)
 
     def test_scanning_frequencies(self):
         """Test scanning frequencies."""
-        self.fh.observation_area = 'JP04'
-        self.assertEqual(self.fh.scheduled_start_time, datetime(2018, 10, 22, 3, 7, 30, 0))
-        self.assertEqual(self.fh.scheduled_end_time, datetime(2018, 10, 22, 3, 7, 30, 0))
-        self.fh.observation_area = 'R304'
-        self.assertEqual(self.fh.scheduled_start_time, datetime(2018, 10, 22, 3, 7, 30, 0))
-        self.assertEqual(self.fh.scheduled_end_time, datetime(2018, 10, 22, 3, 7, 30, 0))
-        self.fh.observation_area = 'R420'
-        self.assertEqual(self.fh.scheduled_start_time, datetime(2018, 10, 22, 3, 9, 30, 0))
-        self.assertEqual(self.fh.scheduled_end_time, datetime(2018, 10, 22, 3, 9, 30, 0))
-        self.fh.observation_area = 'R520'
-        self.assertEqual(self.fh.scheduled_start_time, datetime(2018, 10, 22, 3, 9, 30, 0))
-        self.assertEqual(self.fh.scheduled_end_time, datetime(2018, 10, 22, 3, 9, 30, 0))
-        self.fh.observation_area = 'FLDK'
-        self.assertEqual(self.fh.scheduled_start_time, datetime(2018, 10, 22, 3, 0, 0, 0))
-        self.assertEqual(self.fh.scheduled_end_time, datetime(2018, 10, 22, 3, 0, 0, 0))
+        with _fake_hsd_handler() as fh:
+            fh.observation_area = 'JP04'
+            assert fh.scheduled_start_time == datetime(2018, 10, 22, 3, 7, 30, 0)
+            assert fh.scheduled_end_time == datetime(2018, 10, 22, 3, 7, 30, 0)
+            fh.observation_area = 'R304'
+            assert fh.scheduled_start_time == datetime(2018, 10, 22, 3, 7, 30, 0)
+            assert fh.scheduled_end_time == datetime(2018, 10, 22, 3, 7, 30, 0)
+            fh.observation_area = 'R420'
+            assert fh.scheduled_start_time == datetime(2018, 10, 22, 3, 9, 30, 0)
+            assert fh.scheduled_end_time == datetime(2018, 10, 22, 3, 9, 30, 0)
+            fh.observation_area = 'R520'
+            assert fh.scheduled_start_time == datetime(2018, 10, 22, 3, 9, 30, 0)
+            assert fh.scheduled_end_time == datetime(2018, 10, 22, 3, 9, 30, 0)
+            fh.observation_area = 'FLDK'
+            assert fh.scheduled_start_time == datetime(2018, 10, 22, 3, 0, 0, 0)
+            assert fh.scheduled_end_time == datetime(2018, 10, 22, 3, 0, 0, 0)
 
     def test_blocklen_error(self, *mocks):
         """Test erraneous blocklength."""
         open_name = '%s.open' % __name__
         fpos = 50
-        with mock.patch(open_name, create=True) as mock_open:
-            with mock_open(mock.MagicMock(), 'r') as fp_:
-                # Expected and actual blocklength match
-                fp_.tell.return_value = 50
-                with warnings.catch_warnings(record=True) as w:
-                    self.fh._check_fpos(fp_, fpos, 0, 'header 1')
-                    self.assertTrue(len(w) == 0)
+        with _fake_hsd_handler() as fh, \
+                mock.patch(open_name, create=True) as mock_open, \
+                mock_open(mock.MagicMock(), 'r') as fp_:
+            # Expected and actual blocklength match
+            fp_.tell.return_value = 50
+            with warnings.catch_warnings(record=True) as w:
+                fh._check_fpos(fp_, fpos, 0, 'header 1')
+                assert len(w) == 0
 
-                # Expected and actual blocklength do not match
-                fp_.tell.return_value = 100
-                with warnings.catch_warnings(record=True) as w:
-                    self.fh._check_fpos(fp_, fpos, 0, 'header 1')
-                    self.assertTrue(len(w) > 0)
+            # Expected and actual blocklength do not match
+            fp_.tell.return_value = 100
+            with warnings.catch_warnings(record=True) as w:
+                fh._check_fpos(fp_, fpos, 0, 'header 1')
+                assert len(w) > 0
 
 
 class TestAHICalibration(unittest.TestCase):
