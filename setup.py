48690ca4d18235191539cf23863e2955c2a80f90
Gerrit Holl <gerrit.holl@dwd.de>
2022-05-06 18:32:46 +0200

    Needs pyresample 1.24
    Due to the updates in the parallax correction relying on the latest
    bucket resampler, satpy now needs pyresample 1.24 (which has not been
    released yet).

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ try:
 except ImportError:
     pass
 
-requires = ['numpy >=1.13', 'pillow', 'pyresample >=1.11.0', 'trollsift',
+requires = ['numpy >=1.13', 'pillow', 'pyresample >=1.24.0', 'trollsift',
             'trollimage >1.10.1', 'pykdtree', 'pyyaml', 'xarray >=0.10.1, !=0.13.0',
             'dask[array] >=0.17.1', 'pyproj>=2.2', 'zarr', 'donfig', 'appdirs',
             'pooch']
