f1ea3f04a214b83ecece246a1cfe48dee744555f
Simon Proud <simon.proud@stfc.ac.uk>
2022-11-09 16:14:42 +0000

    Update AGRI reader to remove unneeded properties + functions.

--- satpy/readers/agri_l1.py
+++ satpy/readers/agri_l1.py
@@ -25,8 +25,7 @@ The files read by this reader are described in the official Real Time Data Servi
 
 import logging
 
-from satpy.readers._geos_area import get_area_definition, get_area_extent
-from satpy.readers.fy4_base import RESOLUTION_LIST, FY4Base
+from satpy.readers.fy4_base import FY4Base
 
 logger = logging.getLogger(__name__)
 
@@ -72,74 +71,3 @@ class HDF_AGRI_L1(FY4Base):
         data.attrs.pop('FillValue', None)
         data.attrs.pop('Intercept', None)
         data.attrs.pop('Slope', None)
-
-    def calibrate(self, data, ds_info, ds_name, file_key):
-        """Calibrate the data."""
-        # Check if calibration is present, if not assume dataset is an angle
-        calibration = ds_info.get('calibration')
-        # Return raw data in case of counts or no calibration
-        if calibration in ('counts', None):
-            data.attrs['units'] = ds_info['units']
-            ds_info['valid_range'] = data.attrs['valid_range']
-        elif calibration == 'reflectance':
-            channel_index = int(file_key[-2:]) - 1
-            data = self.calibrate_to_reflectance(data, channel_index, ds_info)
-
-        elif calibration == 'brightness_temperature':
-            data = self.calibrate_to_bt(data, ds_info, ds_name)
-        elif calibration == 'radiance':
-            raise NotImplementedError("Calibration to radiance is not supported.")
-        # Apply range limits, but not for counts or we convert to float!
-        if calibration not in ['counts', None]:
-            data = data.where((data >= min(data.attrs['valid_range'])) &
-                              (data <= max(data.attrs['valid_range'])))
-        else:
-            data.attrs['_FillValue'] = data.attrs['FillValue'].item()
-        if calibration is None:
-            data = data.where(data != data.attrs['_FillValue'])
-        return data
-
-    def calibrate_to_reflectance(self, data, channel_index, ds_info):
-        """Calibrate to reflectance [%]."""
-        logger.debug("Calibrating to reflectances")
-        # using the corresponding SCALE and OFFSET
-        cal_coef = 'CALIBRATION_COEF(SCALE+OFFSET)'
-        num_channel = self.get(cal_coef).shape[0]
-        if num_channel == 1:
-            # only channel_2, resolution = 500 m
-            channel_index = 0
-        data.attrs['scale_factor'] = self.get(cal_coef)[channel_index, 0].values.item()
-        data.attrs['add_offset'] = self.get(cal_coef)[channel_index, 1].values.item()
-        data = scale(data, data.attrs['scale_factor'], data.attrs['add_offset'])
-        data *= 100
-        ds_info['valid_range'] = (data.attrs['valid_range'] * data.attrs['scale_factor'] + data.attrs['add_offset'])
-        ds_info['valid_range'] = ds_info['valid_range'] * 100
-        return data
-
-    def calibrate_to_bt(self, data, ds_info, ds_name):
-        """Calibrate to Brightness Temperatures [K]."""
-        logger.debug("Calibrating to brightness_temperature")
-        lut_key = ds_info.get('lut_key', ds_name)
-        lut = self.get(lut_key)
-        # the value of dn is the index of brightness_temperature
-        data = apply_lut(data, lut)
-        ds_info['valid_range'] = lut.attrs['valid_range']
-        return data
-
-    @property
-    def start_time(self):
-        """Get the start time."""
-        start_time = self['/attr/Observing Beginning Date'] + 'T' + self['/attr/Observing Beginning Time'] + 'Z'
-        try:
-            return datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S.%fZ')
-        except ValueError:
-            return datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%SZ')
-
-    @property
-    def end_time(self):
-        """Get the end time."""
-        end_time = self['/attr/Observing Ending Date'] + 'T' + self['/attr/Observing Ending Time'] + 'Z'
-        try:
-            return datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S.%fZ')
-        except ValueError:
-            return datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%SZ')
