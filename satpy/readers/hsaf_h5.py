b20f68359ac2a19cfbb80e222de955bcf6817e23
cll <lorenzo.clementi@meteoswiss.ch>
2022-11-17 13:35:45 +0000

    Add a reader for H-SAF h10 Snow Cover (SC) hdf5

---
+++ satpy/readers/hsaf_h5.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019.
+#
+# This file is part of satpy.
+#
+# satpy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# satpy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# satpy.  If not, see <http://www.gnu.org/licenses/>.
+"""A reader for HDF5 Snow Cover (SC) daily file produced by the Hydrology SAF.
+
+"""
+import logging
+from datetime import datetime, timedelta
+
+import dask.array as da
+import numpy as np
+import h5py
+import xarray as xr
+from pyresample import geometry
+from satpy.resample import get_area_def
+import os
+from satpy import CHUNK_SIZE
+from satpy.readers.file_handlers import BaseFileHandler
+
+LOG = logging.getLogger(__name__)
+
+class HSAFFileHandler(BaseFileHandler):
+    """File handler for HSAF H5 files."""
+
+    def __init__(self, filename, filename_info, filetype_info):
+        """Init the file handler."""
+        super(HSAFFileHandler, self).__init__(filename,
+                                              filename_info,
+                                              filetype_info)
+
+        self._msg_datasets = {}
+        self._start_time = None
+        self._end_time = None
+        analysis_time = self._get_datetime(filename)
+        self._analysis_time = analysis_time
+
+    @staticmethod
+    def _get_datetime(filename):
+        fname = os.path.basename(filename)
+        dtstr = fname.split('_')[1].zfill(4)
+        return datetime.strptime(dtstr, "%Y%m%d%H%M")
+
+    @property
+    def analysis_time(self):
+        """Get validity time of this file."""
+        return self._analysis_time
+
+    def _prepare_variable_for_palette(self, msg, ds_info):
+        colormap = np.array(np.array(msg))
+        return xr.DataArray(colormap, attrs=ds_info, dims=('idx', 'RGB'))
+
+    def get_metadata(self, msg, name):
+        """Get the metadata."""     
+        if name == 'SC':
+            ds_info = {
+                'name' : 'SC',
+                'filename': self.filename,
+                'data_time': self._analysis_time,
+                'nx': msg.shape[1],	
+                'ny': msg.shape[0]
+            }
+        elif name == 'SC_pal':
+            ds_info = {
+                'name' : 'SC_pal'
+            }
+        return ds_info
+
+    def get_area_def(self, dsid):
+        """Area definition not available in the HDF5 message,
+           so using hardcoded one (it's known).
+        """
+        if dsid['name'] == 'SC':
+            msg = self._get_message(1)
+            return self._get_area_def(msg)
+        else:
+            raise NotImplementedError
+
+    def _get_area_def(self, msg):
+        """Area definition not available in the HDF5 message,
+           so using hardcoded one (it's known).
+           
+        hsaf_h10:
+          description: H SAF H10 area definition
+          projection:
+            proj: geos
+            lon_0: 0
+            h: 35785831
+            x_0: 0
+            y_0: 0
+            a: 6378169
+            rf: 295.488065897001
+            no_defs: null
+            type: crs
+          shape:
+            height: 916
+            width: 1902
+          area_extent:
+            lower_left_xy: [-1936760.3163240477, 2635854.280233425]
+            upper_right_xy: [3770006.7195370505, 5384223.683413638]
+            units: m
+        """
+
+        fd_def = get_area_def('msg_seviri_fes_3km')
+        hsaf_def = fd_def[62:62+916, 1211:1211+1902]
+
+        return hsaf_def
+
+
+    def _get_message(self, idx):
+        hf = h5py.File(self.filename,'r')
+        if (idx == 1):
+            msg = hf.get('SC')
+        if (idx == 2):
+            msg = hf.get('colormap')
+        return msg
+
+    def get_dataset(self, ds_id, ds_info):
+        """Read a HDF5 file into an xarray DataArray."""
+        variable = None
+        if (ds_id['name'] == 'SC'):
+            msg = self._get_message(1)
+            ds_info = self.get_metadata(msg, ds_id['name'])
+
+            fname = os.path.basename(msg.file.filename)
+            dtstr = fname.split('_')[1].zfill(4)
+            h10_time = datetime.strptime(dtstr, "%Y%m%d%H%M")
+        
+            ds_info['start_time'] = h10_time
+            ds_info['end_time'] = h10_time
+        
+            data = np.array(msg)
+            data = da.from_array(data, chunks=CHUNK_SIZE)
+            variable = xr.DataArray(data, attrs=ds_info, dims=('y', 'x'))
+        
+        elif (ds_id['name'] == 'SC_pal'):
+            msg = self._get_message(2)
+            ds_info = self.get_metadata(msg, ds_id['name'])
+            variable = self._prepare_variable_for_palette(msg, ds_info)
+            
+        else:
+            raise IOError("File does not contain {} data".format(ds_id['name']))
+
+        return variable
---
+++ satpy/tests/reader_tests/test_hsaf_h5.py
@@ -0,0 +1,67 @@
+"""Tests for the H-SAF H5 reader."""
+import os
+from datetime import datetime
+import dask.array as da
+import h5py
+import numpy as np
+import pytest
+from satpy.resample import get_area_def
+from satpy import Scene
+#from satpy.tests.utils import make_dataid
+
+# real shape is 916, 1902
+shape_sc = (916, 1902)
+shape_sc_colormap = (256, 3)
+values_sc = np.random.randint(0, 9, shape_sc, dtype=np.uint8)
+
+dataset_names = {"SC": "Snow Cover",
+                 "SC_pal": "Snow Cover Palette"}
+
+dimensions = {"SC": shape_sc,
+              "colormap": shape_sc_colormap,}
+
+start_time = datetime(2022, 11, 15, 0, 0)
+end_time = datetime(2022, 11, 15, 0, 0)
+
+time_pattern = "%Y%m%d%H%M"
+
+@pytest.fixture(scope="session")
+def hsaf_filename(tmp_path_factory):
+    """Create a fake HSAF SC HDF5 file."""
+    filename = tmp_path_factory.mktemp("data") / "h10_20221115_day_merged.H5"
+    with h5py.File(filename, mode="w") as h5f:
+        h5f.create_dataset('SC', shape_sc, dtype=np.uint8)
+        h5f.create_dataset('colormap', shape_sc_colormap, dtype=np.uint8)
+    return filename
+
+
+def test_hsaf_sc_dataset(hsaf_filename):
+    """Test the H-SAF SC dataset."""
+    scn = Scene(filenames=[str(hsaf_filename)], reader="hsaf_h5")
+    scn.load(['SC'])
+    assert scn['SC'].shape == shape_sc
+    
+def test_hsaf_sc_colormap_dataset(hsaf_filename):
+    """Test the H-SAF SC_pal dataset."""
+    scn = Scene(filenames=[str(hsaf_filename)], reader="hsaf_h5")
+    scn.load(['SC_pal'])
+    assert scn['SC_pal'].shape == shape_sc_colormap
+    
+def test_hsaf_sc_datetime(hsaf_filename):
+    """Test the H-SAF reference time."""
+    scn = Scene(filenames=[str(hsaf_filename)], reader="hsaf_h5")
+    scn.load(['SC'])
+    fname = str(hsaf_filename)
+    dtstr = fname.split('_')[1].zfill(4)
+    obs_time = datetime.strptime(dtstr, "%Y%m%d%H%M")
+    assert scn['SC'].attrs['data_time'] == obs_time
+    
+
+def test_hsaf_sc_areadef(hsaf_filename):
+    """Test the H-SAF SC area definition."""
+    scn = Scene(filenames=[str(hsaf_filename)], reader="hsaf_h5")
+    scn.load(['SC'])
+    fd_def = get_area_def('msg_seviri_fes_3km')
+    hsaf_def = fd_def[62:62+916, 1211:1211+1902]
+    assert scn['SC'].area == hsaf_def
+    
