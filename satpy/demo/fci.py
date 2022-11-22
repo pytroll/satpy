f785baee503885ecf8a4f2fe0c63593b96a192af
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-22 08:24:26 +0000

    Adding tarfile member sanitization to extractall()

--- satpy/demo/fci.py
+++ satpy/demo/fci.py
@@ -53,5 +53,27 @@ def _unpack_tarfile_to(filename, subdir):
     """Unpack content of tarfile in filename to subdir."""
     with tarfile.open(filename, mode="r:gz") as tf:
         contents = tf.getnames()
-        tf.extractall(path=subdir)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, path=subdir)
     return contents
