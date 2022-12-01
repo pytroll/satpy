16c6721c82ed6c578b5d1a6a604e45676b6d15f8
clem <clem@sosto.net>
2022-12-01 18:25:54 +0100

    Use temporary files in the tests

--- satpy/tests/reader_tests/test_hsaf_h5.py
+++ satpy/tests/reader_tests/test_hsaf_h5.py
@@ -1,10 +1,10 @@
 """Tests for the H-SAF H5 reader."""
 import os
+import tempfile
 from datetime import datetime
 
 import h5py
 import numpy as np
-import pytest
 
 from satpy import Scene
 from satpy.resample import get_area_def
@@ -14,47 +14,64 @@ shape_sc = (916, 1902)
 shape_sc_colormap = (256, 3)
 
 
-@pytest.fixture(scope="session")
 def hsaf_filename():
     """Create a fake HSAF SC HDF5 file."""
-    filename = "h10_20221115_day_merged.H5"
+    dirname = tempfile.mkdtemp()
+    filename = dirname + "/h10_20221115_day_merged.H5"
     with h5py.File(filename, mode="w") as h5f:
         h5f.create_dataset('SC', shape_sc, dtype=np.uint8)
         h5f.create_dataset('colormap', shape_sc_colormap, dtype=np.uint8)
     return filename
 
 
-def test_hsaf_sc_dataset(hsaf_filename):
+def test_hsaf_sc_dataset():
     """Test the H-SAF SC dataset."""
-    scn = Scene(filenames=[str(hsaf_filename)], reader="hsaf_h5")
-    scn.load(['SC'])
+    filename = hsaf_filename()
+
+    try:
+        scn = Scene(filenames=[str(filename)], reader="hsaf_h5")
+        scn.load(['SC'])
+    finally:
+        os.remove(filename)
     assert scn['SC'].shape == shape_sc
 
 
-def test_hsaf_sc_colormap_dataset(hsaf_filename):
+def test_hsaf_sc_colormap_dataset():
     """Test the H-SAF SC_pal dataset."""
-    scn = Scene(filenames=[str(hsaf_filename)], reader="hsaf_h5")
-    scn.load(['SC_pal'])
+    filename = hsaf_filename()
+
+    try:
+        scn = Scene(filenames=[str(filename)], reader="hsaf_h5")
+        scn.load(['SC_pal'])
+    finally:
+        os.remove(filename)
     assert scn['SC_pal'].shape == shape_sc_colormap
 
 
-def test_hsaf_sc_datetime(hsaf_filename):
+def test_hsaf_sc_datetime():
     """Test the H-SAF reference time."""
-    scn = Scene(filenames=[str(hsaf_filename)], reader="hsaf_h5")
-    scn.load(['SC'])
-    fname = os.path.basename(hsaf_filename)
-    dtstr = fname.split('_')[1]
-    obs_time = datetime.strptime(dtstr, "%Y%m%d")
+    filename = hsaf_filename()
+
+    try:
+        scn = Scene(filenames=[str(filename)], reader="hsaf_h5")
+        scn.load(['SC'])
+        fname = os.path.basename(filename)
+        dtstr = fname.split('_')[1]
+        obs_time = datetime.strptime(dtstr, "%Y%m%d")
+    finally:
+        os.remove(filename)
     assert scn['SC'].attrs['data_time'] == obs_time
 
 
-def test_hsaf_sc_areadef(hsaf_filename):
+def test_hsaf_sc_areadef():
     """Test the H-SAF SC area definition."""
+    filename = hsaf_filename()
+
     try:
-        scn = Scene(filenames=[str(hsaf_filename)], reader="hsaf_h5")
+        scn = Scene(filenames=[str(filename)], reader="hsaf_h5")
         scn.load(['SC'])
         fd_def = get_area_def('msg_seviri_fes_3km')
         hsaf_def = fd_def[62:62+916, 1211:1211+1902]
     finally:
-        os.remove(hsaf_filename)
+        os.remove(filename)
     assert scn['SC'].area == hsaf_def
