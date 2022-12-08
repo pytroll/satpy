fix: bad scan lines reafactor masking good scan lines
The refactor of mask_bad_scan_lines from np.choose to dataarray.where
inverted the logic of the line mask which lead to good scan lines being
masked.

Furthermore this fixes an undiscovered bug regarding the optional line
masking in the hrit reader introduced in in PR 1693 where the
corresponding reader quark was not recognized.

satpy/readers/seviri_base.py
@@ -948,7 +948,7 @@ def _create_bad_quality_lines_mask(line_validity, line_geometric_quality, line_r
             Quality flags with shape (nlines,).
 
     Returns:
-        numpy.ndarray
+        numpy.ndarray: Indicating if the scan line is bad.
     """
     # Based on missing (2) or corrupted (3) data
     line_mask = line_validity >= 2
@@ -977,5 +977,5 @@ def mask_bad_quality(data, line_validity, line_geometric_quality, line_radiometr
     """
     line_mask = _create_bad_quality_lines_mask(line_validity, line_geometric_quality, line_radiometric_quality)
     line_mask = line_mask[:, np.newaxis]
-    data = data.where(line_mask, np.nan).astype(np.float32)
+    data = data.where(~line_mask, np.nan).astype(np.float32)
     return data

satpy/tests/reader_tests/test_seviri_l1b_hrit.py
@@ -415,8 +415,8 @@ class TestHRITMSGCalibration(TestFileHandlerCalibrationBase):
         mda = {
             'image_segment_line_quality': {
                 'line_validity': np.array([3, 3]),
-                'line_radiometric_quality': np.array([3, 3]),
-                'line_geometric_quality': np.array([3, 3])
+                'line_radiometric_quality': np.array([4, 4]),
+                'line_geometric_quality': np.array([4, 4])
             },
         }
 
@@ -496,6 +496,7 @@ class TestHRITMSGCalibration(TestFileHandlerCalibrationBase):
         fh = file_handler
 
         res = fh._mask_bad_quality(expected)
+        print(res)
         new_data = np.zeros_like(expected.data).astype('float32')
         new_data[:, :] = np.nan
         expected = expected.copy(data=new_data)