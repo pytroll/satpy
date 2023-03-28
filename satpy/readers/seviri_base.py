46f431668b72c206633dcb6dc10599dd901b2e6e
youva Aoun <youva.aoun@external.eumetsat.int>
2023-03-28 13:50:01 +0000

    Add a function to round time

--- satpy/readers/seviri_base.py
+++ satpy/readers/seviri_base.py
@@ -166,6 +166,7 @@ References:
 """
 
 import warnings
+from datetime import datetime, timedelta
 
 import dask.array as da
 import numpy as np
@@ -980,3 +981,33 @@ def mask_bad_quality(data, line_validity, line_geometric_quality, line_radiometr
     line_mask = line_mask[:, np.newaxis]
     data = data.where(~line_mask, np.nan).astype(np.float32)
     return data
+
+
+def round_time(dt=None, date_delta=None, to='average'):
+    """Round a datetime object to a multiple of a timedelta.
+
+    dt : datetime.datetime object, default now.
+    dateDelta : timedelta object, we round to a multiple of this, default 1 minute.
+    from:  http://stackoverflow.com/questions/3463930/how-to-round-the-minute-of-a-datetime-object-python
+    """
+    round_to = date_delta.total_seconds()
+    if dt is None:
+        dt = datetime.now()
+    seconds = (dt - dt.min).seconds
+    if date_delta is None:
+        date_delta = timedelta(minutes=1)
+
+    if seconds % round_to == 0 and dt.microsecond == 0:
+        rounding = (seconds + round_to / 2) // round_to * round_to
+    else:
+        if to == 'up':
+            # // is a floor division, not a comment on following line (like in javascript):
+            rounding = (seconds + dt.microsecond/1000000 + round_to) // round_to * round_to
+        elif to == 'down':
+            rounding = seconds // round_to * round_to
+        elif to == 'average':
+            rounding = (seconds + round_to / 2) // round_to * round_to
+        else:
+            raise ValueError(f'Unusported rounding option {to}')
+
+    return dt + timedelta(0, rounding - seconds, - dt.microsecond)
