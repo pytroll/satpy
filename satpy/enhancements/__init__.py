3ae96296379e2e55d80dc752d35b4ea56ea6d9f0
Simon Proud <simon.proud@stfc.ac.uk>
2023-04-14 21:49:23 +0100

    Generalise the `true_color_reproduction` composite and enhancement so they can be used with more than just Himawari/AHI.

--- satpy/enhancements/__init__.py
+++ satpy/enhancements/__init__.py
@@ -575,3 +575,68 @@ def _bt_threshold(band_data, threshold, high_coeffs, low_coeffs):
     return np.where(band_data >= threshold,
                     high_coeffs.offset - high_coeffs.factor * band_data,
                     low_coeffs.offset - low_coeffs.factor * band_data)
+
+
+def jma_true_color_reproduction(img):
+    """Apply CIE XYZ matrix and return True Color Reproduction data.
+
+    Himawari-8 True Color Reproduction Approach Based on the CIE XYZ Color System
+    Hidehiko MURATA, Kotaro SAITOH, and Yasuhiko SUMIDA
+    Meteorological Satellite Center, Japan Meteorological Agency
+    NOAA National Environmental Satellite, Data, and Information Service
+    Colorado State University—CIRA
+    https://www.jma.go.jp/jma/jma-eng/satellite/introduction/TCR.html
+    """
+    _jma_true_color_reproduction(img.data,
+                                 sensor=img.data.attrs['sensor'],
+                                 platform=img.data.attrs['platform_name'])
+
+
+@exclude_alpha
+@on_dask_array
+def _jma_true_color_reproduction(img_data, sensor=None, platform=None):
+    """Convert frmo AHI RGB space to sRGB space.
+
+    The conversion matrices for this are supplied per-sensor and per-platform.
+    The matrices are computed using the method described in the paper:
+    'True Color Imagery Rendering for Himawari-8 with a Color Reproduction Approach
+     Based on the CIE XYZ Color System'
+    DOI:10.2151/jmsj.2018-049
+    """
+    # Conversion matrix dictionaries specifying sensor and platform.
+    ccm_dict = {'AHI': {'himawari-8': np.array([[1.1629,  0.1539, -0.2175],
+                                                [-0.0252, 0.8725,  0.1300],
+                                                [-0.0204, -0.1100, 1.0633]]),
+
+                        'himawari-9': np.array([[1.1619,   0.1542, -0.2168],
+                                                [-0.0271,  0.8749,  0.1295],
+                                                [-0.0202, -0.1103,  1.0634]])
+                        },
+                'ABI': {'goes-16': np.array([[1.1425,  0.1819, -0.2250],
+                                             [-0.0951, 0.9363,  0.1360],
+                                             [-0.0113, -0.1179, 1.0621]]),
+
+                        'goes-17': np.array([[1.1437,  0.1818, -0.2262],
+                                             [-0.0952, 0.9354, 0.1371],
+                                             [-0.0113, -0.1178, 1.0620]]),
+
+                        'goes-18': np.array([[1.1629,  0.1539, -0.2175],
+                                             [-0.0252, 0.8725, 0.1300],
+                                             [-0.0204, -0.1100, 1.0633]]),
+                        }
+                }
+
+    # A conversion matrix, sensor name and platform name is required
+    if sensor is None:
+        raise ValueError("Missing sensor name.")
+    if platform is None:
+        raise ValueError("Missing platform name.")
+
+    # Get the satellite-specific conversion matrix
+    try:
+        ccm = ccm_dict[sensor.upper()][platform.lower()]
+    except KeyError:
+        raise KeyError(f"No conversion matrix found for sensor {sensor} and platform {platform}")
+
+    output = da.dot(img_data.T, ccm.T)
+    return output.T
