a99df5cad14eb88a6fd73ed2c865fe224e4305b2
David Hoese <david.hoese@ssec.wisc.edu>
2023-04-16 20:30:16 -0500

    Refactor to datatree functionality

--- satpy/_scene_converters.py
+++ satpy/_scene_converters.py
@@ -14,6 +14,10 @@
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
 """Helper functions for converting the Scene object to some other object."""
+from __future__ import annotations
+
+from typing import Iterable
+
 import xarray as xr
 
 from .scene import Scene
@@ -24,20 +28,28 @@ except ImportError:
     DataTree = None
 
 
-def to_xarray_datatree(scn: Scene, **kwargs) -> DataTree:
+def to_xarray_datatree(data_arrays: Scene | Iterable[xr.DataArray], **kwargs) -> DataTree:
     """Convert this Scene into an Xarray DataTree object."""
     if DataTree is None:
         raise ImportError("Missing 'xarray-datatree' library required for DataTree conversion")
 
-    datasets = {}
-    for data_arr in scn:
-        sensor_id = data_arr.attrs["sensor"]
-        if isinstance(sensor_id, set):
-            sensor_id = "-".join(sorted(sensor_id))
-        group_id = data_arr.attrs["platform_name"] + "/" + sensor_id
-        if group_id not in datasets:
-            datasets[group_id] = xr.Dataset({
-                data_arr.attrs["name"]: data_arr,
-            })
+    datasets: dict[str, xr.Dataset] = {}
+    for data_arr in data_arrays:
+        group_id = _data_array_to_group_id(data_arr)
+        group_ds = datasets.setdefault(group_id, xr.Dataset())
+        var_name = _data_array_to_variable_name(data_arr)
+        group_ds[var_name] = data_arr
     tree = DataTree.from_dict(datasets)
     return tree
+
+
+def _data_array_to_group_id(data_arr: xr.DataArray) -> str:
+    sensor_id = data_arr.attrs["sensor"]
+    if isinstance(sensor_id, set):
+        sensor_id = "-".join(sorted(sensor_id))
+    group_id = data_arr.attrs["platform_name"] + "/" + sensor_id
+    return group_id
+
+
+def _data_array_to_variable_name(data_arr: xr.DataArray) -> str:
+    return data_arr.attrs["name"]
