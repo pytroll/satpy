8f2f41ba131a6ee0a31853cc0a8f5ac850e1c34c
youva Aoun <youva.aoun@external.eumetsat.int>
2023-05-30 14:08:45 +0000

    Fix doc string for nominal start/end time

--- satpy/readers/seviri_l1b_native.py
+++ satpy/readers/seviri_l1b_native.py
@@ -205,13 +205,13 @@ class NativeMSGFileHandler(BaseFileHandler):
 
     @property
     def nominal_start_time(self):
-        """Read the repeat cycle nominal start time from metadata and round it to expected nominal time slot."""
+        """Get the repeat cycle nominal start time from file header and round it to expected nominal time slot."""
         tm = self.header['15_DATA_HEADER']['ImageAcquisition']['PlannedAcquisitionTime']['TrueRepeatCycleStart']
         return round_nom_time(tm, time_delta=timedelta(minutes=self._repeat_cycle_duration))
 
     @property
     def nominal_end_time(self):
-        """Read the repeat cycle nominal end time from metadata and round it to expected nominal time slot."""
+        """Get the repeat cycle nominal end time from file header and round it to expected nominal time slot."""
         tm = self.header['15_DATA_HEADER']['ImageAcquisition']['PlannedAcquisitionTime']['PlannedRepeatCycleEnd']
         return round_nom_time(tm, time_delta=timedelta(minutes=self._repeat_cycle_duration))
 
