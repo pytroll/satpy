609a39aef92283601aa72acc5b22854d5925b7b3
yukaribbba <72339781+yukaribbba@users.noreply.github.com>
2023-05-31 10:59:51 +0800

    Update satpy/composites/__init__.py
    Co-authored-by: David Hoese <david.hoese@ssec.wisc.edu>

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -1047,8 +1047,8 @@ class RatioSharpenedRGB(GenericCompositor):
         R_hi -  500m resolution - shape=(4000, 4000)
 
     To avoid the green band getting involved in calculating ratio or sharpening,
-    specify it by "neutral_resolution_band: green" in YAML config file. Therefore,
-    only blue band will get sharpened::
+    add "neutral_resolution_band: green" in the YAML config file. This way
+    only the blue band will get sharpened::
 
         ratio = R_hi / R_lo
         new_R = R_hi
