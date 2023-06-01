Fix reflectance units

satpy/readers/gms5_vissr_l1b.py
@@ -605,13 +605,25 @@ class Calibrator:
         """Transform counts to given calibration level."""
         if calibration == "counts":
             return counts
-        res = da.map_blocks(
+        res = self._calibrate(counts)
+        res = self._postproc(res, calibration)
+        return self._make_data_array(res, counts)
+
+    def _calibrate(self, counts):
+        return da.map_blocks(
             self._lookup_calib_table,
             counts.data,
             calib_table=self._calib_table,
             dtype=np.float32,
         )
-        return self._make_data_array(res, counts)
+
+    def _postproc(self, res, calibration):
+        if calibration == "reflectance":
+            res = self._convert_to_percent(res)
+        return res
+
+    def _convert_to_percent(self, res):
+        return res * 100
 
     def _make_data_array(self, interp, counts):
         return xr.DataArray(

satpy/tests/reader_tests/test_gms5_vissr_l1b.py
@@ -364,9 +364,9 @@ class TestFileHandler:
         """Get expected VIS reflectance."""
         lons, lats = lons_lats_exp
         if mask_space:
-            data = [[np.nan, np.nan], [0.5, 1]]
+            data = [[np.nan, np.nan], [50, 100]]
         else:
-            data = [[0, 0.25], [0.5, 1]]
+            data = [[0, 25], [50, 100]]
         return xr.DataArray(
             data,
             dims=("y", "x"),