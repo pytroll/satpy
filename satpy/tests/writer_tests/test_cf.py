396700f0c04aeee05b381eeb16f0480d9ee657ac
ghiggi <gionata.ghiggi@gmail.com>
2023-06-27 23:35:24 +0200

    Refactor Dataset encodings

--- satpy/tests/writer_tests/test_cf.py
+++ satpy/tests/writer_tests/test_cf.py
@@ -808,109 +808,8 @@ class TestCFWriterData:
         assert ds2['var1']['longitude'].attrs['name'] == 'longitude'
 
 
-class EncodingUpdateTest:
-    """Test update of netCDF encoding."""
-
-    @pytest.fixture
-    def fake_ds(self):
-        """Create fake data for testing."""
-        ds = xr.Dataset({'foo': (('y', 'x'), [[1, 2], [3, 4]]),
-                         'bar': (('y', 'x'), [[3, 4], [5, 6]])},
-                        coords={'y': [1, 2],
-                                'x': [3, 4],
-                                'lon': (('y', 'x'), [[7, 8], [9, 10]])})
-        return ds
-
-    @pytest.fixture
-    def fake_ds_digit(self):
-        """Create fake data for testing."""
-        ds_digit = xr.Dataset({'CHANNEL_1': (('y', 'x'), [[1, 2], [3, 4]]),
-                               'CHANNEL_2': (('y', 'x'), [[3, 4], [5, 6]])},
-                              coords={'y': [1, 2],
-                                      'x': [3, 4],
-                                      'lon': (('y', 'x'), [[7, 8], [9, 10]])})
-        return ds_digit
-
-    def test_dataset_name_digit(self, fake_ds_digit):
-        """Test data with dataset name staring with a digit."""
-        from satpy.writers.cf_writer import update_encoding
-
-        # Dataset with name staring with digit
-        ds_digit = fake_ds_digit
-        kwargs = {'encoding': {'1': {'dtype': 'float32'},
-                               '2': {'dtype': 'float32'}},
-                  'other': 'kwargs'}
-        enc, other_kwargs = update_encoding(ds_digit, kwargs, numeric_name_prefix='CHANNEL_')
-        expected_dict = {
-            'y': {'_FillValue': None},
-            'x': {'_FillValue': None},
-            'CHANNEL_1': {'dtype': 'float32'},
-            'CHANNEL_2': {'dtype': 'float32'}
-        }
-        assert enc == expected_dict
-        assert other_kwargs == {'other': 'kwargs'}
-
-    def test_without_time(self, fake_ds):
-        """Test data with no time dimension."""
-        from satpy.writers.cf_writer import update_encoding
-
-        # Without time dimension
-        ds = fake_ds.chunk(2)
-        kwargs = {'encoding': {'bar': {'chunksizes': (1, 1)}},
-                  'other': 'kwargs'}
-        enc, other_kwargs = update_encoding(ds, kwargs)
-        expected_dict = {
-            'y': {'_FillValue': None},
-            'x': {'_FillValue': None},
-            'lon': {'chunksizes': (2, 2)},
-            'foo': {'chunksizes': (2, 2)},
-            'bar': {'chunksizes': (1, 1)}
-        }
-        assert enc == expected_dict
-        assert other_kwargs == {'other': 'kwargs'}
-
-        # Chunksize may not exceed shape
-        ds = fake_ds.chunk(8)
-        kwargs = {'encoding': {}, 'other': 'kwargs'}
-        enc, other_kwargs = update_encoding(ds, kwargs)
-        expected_dict = {
-            'y': {'_FillValue': None},
-            'x': {'_FillValue': None},
-            'lon': {'chunksizes': (2, 2)},
-            'foo': {'chunksizes': (2, 2)},
-            'bar': {'chunksizes': (2, 2)}
-        }
-        assert enc == expected_dict
-
-    def test_with_time(self, fake_ds):
-        """Test data with a time dimension."""
-        from satpy.writers.cf_writer import update_encoding
-
-        # With time dimension
-        ds = fake_ds.chunk(8).expand_dims({'time': [datetime(2009, 7, 1, 12, 15)]})
-        kwargs = {'encoding': {'bar': {'chunksizes': (1, 1, 1)}},
-                  'other': 'kwargs'}
-        enc, other_kwargs = update_encoding(ds, kwargs)
-        expected_dict = {
-            'y': {'_FillValue': None},
-            'x': {'_FillValue': None},
-            'lon': {'chunksizes': (2, 2)},
-            'foo': {'chunksizes': (1, 2, 2)},
-            'bar': {'chunksizes': (1, 1, 1)},
-            'time': {'_FillValue': None,
-                     'calendar': 'proleptic_gregorian',
-                     'units': 'days since 2009-07-01 12:15:00'},
-            'time_bnds': {'_FillValue': None,
-                          'calendar': 'proleptic_gregorian',
-                          'units': 'days since 2009-07-01 12:15:00'}
-        }
-        assert enc == expected_dict
-        # User-defined encoding may not be altered
-        assert kwargs['encoding'] == {'bar': {'chunksizes': (1, 1, 1)}}
-
-
-class TestEncodingKwarg:
-    """Test CF writer with 'encoding' keyword argument."""
+class TestNETCDFEncodingKwargs:
+    """Test netCDF compression encodings."""
 
     @pytest.fixture
     def scene(self):
@@ -1001,7 +900,7 @@ class TestEncodingKwarg:
                 warnings.simplefilter("error")
 
 
-class TestEncodingAttribute(TestEncodingKwarg):
+class TestEncodingAttribute(TestNETCDFEncodingKwargs):
     """Test CF writer with 'encoding' dataset attribute."""
 
     @pytest.fixture
--- satpy/writers/cf_writer.py
+++ satpy/writers/cf_writer.py
@@ -162,7 +162,6 @@ from collections import defaultdict
 import numpy as np
 import xarray as xr
 from packaging.version import Version
-from xarray.coding.times import CFDatetimeCoder
 
 from satpy.writers import Writer
 from satpy.writers.cf.coords_attrs import add_xy_coords_attrs
@@ -220,98 +219,6 @@ def get_extra_ds(dataarray, keys=None):
     # Add input dataarray
     ds_collection[dataarray.attrs['name']] = dataarray
     return ds_collection
-
-
-# ###--------------------------------------------------------------------------.
-# ### netCDF encodings
-
-
-def _set_default_chunks(encoding, dataset):
-    """Update encoding to preserve current dask chunks.
-
-    Existing user-defined chunks take precedence.
-    """
-    for var_name, variable in dataset.variables.items():
-        if variable.chunks:
-            chunks = tuple(
-                np.stack([variable.data.chunksize,
-                          variable.shape]).min(axis=0)
-            )  # Chunksize may not exceed shape
-            encoding.setdefault(var_name, {})
-            encoding[var_name].setdefault('chunksizes', chunks)
-    return encoding
-
-
-def _set_default_fill_value(encoding, dataset):
-    """Set default fill values.
-
-    Avoid _FillValue attribute being added to coordinate variables
-    (https://github.com/pydata/xarray/issues/1865).
-    """
-    coord_vars = []
-    for data_array in dataset.values():
-        coord_vars.extend(set(data_array.dims).intersection(data_array.coords))
-    for coord_var in coord_vars:
-        encoding.setdefault(coord_var, {})
-        encoding[coord_var].update({'_FillValue': None})
-    return encoding
-
-
-def _set_default_time_encoding(encoding, dataset):
-    """Set default time encoding.
-
-    Make sure time coordinates and bounds have the same units.
-    Default is xarray's CF datetime encoding, which can be overridden
-    by user-defined encoding.
-    """
-    if 'time' in dataset:
-        try:
-            dtnp64 = dataset['time'].data[0]
-        except IndexError:
-            dtnp64 = dataset['time'].data
-
-        default = CFDatetimeCoder().encode(xr.DataArray(dtnp64))
-        time_enc = {'units': default.attrs['units'], 'calendar': default.attrs['calendar']}
-        time_enc.update(encoding.get('time', {}))
-        bounds_enc = {'units': time_enc['units'],
-                      'calendar': time_enc['calendar'],
-                      '_FillValue': None}
-        encoding['time'] = time_enc
-        encoding['time_bnds'] = bounds_enc  # FUTURE: Not required anymore with xarray-0.14+
-    return encoding
-
-
-def _update_encoding_dataset_names(encoding, dataset, numeric_name_prefix):
-    """Ensure variable names of the encoding dictionary account for numeric_name_prefix.
-
-    A lot of channel names in satpy starts with a digit.
-    When preparing CF-compliant datasets, these channels are prefixed with numeric_name_prefix.
-
-    If variables names in the encoding dictionary are numeric digits, their name is prefixed
-    with numeric_name_prefix
-    """
-    for var_name in list(dataset.variables):
-        if not numeric_name_prefix or not var_name.startswith(numeric_name_prefix):
-            continue
-        orig_var_name = var_name.replace(numeric_name_prefix, '')
-        if orig_var_name in encoding:
-            encoding[var_name] = encoding.pop(orig_var_name)
-    return encoding
-
-
-def update_encoding(dataset, to_netcdf_kwargs, numeric_name_prefix='CHANNEL_'):
-    """Update encoding.
-
-    Preserve dask chunks, avoid fill values in coordinate variables and make sure that
-    time & time bounds have the same units.
-    """
-    other_to_netcdf_kwargs = to_netcdf_kwargs.copy()
-    encoding = other_to_netcdf_kwargs.pop('encoding', {}).copy()
-    encoding = _update_encoding_dataset_names(encoding, dataset, numeric_name_prefix)
-    encoding = _set_default_chunks(encoding, dataset)
-    encoding = _set_default_fill_value(encoding, dataset)
-    encoding = _set_default_time_encoding(encoding, dataset)
-    return encoding, other_to_netcdf_kwargs
 
 
 # ###--------------------------------------------------------------------------.
@@ -681,8 +588,10 @@ class CFWriter(Writer):
     @staticmethod
     def update_encoding(dataset, to_netcdf_kwargs):
         """Update encoding info (deprecated)."""
+        from satpy.writers.cf.encoding import update_encoding
+
         warnings.warn('CFWriter.update_encoding is deprecated. '
-                      'Use satpy.writers.cf_writer.update_encoding instead.',
+                      'Use satpy.writers.cf.encoding.update_encoding instead.',
                       DeprecationWarning, stacklevel=3)
         return update_encoding(dataset, to_netcdf_kwargs)
 
@@ -728,6 +637,8 @@ class CFWriter(Writer):
                 Prefix to add the each variable with name starting with a digit. Use '' or None to leave this out.
 
         """
+        from satpy.writers.cf.encoding import update_encoding
+
         logger.info('Saving datasets to NetCDF4/CF.')
         _check_backend_versions()
 
