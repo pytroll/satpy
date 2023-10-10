700ac96958c381da0eb2ff4c6d4a0eaa5b494687
Martin Raspaud <martin.raspaud@smhi.se>
2023-10-10 22:00:13 +0200

    Update setup.py
    Co-authored-by: David Hoese <david.hoese@ssec.wisc.edu>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from glob import glob
 
 from setuptools import find_packages, setup
 
-requires = ['numpy >1.20', 'pillow', 'pyresample >=1.24.0', 'trollsift',
+requires = ['numpy >=1.21', 'pillow', 'pyresample >=1.24.0', 'trollsift',
             'trollimage >=1.20', 'pykdtree', 'pyyaml >=5.1', 'xarray >=0.10.1, !=0.13.0',
             'dask[array] >=0.17.1', 'pyproj>=2.2', 'zarr', 'donfig', 'appdirs',
             'packaging', 'pooch', 'pyorbital']
