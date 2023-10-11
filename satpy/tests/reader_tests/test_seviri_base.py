714130cba99751d3b604e9ff8252662b9334dddb
Pierre de Buyl <pdebuyl@pdebuyl.be>
2023-10-11 09:59:30 +0200

    Update test for changes in Meirink calibration

--- satpy/tests/reader_tests/test_seviri_base.py
+++ satpy/tests/reader_tests/test_seviri_base.py
@@ -26,7 +26,7 @@ import pytest
 import xarray as xr
 
 from satpy.readers.seviri_base import (
-    DATE_2000,
+    MEIRINK_EPOCH,
     MEIRINK_COEFS,
     NoValidOrbitParams,
     OrbitPolynomial,
@@ -373,8 +373,8 @@ class TestMeirinkSlope:
         coefs = {'coefs': {}}
         coefs['coefs']['NOMINAL'] = {'gain': -1, 'offset': -1}
         coefs['coefs']['EXTERNAL'] = {}
-        calibration_handler = SEVIRICalibrationHandler(platform_id, channel_name, coefs, 'MEIRINK', DATE_2000)
-        assert calibration_handler.get_gain_offset()[0] == MEIRINK_COEFS[platform_id][channel_name][0]/1000.
+        calibration_handler = SEVIRICalibrationHandler(platform_id, channel_name, coefs, 'MEIRINK-2013', MEIRINK_EPOCH)
+        assert calibration_handler.get_gain_offset()[0] == MEIRINK_COEFS['2013'][platform_id][channel_name][0]/1000.
 
     @pytest.mark.parametrize('platform_id,time,expected', (
         (321, datetime(2005, 1, 18, 0, 0), [0.0250354716, 0.0315626684, 0.022880986]),
@@ -392,5 +392,5 @@ class TestMeirinkSlope:
         coefs['coefs']['NOMINAL'] = {'gain': -1, 'offset': -1}
         coefs['coefs']['EXTERNAL'] = {}
         for i, channel_name in enumerate(['VIS006', 'VIS008', 'IR_016']):
-            calibration_handler = SEVIRICalibrationHandler(platform_id, channel_name, coefs, 'MEIRINK', time)
+            calibration_handler = SEVIRICalibrationHandler(platform_id, channel_name, coefs, 'MEIRINK-2013', time)
             assert abs(calibration_handler.get_gain_offset()[0] - expected[i]) < 1e-6
