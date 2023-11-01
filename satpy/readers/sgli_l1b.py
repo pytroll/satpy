5db79f55232dcada3b984c5d886f48420a515524
Martin Raspaud <martin.raspaud@smhi.se>
2023-11-01 14:09:13 +0100

    Update sgli reader, add tests

--- satpy/readers/sgli_l1b.py
+++ satpy/readers/sgli_l1b.py
@@ -34,18 +34,19 @@ import dask.array as da
 import h5py
 import numpy as np
 import xarray as xr
+from dask.array.core import normalize_chunks
 from xarray import Dataset, Variable
 from xarray.backends import BackendArray, BackendEntrypoint
 from xarray.core import indexing
 
-from satpy import CHUNK_SIZE
+# from satpy import CHUNK_SIZE
 from satpy.readers.file_handlers import BaseFileHandler
 
 logger = logging.getLogger(__name__)
 
-resolutions = {'Q': 250,
-               'K': 1000,
-               'L': 1000}
+resolutions = {"Q": 250,
+               "K": 1000,
+               "L": 1000}
 
 
 def interpolate(arr, sampling, full_shape):
@@ -58,13 +59,14 @@ def interpolate(arr, sampling, full_shape):
     full_x = np.arange(0, full_shape[0])
     full_y = np.arange(0, full_shape[1])
 
+
     from scipy.interpolate import RectBivariateSpline
     spl = RectBivariateSpline(
         tie_x, tie_y, arr)
 
     values = spl(full_x, full_y)
 
-    return da.from_array(values, chunks=(CHUNK_SIZE, CHUNK_SIZE))
+    return da.from_array(values, chunks=(1000, 1000))
 
 
 class HDF5SGLI(BaseFileHandler):
@@ -73,77 +75,146 @@ class HDF5SGLI(BaseFileHandler):
     def __init__(self, filename, filename_info, filetype_info):
         """Initialize the filehandler."""
         super().__init__(filename, filename_info, filetype_info)
-        self.resolution = resolutions[self.filename_info['resolution']]
-        self.fh = h5py.File(self.filename, 'r')
+        self.resolution = resolutions[self.filename_info["resolution"]]
+        self.h5file = h5py.File(self.filename, "r")
 
     @property
     def start_time(self):
         """Get the start time."""
-        the_time = self.fh['Global_attributes'].attrs['Scene_start_time'].item()
-        return datetime.strptime(the_time.decode('ascii'), '%Y%m%d %H:%M:%S.%f')
+        the_time = self.h5file["Global_attributes"].attrs["Scene_start_time"].item()
+        return datetime.strptime(the_time.decode("ascii"), "%Y%m%d %H:%M:%S.%f")
 
     @property
     def end_time(self):
         """Get the end time."""
-        the_time = self.fh['Global_attributes'].attrs['Scene_end_time'].item()
-        return datetime.strptime(the_time.decode('ascii'), '%Y%m%d %H:%M:%S.%f')
+        the_time = self.h5file["Global_attributes"].attrs["Scene_end_time"].item()
+        return datetime.strptime(the_time.decode("ascii"), "%Y%m%d %H:%M:%S.%f")
 
     def get_dataset(self, key, info):
         """Get the dataset."""
         if key["resolution"] != self.resolution:
             return
 
-        if key["polarization"] is not None:
-            pols = {0: '0', -60: 'm60', 60: 'p60'}
-            file_key = info['file_key'].format(pol=pols[key["polarization"]])
-        else:
-            file_key = info['file_key']
+        # if key["polarization"] is not None:
+        #     pols = {0: '0', -60: 'm60', 60: 'p60'}
+        #     file_key = info['file_key'].format(pol=pols[key["polarization"]])
+        # else:
+        #     file_key = info['file_key']
+        file_key = info["file_key"]
+        h5dataset = self.h5file[file_key]
 
-        h5dataset = self.fh[file_key]
+        # resampling_interval = h5dataset.attrs.get('Resampling_interval', 1)
+        # if resampling_interval != 1:
+        #     logger.debug('Interpolating %s.', key["name"])
+        #     full_shape = (self.h5file['Image_data'].attrs['Number_of_lines'],
+        #                   self.h5file['Image_data'].attrs['Number_of_pixels'])
+        #     dataset = interpolate(h5dataset, resampling_interval, full_shape)
+        # else:
+        #     dataset = da.from_array(h5dataset[:].astype('<u2'), chunks=h5dataset.chunks)
+        chunks = normalize_chunks(("auto", "auto"), h5dataset.shape, previous_chunks=h5dataset.chunks, dtype=np.float32)
+        dataset = da.from_array(h5dataset, chunks=chunks)
+        attrs = h5dataset.attrs
 
-        resampling_interval = h5dataset.attrs.get('Resampling_interval', 1)
-        if resampling_interval != 1:
-            logger.debug('Interpolating %s.', key["name"])
-            full_shape = (self.fh['Image_data'].attrs['Number_of_lines'],
-                          self.fh['Image_data'].attrs['Number_of_pixels'])
-            dataset = interpolate(h5dataset, resampling_interval, full_shape)
-        else:
-            dataset = da.from_array(h5dataset[:].astype('<u2'), chunks=h5dataset.chunks)
-        dataset = xr.DataArray(dataset, attrs=h5dataset.attrs, dims=['y', 'x'])
-        dataset.attrs.update(info)
-        dataset = self._mask_and_scale(dataset, h5dataset, key)
-
-        dataset.attrs['platform_name'] = 'GCOM-C1'
-        return dataset
-
-    def _mask_and_scale(self, dataset, h5dataset, key):
+        dataset = xr.DataArray(dataset, attrs=attrs, dims=["y", "x"])
         with xr.set_options(keep_attrs=True):
-            if 'Mask' in h5dataset.attrs:
-                mask_value = h5dataset.attrs['Mask'].item()
-                dataset = dataset & mask_value
-            if 'Bit00(LSB)-13' in h5dataset.attrs:
-                mask_info = h5dataset.attrs['Bit00(LSB)-13'].item()
-                mask_vals = mask_info.split(b'\n')[1:]
-                missing = int(mask_vals[0].split(b':')[0].strip())
-                saturation = int(mask_vals[1].split(b':')[0].strip())
-                dataset = dataset.where(dataset < min(missing, saturation))
-            if 'Maximum_valid_DN' in h5dataset.attrs:
-                # dataset = dataset.where(dataset <= h5dataset.attrs['Maximum_valid_DN'].item())
-                pass
-            if key["name"][:2] in ['VN', 'SW', 'P1', 'P2']:
-                if key["calibration"] == 'counts':
-                    pass
-                if key["calibration"] == 'radiance':
-                    dataset = dataset * h5dataset.attrs['Slope'] + h5dataset.attrs['Offset']
-                if key["calibration"] == 'reflectance':
-                    # dataset = dataset * h5dataset.attrs['Slope'] + h5dataset.attrs['Offset']
-                    # dataset *= np.pi / h5dataset.attrs['Band_weighted_TOA_solar_irradiance'] * 100
-                    # equivalent to the two lines above
-                    dataset = (dataset * h5dataset.attrs['Slope_reflectance']
-                               + h5dataset.attrs['Offset_reflectance']) * 100
+            if key["name"][:2] in ["VN", "SW", "P1", "P2"]:
+                dataset = self.get_visible_dataset(key, h5dataset, dataset)
+            elif key["name"][:-2] in ["longitude", "latitude"]:
+                resampling_interval = attrs["Resampling_interval"]
+                if resampling_interval != 1:
+                    new_lons, new_lats = self.interpolate_lons_lats(resampling_interval)
+                    if key["name"][:-2] == "longitude":
+                        dataset = new_lons
+                    else:
+                        dataset = new_lats
+                    dataset = xr.DataArray(da.from_array(dataset, chunks="auto"), attrs=attrs)
+                return dataset
             else:
-                dataset = dataset * h5dataset.attrs['Slope'] + h5dataset.attrs['Offset']
+                raise NotImplementedError()
+
+        dataset.attrs["platform_name"] = "GCOM-C1"
+
         return dataset
+
+    def interpolate_lons_lats(self, resampling_interval):
+        lons = self.h5file["Geometry_data/Longitude"]
+        lats = self.h5file["Geometry_data/Latitude"]
+
+        full_shape = (self.h5file["Image_data"].attrs["Number_of_lines"],
+                                  self.h5file["Image_data"].attrs["Number_of_pixels"])
+
+        tie_x = np.arange(0, lons.shape[0] * resampling_interval, resampling_interval)
+        tie_y = np.arange(0, lons.shape[1] * resampling_interval, resampling_interval)
+        full_x = np.arange(0, full_shape[0])
+        full_y = np.arange(0, full_shape[1])
+
+        from geotiepoints import SatelliteInterpolator
+        interpolator = SatelliteInterpolator((lons, lats), (tie_x, tie_y), (full_x, full_y), 2, 2,
+                                             chunk_size=(1000, 500))
+        new_lons, new_lats = interpolator.interpolate()
+        return new_lons,new_lats
+
+    def get_visible_dataset(self, key, h5dataset, dataset):
+
+        dataset = self.mask_to_14_bits(dataset)
+        dataset = self.calibrate(dataset, key["calibration"])
+            #dataset.attrs.update(info)
+            #dataset = self._mask_and_scale(dataset, h5dataset, key)
+
+            #
+        return dataset
+
+    def mask_to_14_bits(self, dataset):
+        """Mask data to 14 bits."""
+        return dataset & dataset.attrs["Mask"].item()
+
+
+    def calibrate(self, dataset, calibration):
+        attrs = dataset.attrs
+        if calibration == "counts":
+            return dataset
+        if calibration == "reflectance":
+            calibrated = (dataset * attrs["Slope_reflectance"] + attrs["Offset_reflectance"]) * 100
+        elif calibration == "radiance":
+            calibrated = dataset * attrs["Slope"] + attrs["Offset"]
+        missing, _ = self.get_missing_and_saturated(attrs)
+        return calibrated.where(dataset < missing)
+
+    def get_missing_and_saturated(self, attrs):
+        missing_and_saturated = attrs["Bit00(LSB)-13"].item()
+        mask_vals = missing_and_saturated.split(b"\n")[1:]
+        missing = int(mask_vals[0].split(b":")[0].strip())
+        saturation = int(mask_vals[1].split(b":")[0].strip())
+        return missing, saturation
+
+    # def _mask_and_scale(self, dataset, h5dataset, key):
+    #     with xr.set_options(keep_attrs=True):
+    #         if 'Mask' in h5dataset.attrs:
+    #             mask_value = h5dataset.attrs['Mask'].item()
+    #             dataset = dataset & mask_value
+    #         if 'Bit00(LSB)-13' in h5dataset.attrs:
+    #             mask_info = h5dataset.attrs['Bit00(LSB)-13'].item()
+    #             mask_vals = mask_info.split(b'\n')[1:]
+    #             missing = int(mask_vals[0].split(b':')[0].strip())
+    #             saturation = int(mask_vals[1].split(b':')[0].strip())
+    #             dataset = dataset.where(dataset < min(missing, saturation))
+    #         if 'Maximum_valid_DN' in h5dataset.attrs:
+    #             # dataset = dataset.where(dataset <= h5dataset.attrs['Maximum_valid_DN'].item())
+    #             pass
+    #         if key["name"][:2] in ['VN', 'SW', 'P1', 'P2']:
+    #             if key["calibration"] == 'counts':
+    #                 pass
+    #             if key["calibration"] == 'radiance':
+    #                 dataset = dataset * h5dataset.attrs['Slope'] + h5dataset.attrs['Offset']
+    #             if key["calibration"] == 'reflectance':
+    #                 # dataset = dataset * h5dataset.attrs['Slope'] + h5dataset.attrs['Offset']
+    #                 # dataset *= np.pi / h5dataset.attrs['Band_weighted_TOA_solar_irradiance'] * 100
+    #                 # equivalent to the two lines above
+    #                 dataset = (dataset * h5dataset.attrs['Slope_reflectance']
+    #                            + h5dataset.attrs['Offset_reflectance']) * 100
+    #         else:
+    #             dataset = dataset * h5dataset.attrs['Slope'] + h5dataset.attrs['Offset']
+    #     return dataset
 
 
 class H5Array(BackendArray):
--- satpy/tests/reader_tests/test_sgli_l1b.py
+++ satpy/tests/reader_tests/test_sgli_l1b.py
@@ -1,7 +1,21 @@
 """Tests for the SGLI L1B backend."""
+from datetime import datetime, timedelta
+
+import dask
 import dask.array as da
+import h5py
+import numpy as np
+import pytest
 from dask.array.core import normalize_chunks
 from xarray import DataArray, Dataset, open_dataset
+
+from satpy.readers.sgli_l1b import HDF5SGLI
+
+START_TIME = datetime.now()
+END_TIME = START_TIME + timedelta(minutes=5)
+FULL_KM_ARRAY = np.arange(1955 * 1250, dtype=np.uint16).reshape((1955, 1250))
+MASK = 16383
+LON_LAT_ARRAY = np.arange(197 * 126, dtype=np.uint16).reshape((197, 126))
 
 
 def test_open_dataset():
@@ -14,3 +28,93 @@ def test_open_dataset():
     assert isinstance(data_array, DataArray)
     assert isinstance(data_array.data, da.Array)
     assert data_array.chunks == normalize_chunks((116, 157), data_array.shape)
+
+
+@pytest.fixture(scope="session")
+def sgli_file(tmp_path_factory):
+    filename = tmp_path_factory.mktemp("data") / "test_file.h5"
+    with h5py.File(filename, "w") as h5f:
+        global_attributes = h5f.create_group("Global_attributes")
+        global_attributes.attrs["Scene_start_time"] = np.array([START_TIME.strftime("%Y%m%d %H:%M:%S.%f")[:-3]],
+                                                               dtype="|S21")
+        global_attributes.attrs["Scene_end_time"] = np.array([END_TIME.strftime("%Y%m%d %H:%M:%S.%f")[:-3]],
+                                                             dtype="|S21")
+
+        image_data = h5f.create_group("Image_data")
+        image_data.attrs["Number_of_lines"] = 1955
+        image_data.attrs["Number_of_pixels"] = 1250
+        vn01 = image_data.create_dataset("Lt_VN01", data=FULL_KM_ARRAY, chunks=(116, 157))
+        vn01.attrs["Slope_reflectance"] = np.array([5e-05], dtype=np.float32)
+        vn01.attrs["Offset_reflectance"] = np.array([-0.05], dtype=np.float32)
+        vn01.attrs["Slope"] = np.array([0.02], dtype=np.float32)
+        vn01.attrs["Offset"] = np.array([-25], dtype=np.float32)
+        vn01.attrs["Mask"] = np.array([16383], dtype=np.uint16)
+        vn01.attrs["Bit00(LSB)-13"] = np.array([b"Digital Number\n16383 : Missing value\n16382 : Saturation value"],
+                                               dtype="|S61")
+
+        geometry_data = h5f.create_group("Geometry_data")
+        longitude = geometry_data.create_dataset("Longitude", data=LON_LAT_ARRAY, chunks=(47, 63))
+        longitude.attrs["Resampling_interval"] = 10
+        latitude = geometry_data.create_dataset("Latitude", data=LON_LAT_ARRAY, chunks=(47, 63))
+        latitude.attrs["Resampling_interval"] = 10
+    return filename
+
+def test_start_time(sgli_file):
+    handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+    microseconds = START_TIME.microsecond % 1000
+    assert handler.start_time == START_TIME - timedelta(microseconds=microseconds)
+
+
+def test_end_time(sgli_file):
+    handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+    microseconds = END_TIME.microsecond % 1000
+    assert handler.end_time == END_TIME - timedelta(microseconds=microseconds)
+
+def test_get_dataset_counts(sgli_file):
+    handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+    did = dict(name="VN1", resolution=1000, polarization=None, calibration="counts")
+    res = handler.get_dataset(did, {"file_key": "Image_data/Lt_VN01"})
+    assert np.allclose(res, FULL_KM_ARRAY & MASK)
+    assert res.dtype == np.uint16
+    assert res.attrs["platform_name"] == "GCOM-C1"
+
+def test_get_dataset_reflectances(sgli_file):
+    handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+    did = dict(name="VN1", resolution=1000, polarization=None, calibration="reflectance")
+    res = handler.get_dataset(did, {"file_key": "Image_data/Lt_VN01"})
+    assert np.allclose(res[0, :] / 100, FULL_KM_ARRAY[0, :] * 5e-5 - 0.05)
+    assert res.dtype == np.float32
+
+def test_get_dataset_radiance(sgli_file):
+    handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+    did = dict(name="VN1", resolution=1000, polarization=None, calibration="radiance")
+    res = handler.get_dataset(did, {"file_key": "Image_data/Lt_VN01"})
+    assert np.allclose(res[0, :], FULL_KM_ARRAY[0, :] * np.float32(0.02) - 25)
+    assert res.dtype == np.float32
+
+def test_channel_is_masked(sgli_file):
+    handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+    did = dict(name="VN1", resolution=1000, polarization=None, calibration="counts")
+    res = handler.get_dataset(did, {"file_key": "Image_data/Lt_VN01"})
+    assert res.max() == MASK
+
+def test_missing_values_are_masked(sgli_file):
+    handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+    did = dict(name="VN1", resolution=1000, polarization=None, calibration="radiance")
+    res = handler.get_dataset(did, {"file_key": "Image_data/Lt_VN01"})
+    assert np.isnan(res).sum() == 149
+
+def test_channel_is_chunked(sgli_file):
+    with dask.config.set({"array.chunk-size": "1MiB"}):
+        handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+        did = dict(name="VN1", resolution=1000, polarization=None, calibration="counts")
+        res = handler.get_dataset(did, {"file_key": "Image_data/Lt_VN01"})
+        assert res.chunks[0][0] > 116
+
+def test_loading_lon_lat(sgli_file):
+    handler = HDF5SGLI(sgli_file, {"resolution": "L"}, {})
+    did = dict(name="longitude_v", resolution=1000, polarization=None)
+    res = handler.get_dataset(did, {"file_key": "Geometry_data/Longitude"})
+    assert res.shape == (1955, 1250)
+    assert res.chunks is not None
+    assert res.dtype == np.float32
