60a7c1b3a527b896838114a5ee0f2ce7b26fabec
Panu Lahtinen <pnuu+git@iki.fi>
2023-12-08 09:42:53 +0200

    Fix RealisticColors compositor not to upcast data to float64

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -992,17 +992,17 @@ class RealisticColors(GenericCompositor):
         hrv = projectables[2]
 
         try:
-            ch3 = 3 * hrv - vis06 - vis08
+            ch3 = 3.0 * hrv - vis06 - vis08
             ch3.attrs = hrv.attrs
         except ValueError:
             raise IncompatibleAreas
 
         ndvi = (vis08 - vis06) / (vis08 + vis06)
-        ndvi = np.where(ndvi < 0, 0, ndvi)
+        ndvi = ndvi.where(ndvi >= 0.0, 0.0)
 
-        ch1 = ndvi * vis06 + (1 - ndvi) * vis08
+        ch1 = ndvi * vis06 + (1.0 - ndvi) * vis08
         ch1.attrs = vis06.attrs
-        ch2 = ndvi * vis08 + (1 - ndvi) * vis06
+        ch2 = ndvi * vis08 + (1.0 - ndvi) * vis06
         ch2.attrs = vis08.attrs
 
         res = super(RealisticColors, self).__call__((ch1, ch2, ch3),
