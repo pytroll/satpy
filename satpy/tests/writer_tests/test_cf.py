fe50d42cc928c390b4582978045e25edc17c8137
Panu Lahtinen <pnuu+git@iki.fi>
2023-12-11 15:14:50 +0200

    Update xarray version in compression tests for compression kwarg

--- satpy/tests/writer_tests/test_cf.py
+++ satpy/tests/writer_tests/test_cf.py
@@ -570,5 +570,5 @@ def _should_use_compression_keyword():
     versions = _get_backend_versions()
     return (
         versions["libnetcdf"] >= Version("4.9.0") and
-        versions["xarray"] >= Version("2023.12")
+        versions["xarray"] >= Version("2024.1")
     )
