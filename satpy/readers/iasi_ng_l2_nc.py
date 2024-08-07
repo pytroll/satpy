83736952ddac308b676383c6da9a364add622f29
GMV - Emmanuel Roche <eroche@gmv.com>
2024-08-07 17:43:04 +0200

    Added initial version of file pattern and unit test

---
+++ satpy/readers/iasi_ng_l2_nc.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2017-2023 Satpy developers
+#
+# This file is part of satpy.
+#
+# satpy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# satpy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# satpy.  If not, see <http://www.gnu.org/licenses/>.
+
+r"""IASI-NG L2 reader
+
+This reader supports reading all the products from the IASI-NG L2 processing
+level:
+  * IASI-L2-TWV
+  * IASI-L2-CLD
+  * IASI-L2-GHG
+  * IASI-L2-SFC
+"""
+
+import logging
+import logging
+import numpy as np
+import xarray as xr
+
+
+from .netcdf_utils import NetCDF4FsspecFileHandler
+
+logger = logging.getLogger(__name__)
+
+
+class IASINGL2NCFileHandler(NetCDF4FsspecFileHandler):
+    """Reader for IASI-NG L2 products in NetCDF format."""
+
+    def __init__(self, filename, filename_info, filetype_info, **kwargs):
+        """Initialize object."""
+        super().__init__(filename, filename_info, filetype_info, **kwargs)
+
+        logger.info("Creating reader with infos: %s", filename_info)
+
+    # def get_dataset(self, data_id, ds_info):
+    #     """Obtain dataset."""
+    #     ds = self[data_id["name"]]
+    #     if "scan_lines" in ds.dims:
+    #         ds = ds.rename(scan_lines="y")
+    #     if "pixels" in ds.dims:
+    #         ds = ds.rename(pixels="x")
+    #     if "_FillValue" in ds.attrs and ds.dtype.kind == "f":
+    #         with xr.set_options(keep_attrs=True):
+    #             # have to inverse the logic due to https://github.com/pydata/xarray/issues/7581
+    #             return xr.where(ds != ds.attrs["_FillValue"], ds, np.nan)
+    #     return ds
+
+    # def available_datasets(self, configured_datasets=None):
+    #     """Get available datasets based on what's in the file.
+
+    #     Returns all datasets in the root group.
+    #     """
+    #     yield from super().available_datasets(configured_datasets)
+    #     common = {"file_type": "iasi_l2_cdr_nc", "resolution": 12000}
+    #     for key in self.file_content:
+    #         if "/" in key:  # not a dataset
+    #             continue
+    #         yield (True, {"name": key} | common | self[key].attrs)
--- satpy/tests/reader_tests/test_iasi_ng_l2_nc.py
+++ satpy/tests/reader_tests/test_iasi_ng_l2_nc.py
@@ -14,11 +14,81 @@
 # along with satpy.  If not, see <http://www.gnu.org/licenses/>.
 
 """Unit tests on the IASI NG L2 reader using the conventional mock constructed context."""
+import pytest
+import logging
+import os
+from unittest import mock
+from satpy.readers.iasi_ng_l2_nc import IASINGL2NCFileHandler
+
+from satpy.readers import load_reader
+
+from satpy.tests.reader_tests.test_netcdf_utils import FakeNetCDF4FileHandler
+
+logger = logging.getLogger(__name__)
 
 
-class TestIasiNGL2:
+class FakeIASINGFileHandlerBase(FakeNetCDF4FileHandler):
+    """Fake base class for IASI NG handler"""
+
+    def get_test_content(self, _filename, _filename_info, _filetype_info):
+        """Get the content of the test data.
+
+        Here we generate the default content we want to provide depending
+        on the provided filename infos.
+        """
+
+        dset = {}
+        return dset
+
+
+class TestIASINGL2NCReader:
     """Main test class for the IASI NG L2 reader."""
 
-    def test_sanity(self):
-        """Minimal temporary sanity check unit test."""
-        assert 1 == 1
+    yaml_file = "iasi_ng_l2_nc.yaml"
+
+    def setup_method(self):
+        """Setup the reade config"""
+        from satpy._config import config_search_paths
+
+        self.reader_configs = config_search_paths(
+            os.path.join("readers", self.yaml_file)
+        )
+
+    @pytest.fixture(autouse=True, scope="class")
+    def fake_handler(self):
+        """Wrap NetCDF4 FileHandler with our own fake handler."""
+        patch_ctx = mock.patch.object(
+            IASINGL2NCFileHandler, "__bases__", (FakeIASINGFileHandlerBase,)
+        )
+
+        with patch_ctx:
+            patch_ctx.is_local = True
+            yield patch_ctx
+
+    def test_filename_matching(self):
+        # Example filename
+        filename = "W_fr-meteo-sat,GRAL,MTI1-IASING-2-l2p_C_EUMS_20220101120000_LEO_O_D_20220101115425-20220101115728_____W______.nc"
+
+        reader = load_reader(self.reader_configs)
+
+        # Test if the file is recognized by the reader
+        files = reader.select_files_from_pathnames([filename])
+
+        assert len(files) == 1, "File should be recognized by the reader"
+
+        # Create the file handler:
+        reader.create_filehandlers(files)
+
+        # We should have our handler now:
+        assert len(reader.file_handlers) == 1
+
+        # logger.info("File handlers are: %s", reader.file_handlers)
+
+        assert "iasi_ng_l2_nc" in reader.file_handlers
+
+        handlers = reader.file_handlers["iasi_ng_l2_nc"]
+
+        # We should have a single handler for a single file:
+        assert len(handlers) == 1
+
+        assert isinstance(handlers[0], IASINGL2NCFileHandler)
