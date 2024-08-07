0de793db7f9372888ff9850faad451925d657115
GMV - Emmanuel Roche <eroche@gmv.com>
2024-08-07 10:33:52 +0200

    Adding minimal test class on iasi ng l2 reader

---
+++ satpy/tests/reader_tests/test_iasi_ng_l2_nc.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2022 Satpy developers
+#
+# satpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# satpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with satpy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Unit tests on the IASI NG L2 reader using the conventional mock constructed context."""
+
+
+class TestIasiNGL2:
+    """Main test class for the IASI NG L2 reader."""
+
+    def test_sanity(self):
+        """Minimal temporary sanity check unit test."""
+        assert 1 == 0
