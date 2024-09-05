Bugfix for Sentinel-2 L1C radiance calculation, initial tests.

satpy/readers/msi_safe.py
@@ -59,13 +59,15 @@ PLATFORMS = {"S2A": "Sentinel-2A",
 class SAFEMSIL1C(BaseFileHandler):
     """File handler for SAFE MSI files (jp2)."""
 
-    def __init__(self, filename, filename_info, filetype_info, mda, tile_mda, mask_saturated=True):
+    def __init__(self, filename, filename_info, filetype_info, mda, tile_mda,
+                 mask_saturated=True, solar_ang_method="mean"):
         """Initialize the reader."""
         super(SAFEMSIL1C, self).__init__(filename, filename_info,
                                          filetype_info)
         del mask_saturated
         self._channel = filename_info["band_name"]
         self.process_level = filename_info["process_level"]
+        self.solar_ang_method = solar_ang_method
         self._tile_mda = tile_mda
         self._mda = mda
         self.platform_name = PLATFORMS[filename_info["fmission_id"]]
@@ -98,9 +100,17 @@ class SAFEMSIL1C(BaseFileHandler):
                 return self._mda.calibrate_to_radiances_l1b(proj, self._channel)
             else:
                 # For higher level data, radiances must be computed from the reflectance.
-                # sza = self._tile_mda.get_dataset()
+                # By default, we use the mean solar angles so that the user does not need to resample,
+                # but the user can also choose to use the solar angles from the tile metadata.
+                # This is on a coarse grid so for most bands must be resampled before use.
+                if self.solar_ang_method == "mean":
+                    zen, azi = self._tile_mda.mean_sun_angles
+                else:
+                    from satpy import DataQuery
+                    dq = DataQuery(name="solar_zenith_angle")
+                    zen = self._tile_mda.get_dataset(dq, {})
                 tmp_refl = self._mda.calibrate_to_reflectances(proj, self._channel)
-                return self._mda.calibrate_to_radiances(tmp_refl, self._channel)
+                return self._mda.calibrate_to_radiances(tmp_refl, zen, self._channel)
 
         if key["calibration"] == "counts":
             return self._mda._sanitize_data(proj)
@@ -257,14 +267,12 @@ class SAFEMSIMDXML(SAFEMSIXMLMetadata):
         data = self._sanitize_data(data)
         return (data + self.band_offset(band_name)) / physical_gain
 
-    def calibrate_to_radiances(self, data, band_name):
+    def calibrate_to_radiances(self, data, solar_zenith, band_name):
         """Calibrate *data* to radiance using the radiometric information for the metadata."""
         sed = self.sun_earth_dist
         if sed < 0.5 or sed > 1.5:
             raise ValueError(f"Sun-Earth distance is incorrect in the metadata: {sed}")
         solar_irrad_band = self.solar_irradiance(band_name)
-
-        solar_zenith = 32.029
 
         solar_zenith = np.deg2rad(solar_zenith)
 
@@ -313,7 +321,18 @@ class SAFEMSITileMDXML(SAFEMSIXMLMetadata):
             cols,
             rows,
             area_extent)
-        return area
+        return (area)
+
+    @cached_property
+    def mean_sun_angles(self):
+        """Get the mean sun angles from the metadata."""
+        angs = self.root.find(".//Mean_Sun_Angle")
+        if angs is not None:
+            zen = float(angs.find("ZENITH_ANGLE").text)
+            azi = float(angs.find("AZIMUTH_ANGLE").text)
+            return zen, azi
+        else:
+            return -999, -999
 
     @cached_property
     def projection(self):

satpy/tests/reader_tests/test_msi_safe.py
@@ -1442,10 +1442,11 @@ def jp2_builder(process_level, band_name, mask_saturated=True):
                          process_level=process_level.replace("old", ""))
     xml_fh = xml_builder(process_level, mask_saturated, band_name)[0]
     tile_xml_fh = mock.create_autospec(SAFEMSITileMDXML)(BytesIO(TILE_XMLS[PROCESS_LEVELS.index(process_level)]),
-                                   filename_info, mock.MagicMock())
+                                                         filename_info, mock.MagicMock())
     tile_xml_fh.start_time.return_value = tilemd_dt
     jp2_fh = SAFEMSIL1C("somefile", filename_info, mock.MagicMock(), xml_fh, tile_xml_fh)
     return jp2_fh
+
 
 def make_alt_dataid(**items):
     """Make a DataID with modified keys."""
@@ -1578,26 +1579,26 @@ class TestMTDXML:
                              [
                                  ("L1C", True, "B01", ([[[np.nan, -9.99, -9.98, -9.97],
                                                          [-9.96, 0, 645.34, np.inf]]],
-                                                       [[[np.nan, -251.584265, -251.332429, -251.080593],
-                                                         [-250.828757, 0., 16251.99095, np.inf]]],
+                                                       [[[0.0, 5.60879825, 11.2175965, 16.8263948,],
+                                                         [22.435193, 5608.79825, 367566.985, 367572.593]]],
                                                        [[[np.nan, 1, 2, 3],
                                                          [4, 1000, 65534, np.inf]]])),
                                  ("L1C", False, "B10", ([[[np.nan, -19.99, -19.98, -19.97],
                                                           [-19.96, -10, 635.34, 635.35]]],
-                                                        [[[np.nan, -35.465976, -35.448234, -35.430493],
-                                                          [-35.412751, -17.741859, 1127.211275, 1127.229017]]],
+                                                        [[[0.0, 1.09348075, 2.1869615, 3.28044225],
+                                                          [4.373923, 1093.48075, 71660.1675, 71661.2609]]],
                                                         [[[np.nan, 1, 2, 3],
                                                           [4, 1000, 65534, 65535]]])),
                                  ("oldL1C", True, "B01", ([[[np.nan, 0.01, 0.02, 0.03],
                                                             [0.04, 10, 655.34, np.inf]]],
-                                                          [[[np.nan, 0.251836101, 0.503672202, 0.755508303],
-                                                            [1.00734440, 251.836101, 16503.8271, np.inf]]],
+                                                          [[[0.0, 5.60879825, 11.2175965, 16.8263948,],
+                                                            [22.435193, 5608.79825, 367566.985, 367572.593]]],
                                                           [[[np.nan, 1, 2, 3],
                                                             [4, 1000, 65534, np.inf]]])),
                                  ("L2A", False, "B03", ([[[np.nan, -9.99, -9.98, -9.97],
                                                           [-9.96, 0, 645.34, 645.35]]],
-                                                        [[[np.nan, -238.571863, -238.333052, -238.094241],
-                                                          [-237.855431, 0, 15411.407995, 15411.646806]]],
+                                                        [[[0.0, 5.25188783, 10.5037757, 15.7556635,],
+                                                          [21.0075513, 5251.88783, 344177.217, 344182.469]]],
                                                         [[[np.nan, 1, 2, 3],
                                                           [4, 1000, 65534, 65535]]])),
                              ])
@@ -1606,10 +1607,11 @@ class TestMTDXML:
         xml_fh = xml_builder(process_level, mask_saturated)[0]
 
         res1 = xml_fh.calibrate_to_reflectances(self.fake_data, band_name)
-        res2 = xml_fh.calibrate_to_radiances(self.fake_data, band_name)
+        res2 = xml_fh.calibrate_to_radiances(self.fake_data, 25.6, band_name)
         res3 = xml_fh._sanitize_data(self.fake_data)
 
         results = (res1, res2, res3)
+
         np.testing.assert_allclose(results, expected)
 
     @pytest.mark.parametrize(("process_level", "mask_saturated", "band_name", "expected"),
@@ -1655,11 +1657,14 @@ class TestSAFEMSIL1C:
         jp2_fh = jp2_builder("L2A", dataset_name, mask_saturated)
 
         with mock.patch("xarray.open_dataset", return_value=self.fake_data):
-            res = jp2_fh.get_dataset(make_alt_dataid(name=dataset_name, calibration=calibration), info=dict())
-            if res is not None:
-                np.testing.assert_allclose(res, expected)
-            else:
-                assert res is expected
+            with mock.patch("satpy.readers.msi_safe.SAFEMSITileMDXML.mean_sun_angles",
+                            new_callable=mock.PropertyMock) as mocker:
+                mocker.return_value = (25, 8)
+                res = jp2_fh.get_dataset(make_alt_dataid(name=dataset_name, calibration=calibration), info=dict())
+                if res is not None:
+                    np.testing.assert_allclose(res, expected)
+                else:
+                    assert res is expected
 
     @pytest.mark.parametrize(("process_level", "band_name", "dataset_name"),
                              [