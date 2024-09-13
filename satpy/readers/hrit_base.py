37ea808dcb9ab7505039118bba346fc17458b0d1
Michael Schmutz <michael.schmutz@meteotest.ch>
2024-09-13 10:44:28 +0200

    remove obsolete doc string; add dependency on pyPublicDecompWT to project

--- satpy/readers/hrit_base.py
+++ satpy/readers/hrit_base.py
@@ -21,11 +21,8 @@
 This module is the base module for all HRIT-based formats. Here, you will find
 the common building blocks for hrit reading.
 
-One of the features here is the on-the-fly decompression of hrit files. It needs
-a path to the xRITDecompress binary to be provided through the environment
-variable called XRIT_DECOMPRESS_PATH. When compressed hrit files are then
-encountered (files finishing with `.C_`), they are decompressed to the system's
-temporary directory for reading.
+One of the features here is the on-the-fly decompression of hrit files when
+compressed hrit files are encountered (files finishing with `.C_`).
 
 """
 
