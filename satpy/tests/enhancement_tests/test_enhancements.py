b9a2f4d39dd395da4150e1d55c724e9734797c2b
Panu Lahtinen <panu.lahtinen@fmi.fi>
2024-10-21 12:40:06 +0300

    Check for _FillValue in enhanced images

--- satpy/tests/enhancement_tests/test_enhancements.py
+++ satpy/tests/enhancement_tests/test_enhancements.py
@@ -44,6 +44,12 @@ def run_and_check_enhancement(func, data, expected, **kwargs):
     old_keys = set(pre_attrs.keys())
     # It is OK to have "enhancement_history" added
     new_keys = set(img.data.attrs.keys()) - {"enhancement_history"}
+    # In case of palettes are used, _FillValue is added.
+    # Colorize doesn't add the fill value, so ignore that
+    if "palettes" in kwargs and func.__name__ != "colorize":
+        assert "_FillValue" in new_keys
+        # Remove it from further comparisons
+        new_keys = new_keys - {"_FillValue"}
     assert old_keys == new_keys
 
     res_data_arr = img.data
