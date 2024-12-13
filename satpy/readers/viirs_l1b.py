5f7e75177797e3de6048fc2100e2cb1fc11ce302
zxdawn <xinzhang1215@gmail.com>
2024-12-13 20:55:13 +0100

    update scale factor and offset

--- satpy/readers/viirs_l1b.py
+++ satpy/readers/viirs_l1b.py
@@ -235,6 +235,11 @@ class VIIRSL1BFileHandler(NetCDF4FileHandler):
         if factors[0] != 1 or factors[1] != 0:
             data *= factors[0]
             data += factors[1]
+
+        # update scale_factor and scale_offset
+        data.attrs['scale_factor'] = scale_factor
+        data.attrs['add_offset'] = scale_offset
+
         # rename dimensions to correspond to satpy's 'y' and 'x' standard
         if "number_of_lines" in data.dims:
             data = data.rename({"number_of_lines": "y", "number_of_pixels": "x"})
