606e8e3dc0afdb589ed18c01644c3e4989dc4f46
BENR0 <mail2benr@gmail.com>
2025-05-15 12:23:49 +0200

    add: sqlite writer for features.

--- satpy/writers/feature.py
+++ satpy/writers/feature.py
@@ -1,15 +1,17 @@
+"""Module for writers for feature datasets."""
 from satpy.writers import Writer
 
 
 class sqlite(Writer):
     """Write features in geodataframe to sqlite."""
     def __init__(self, name=None, filename=None, base_dir=None, layer_name=None, **kwargs):
+        """Init."""
         super().__init__(name, filename, base_dir,  **kwargs)
 
     def save_dataset(self, dataset, filename=None, layer_name=None, **kwargs):
-        """Save the geodataframe to sqlite"""
+        """Save the geodataframe to sqlite."""
         if layer_name is None:
             layer_name = dataset.attrs["name"]
 
-        print(layer_name, filename)
+        filename = filename or self.get_filename()
         dataset.data.to_file(filename, driver="SQLite", spatialite=True, layer=layer_name)
