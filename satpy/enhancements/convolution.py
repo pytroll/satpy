c0765feb3cc9834c62884206cd5900abbfbe5383
Panu Lahtinen <panu.lahtinen@fmi.fi>
2025-06-26 08:30:15 +0300

    Update docstring of convolution enhancement module

--- satpy/enhancements/convolution.py
+++ satpy/enhancements/convolution.py
@@ -13,7 +13,8 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # satpy.  If not, see <http://www.gnu.org/licenses/>.
-"""Stretching."""
+"""Enhancements based on convolution."""
+
 from __future__ import annotations
 
 import logging
