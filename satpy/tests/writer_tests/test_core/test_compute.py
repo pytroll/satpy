77635d01d58dfb3161a94841a380e1f95edc33fb
David Hoese <david.hoese@ssec.wisc.edu>
2025-08-12 20:58:56 -0500

    Fix some divide by zero warnings during tests

--- satpy/tests/writer_tests/test_core/test_compute.py
+++ satpy/tests/writer_tests/test_core/test_compute.py
@@ -89,7 +89,7 @@ class TestComputeWriterResults:
             100, 200, (-180., -90., 180., 90.),
         )
         ds1 = xr.DataArray(
-            da.zeros((100, 200), chunks=50),
+            da.arange(100 * 200).reshape((100, 200)).rechunk(50),
             dims=("y", "x"),
             attrs={"name": "test",
                    "start_time": dt.datetime(2018, 1, 1, 0, 0, 0),
