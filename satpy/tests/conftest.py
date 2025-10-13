26723ff850f32ab26f21dfcabc9ea41b74f3732c
David Hoese <david.hoese@ssec.wisc.edu>
2025-10-12 20:40:44 -0500

    Remove pyspectral downloading from tests and fail if used

--- satpy/tests/conftest.py
+++ satpy/tests/conftest.py
@@ -21,6 +21,7 @@ This module is executed automatically by pytest.
 
 """
 import os
+from unittest import mock
 
 import pytest
 
@@ -55,3 +56,11 @@ def include_test_etc():
     """Tell Satpy to use the config 'etc' directory from the tests directory."""
     with satpy.config.set(config_path=[TEST_ETC_DIR]):
         yield TEST_ETC_DIR
+
+
+@pytest.fixture(autouse=True, scope="session")
+def _forbid_pyspectral_downloads():
+    with mock.patch("pyspectral.utils.requests") as mock_requests:
+        mock_requests.get.side_effect = RuntimeError(
+            "Pyspectral is attempting a download during tests. Mock pyspectral as necessary to avoid this.")
+        yield
