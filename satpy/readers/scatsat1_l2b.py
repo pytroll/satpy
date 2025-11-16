76f153c459c1fcdda18be77ee5d1b910f14141ee
Trygve Aspenes <trygveas@met.no>
2025-11-16 08:39:43 +0100

    Update according to ruff

--- satpy/readers/scatsat1_l2b.py
+++ satpy/readers/scatsat1_l2b.py
@@ -20,6 +20,7 @@
 import datetime as dt
 
 import xarray as xr
+
 from satpy.readers.core.hdf5 import HDF5FileHandler
 
 
@@ -59,10 +60,10 @@ class SCATSAT1L2BFileHandler(HDF5FileHandler):
         elif "Wind_direction_selection" in var_path:
             data = data * float(self["science_data/attr/Wind Direction Selection Scale"])
 
-        if self.filename_info['cell_spacing'] == '25km':
-            data.attrs.update({'resolution': 25000})
-        elif self.filename_info['cell_spacing'] == '12km':
-            data.attrs.update({'resolution': 12500})
+        if self.filename_info["cell_spacing"] == "25km":
+            data.attrs.update({"resolution": 25000})
+        elif self.filename_info["cell_spacing"] == "12km":
+            data.attrs.update({"resolution": 12500})
 
         data.attrs.update({"platform_name": self.platform_name})
         data.attrs.update(ds_info)
