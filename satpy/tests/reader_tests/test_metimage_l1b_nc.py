0c7cd784e8823e72de9315408857be0885a8f3fb
David Hoese <david.hoese@ssec.wisc.edu>
2026-08-27 13:30:10 -0500

    Convert metimage_l1b_nc tests to pytest

--- satpy/tests/reader_tests/test_metimage_l1b_nc.py
+++ satpy/tests/reader_tests/test_metimage_l1b_nc.py
@@ -6,9 +6,6 @@ This version tests the readers for METimage.
 
 
 import datetime
-import os
-import unittest
-import uuid
 
 import dask
 import dask.array as da
@@ -24,8 +21,6 @@ from satpy.readers.core.metimage import (
     TIE_POINTS_FACTOR,
 )
 from satpy.readers.metimage_l1b_nc import METimageL1BNCFileHandler
-
-TEST_FILE = "test_file_vii_l1b_nc.nc"
 
 NUM_SCANS = 25
 NUM_TIE_POINTS_ACT = 10
@@ -107,91 +102,23 @@ def _make_handler(path):
     )
 
 
-class TestMETimageL1bNCFileHandler(unittest.TestCase):
-    """Test the METimageL1BNCFileHandler reader."""
-
-    def setUp(self):
-        """Set up the test."""
-        # Easiest way to test the reader is to create a test netCDF file on the fly
-        # uses a UUID to avoid permission conflicts during execution of tests in parallel
-        self.test_file_name = TEST_FILE + str(uuid.uuid1()) + ".nc"
-        _create_l1b_file(self.test_file_name)
-
-        self.reader = _make_handler(self.test_file_name)
-
-    def tearDown(self):
-        """Remove the previously created test file."""
-        # Catch Windows PermissionError for removing the created test file.
-        try:
-            os.remove(self.test_file_name)
-        except OSError:
-            pass
-
-    def test_calibration_functions(self):
-        """Test the calibration functions."""
-        radiance = np.array([[1.0, 2.0, 5.0], [7.0, 10.0, 20.0]])
-
-        cw = 13.0
-        a = 3.0
-        b = 100.0
-        bt = self.reader._calibrate_bt(radiance, cw, a, b)
-        expected_bt = np.array([[675.04993213, 753.10301462, 894.93149648],
-                                [963.20401882, 1048.95086402, 1270.95546218]])
-        assert np.allclose(bt, expected_bt)
-
-        isi = 2.0
-        refl = self.reader._calibrate_refl(radiance, isi)
-        expected_refl = np.array([[157.07963268, 314.15926536, 785.3981634],
-                                  [1099.55742876, 1570.79632679, 3141.59265359]])
-        assert np.allclose(refl, expected_refl)
-
-    def test_functions(self):
-        """Test the functions."""
-        # Checks that the _perform_orthorectification function is correctly executed
-        variable = xr.DataArray(
-            dims=("num_lines", "num_pixels"),
-            name="test_name",
-            attrs={
-                "key_1": "value_1",
-                "key_2": "value_2"
-            },
-            data=da.from_array(np.ones((NUM_LINES, NUM_PIXELS)))
-        )
-
-        orthorect_variable = self.reader._perform_orthorectification(variable, "data/measurement_data/delta_lat")
-        expected_values = (np.degrees(np.ones((NUM_LINES, NUM_PIXELS)) / MEAN_EARTH_RADIUS)
-                           + np.ones((NUM_LINES, NUM_PIXELS)))
-        assert np.allclose(orthorect_variable.values, expected_values)
-
-        # Checks that the _perform_calibration function is correctly executed in all cases
-        # radiance calibration: return value is simply a copy of the variable
-        return_variable = self.reader._perform_calibration(variable, {"calibration": "radiance"})
-        assert np.all(return_variable == variable)
-
-        # invalid calibration: raises a ValueError
-        with pytest.raises(ValueError, match="Unknown calibration invalid for dataset test"):
-            self.reader._perform_calibration(variable,
-                                             {"calibration": "invalid", "name": "test"})
-
-        # brightness_temperature calibration: checks that the return value is correct
-        calibrated_variable = self.reader._perform_calibration(variable,
-                                                               {"calibration": "brightness_temperature",
-                                                                "chan_thermal_index": 3})
-        expected_values = np.full((NUM_LINES, NUM_PIXELS), 1237.52069907)
-        assert np.allclose(calibrated_variable.values, expected_values)
-
-        # reflectance calibration: checks that the return value is correct
-        calibrated_variable = self.reader._perform_calibration(variable,
-                                                               {"calibration": "reflectance",
-                                                                "wavelength": [0.658, 0.668, 0.678],
-                                                                "chan_solar_index": 2})
-        expected_values = np.full((NUM_LINES, NUM_PIXELS), 104.71975512)
-        assert np.allclose(calibrated_variable.values, expected_values)
+def _make_variable():
+    """Create a dataset of ones with the shape and dims the reader produces."""
+    return xr.DataArray(
+        dims=("num_lines", "num_pixels"),
+        name="test_name",
+        attrs={
+            "key_1": "value_1",
+            "key_2": "value_2"
+        },
+        data=da.ones((NUM_LINES, NUM_PIXELS), chunks=(ROWS_PER_SCAN, NUM_PIXELS))
+    )
 
 
 # NOTE:
-# The tests below use the following fixture, defined in this module:
+# The tests below use the following fixtures, defined in this module:
 #   metimage_l1b_file - path to a small but structurally realistic L1B netCDF file
+#   reader - a METimageL1BNCFileHandler for that file
 
 
 @pytest.fixture
@@ -200,6 +127,87 @@ def metimage_l1b_file(tmp_path):
     path = tmp_path / "metimage_l1b.nc"
     _create_l1b_file(path)
     return path
+
+
+@pytest.fixture
+def reader(metimage_l1b_file):
+    """Create a file handler for a small METimage L1B file."""
+    return _make_handler(metimage_l1b_file)
+
+
+def test_calibrate_bt():
+    """Test the brightness temperature calibration function."""
+    radiance = np.array([[1.0, 2.0, 5.0], [7.0, 10.0, 20.0]])
+
+    bt = METimageL1BNCFileHandler._calibrate_bt(radiance, 13.0, 3.0, 100.0)
+
+    expected_bt = np.array([[675.04993213, 753.10301462, 894.93149648],
+                            [963.20401882, 1048.95086402, 1270.95546218]])
+    np.testing.assert_allclose(bt, expected_bt)
+
+
+def test_calibrate_refl():
+    """Test the reflectance calibration function."""
+    radiance = np.array([[1.0, 2.0, 5.0], [7.0, 10.0, 20.0]])
+
+    refl = METimageL1BNCFileHandler._calibrate_refl(radiance, 2.0)
+
+    expected_refl = np.array([[157.07963268, 314.15926536, 785.3981634],
+                              [1099.55742876, 1570.79632679, 3141.59265359]])
+    np.testing.assert_allclose(refl, expected_refl)
+
+
+def test_perform_orthorectification(reader):
+    """Test that the orthorectification offsets the variable by the delta in degrees."""
+    variable = _make_variable()
+
+    orthorect_variable = reader._perform_orthorectification(variable, "data/measurement_data/delta_lat")
+
+    expected_values = (np.degrees(np.ones((NUM_LINES, NUM_PIXELS)) / MEAN_EARTH_RADIUS)
+                       + np.ones((NUM_LINES, NUM_PIXELS)))
+    np.testing.assert_allclose(orthorect_variable.values, expected_values)
+
+
+def test_radiance_calibration_returns_input(reader):
+    """Test that the radiance calibration returns a copy of the variable."""
+    variable = _make_variable()
+
+    return_variable = reader._perform_calibration(variable, {"calibration": "radiance"})
+
+    xr.testing.assert_equal(return_variable, variable)
+
+
+def test_invalid_calibration_raises(reader):
+    """Test that an unknown calibration raises a ValueError."""
+    variable = _make_variable()
+
+    with pytest.raises(ValueError, match="Unknown calibration invalid for dataset test"):
+        reader._perform_calibration(variable, {"calibration": "invalid", "name": "test"})
+
+
+def test_brightness_temperature_calibration(reader):
+    """Test that the brightness temperature calibration uses the file coefficients."""
+    variable = _make_variable()
+
+    calibrated_variable = reader._perform_calibration(variable,
+                                                      {"calibration": "brightness_temperature",
+                                                       "chan_thermal_index": 3})
+
+    expected_values = np.full((NUM_LINES, NUM_PIXELS), 1237.52069907)
+    np.testing.assert_allclose(calibrated_variable.values, expected_values)
+
+
+def test_reflectance_calibration(reader):
+    """Test that the reflectance calibration uses the file coefficients."""
+    variable = _make_variable()
+
+    calibrated_variable = reader._perform_calibration(variable,
+                                                      {"calibration": "reflectance",
+                                                       "wavelength": [0.658, 0.668, 0.678],
+                                                       "chan_solar_index": 2})
+
+    expected_values = np.full((NUM_LINES, NUM_PIXELS), 104.71975512)
+    np.testing.assert_allclose(calibrated_variable.values, expected_values)
 
 
 # Row chunks the reader is expected to produce for the test file (600 rows of 72 float32 pixels,
