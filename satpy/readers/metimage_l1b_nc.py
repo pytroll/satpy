fc09e959b6dadb0f277ae9cd1709f6377f3e892d
andream <andrea.meraner@eumetsat.int>
2026-09-10 15:01:50 +0000

    remove old comment

--- satpy/readers/metimage_l1b_nc.py
+++ satpy/readers/metimage_l1b_nc.py
@@ -33,9 +33,6 @@ class METimageL1BNCFileHandler(METimageNCBaseFileHandler):
         self._bt_conversion_b = self["data/calibration_data/bt_conversion_b"].values
         self._channel_cw_thermal = self["data/calibration_data/channel_cw_thermal"].values
         self._integrated_solar_irradiance = self["data/calibration_data/band_averaged_solar_irradiance"].values
-        # Computes the angle factor for reflectance calibration as inverse of cosine of solar zenith angle
-        # (the values in the product file are on tie points and in degrees,
-        # therefore interpolation and conversion to radians are required)
 
     def _perform_calibration(self, variable: xr.DataArray, dataset_info: dict) -> xr.DataArray:
         """Perform the calibration.
