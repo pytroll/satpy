3ef3599654be27fbc46510daf0167d1f248e4069
David Hoese <david.hoese@ssec.wisc.edu>
2026-09-16 06:53:56 -0500

    Convert one unittest test case in test_crefl_utils to pytest

--- satpy/tests/test_crefl_utils.py
+++ satpy/tests/test_crefl_utils.py
@@ -1,27 +1,23 @@
 """Test CREFL rayleigh correction functions."""
-import unittest
 import warnings
 
 import numpy as np
 import pytest
 
 
-class TestCreflUtils(unittest.TestCase):
-    """Test crefl_utils."""
-
-    def test_get_atm_variables_abi(self):
-        """Test getting atmospheric variables for ABI."""
-        from satpy.modifiers._crefl_utils import _ABIAtmosphereVariables
-        atm_vars = _ABIAtmosphereVariables(
-            21.71342113, 77.14385758, 56.214566960,
-            0.17690244, 6.123234e-17, 530.61332168, 405.,
-            0.0043149700000000004, 0.0037296, 0.014107995000000002, 0.052349,
-        )
-        sphalb, rhoray, TtotraytH2O, tOG = atm_vars()
-        assert abs(np.array(sphalb) - 0.045213532544630494) < 1e-10
-        assert abs(rhoray - 2.2030281148621356) < 1e-10
-        assert abs(TtotraytH2O - 0.30309880915889087) < 1e-10
-        assert abs(tOG - 0.5969089524560548) < 1e-10
+def test_get_atm_variables_abi():
+    """Test getting atmospheric variables for ABI."""
+    from satpy.modifiers._crefl_utils import _ABIAtmosphereVariables
+    atm_vars = _ABIAtmosphereVariables(
+        21.71342113, 77.14385758, 56.214566960,
+        0.17690244, 6.123234e-17, 530.61332168, 405.,
+        0.0043149700000000004, 0.0037296, 0.014107995000000002, 0.052349,
+    )
+    sphalb, rhoray, TtotraytH2O, tOG = atm_vars()
+    assert abs(np.array(sphalb) - 0.045213532544630494) < 1e-10
+    assert abs(rhoray - 2.2030281148621356) < 1e-10
+    assert abs(TtotraytH2O - 0.30309880915889087) < 1e-10
+    assert abs(tOG - 0.5969089524560548) < 1e-10
 
 
 # METimage band name, wavelength range, matching MODIS band, LUT index
